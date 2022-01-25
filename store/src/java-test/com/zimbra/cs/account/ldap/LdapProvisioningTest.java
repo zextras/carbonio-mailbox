8d43496e433805639457db1317faf86ce45f635e
Keshav Bhatt <keshavnrj@gmail.com>
2022-01-25 18:41:21 +0530

    chore: test method by directly ivoking it

--- store/src/java-test/com/zimbra/cs/account/ldap/LdapProvisioningTest.java
+++ store/src/java-test/com/zimbra/cs/account/ldap/LdapProvisioningTest.java
@@ -2,30 +2,23 @@ package com.zimbra.cs.account.ldap;
 
 import com.zimbra.common.service.ServiceException;
 import com.zimbra.cs.account.Account;
+import com.zimbra.cs.account.AccountServiceException;
 import com.zimbra.cs.account.Provisioning;
 import com.zimbra.cs.mailbox.MailboxTestUtil;
-import java.util.Arrays;
 import java.util.HashMap;
-import java.util.Optional;
 import java.util.UUID;
-import java.util.stream.Stream;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-
-/**
- * Unit test for {@link LdapProvisioning}.
- *
- */
+/** Unit test for {@link LdapProvisioning}. */
 public class LdapProvisioningTest {
 
   @BeforeClass
   public static void init() throws Exception {
     MailboxTestUtil.initServer();
     Provisioning prov = Provisioning.getInstance();
-    HashMap<String,Object> attrs;
+    HashMap<String, Object> attrs;
     attrs = new HashMap<>();
     attrs.put(Provisioning.A_zimbraId, UUID.randomUUID().toString());
     attrs.put(Provisioning.A_sn, "Leesa");
@@ -33,6 +26,15 @@ public class LdapProvisioningTest {
     attrs.put(Provisioning.A_initials, "James");
     attrs.put(Provisioning.A_mail, "natalie.leesa@example.com");
     prov.createAccount("natalie.leesa@example.com", "testpassword", attrs);
+
+    HashMap<String, Object> attrs2;
+    attrs2 = new HashMap<>();
+    attrs2.put(Provisioning.A_zimbraId, UUID.randomUUID().toString());
+    attrs2.put(Provisioning.A_sn, "Lamborghini");
+    attrs2.put(Provisioning.A_cn, "Milano");
+    attrs2.put(Provisioning.A_initials, "Cars");
+    attrs2.put(Provisioning.A_mail, "milano@lamborghini");
+    prov.createAccount("milano@lamborghini", "testpassword", attrs2);
   }
 
   @Before
@@ -41,58 +43,47 @@ public class LdapProvisioningTest {
   }
 
   /**
-   * This test is to validate functionality of
-   * ({@link com.zimbra.cs.account.ldap.LdapProvisioning#validatePasswordEntropyForPersonalData(String, Account)})
-   * @throws ServiceException */
-  @Test
+   * This test is to validate functionality of ({@link
+   * com.zimbra.cs.account.ldap.LdapProvisioning#validatePasswordEntropyForPersonalData(String,
+   * Account)})
+   *
+   * @throws ServiceException
+   */
+  @Test(expected = AccountServiceException.class)
   public void shouldFindPersonalDataInPassword() throws ServiceException {
 
     Account acct = Provisioning.getInstance().getAccount("natalie.leesa@example.com");
 
-    //fake passwords for test against created user account
-    String[] testPasswords = {"natalie", "Natalie123", "Leesa34", "example01", "Leesa.Example", "eXamPle", "2022james"};
-
-    // get possible personal data for account
-    Optional<String> sName =
-        Optional.of(Optional.ofNullable(acct.getAttr(Provisioning.A_sn)).orElse(""));
-    Optional<String> cName =
-        Optional.of(Optional.ofNullable(acct.getAttr(Provisioning.A_cn)).orElse(""));
-    Optional<String> initials =
-        Optional.of(Optional.ofNullable(acct.getAttr(Provisioning.A_initials)).orElse(""));
-    Optional<String> email =
-        Optional.of(Optional.ofNullable(acct.getAttr(Provisioning.A_mail)).orElse(""));
-
-    // prepare dictionary
-    String[] fullNameExploded = cName.get().toLowerCase().split(" ");
-    String[] emailExploded = email.get().split("[@._]");
-    String[] cleanEmailExploded =
-        Arrays.copyOf(emailExploded, emailExploded.length - 1); // remove the top level domain(TLD)
-    Arrays.stream(cleanEmailExploded)
-        .forEach(
-            em -> {
-              System.out.println("email element" + em);
-            });
-
-    // find match in dictionary and password
-    Arrays.stream(testPasswords).map(String::toLowerCase).forEach(passwordLower -> {
-      String[] personalDataImploded = {
-          sName.get().toLowerCase(),
-          cName.get().toLowerCase(),
-          cName.get().replace(" ", "").toLowerCase(),
-          initials.get().toLowerCase()
-      };
-      Optional<String> matchedSensitivePart =
-          Stream.of(personalDataImploded, cleanEmailExploded, fullNameExploded)
-              .flatMap(Stream::of)
-              .parallel()
-              .filter(
-                  part ->
-                      (part.length() > 2
-                          && (part.contains(passwordLower) || passwordLower.contains(part))))
-              .findAny();
-      Assert.assertTrue("matching against password:" + passwordLower,
-          matchedSensitivePart.isPresent());
-    });
+    // fake passwords for test against created user account
+    String[] testPasswords = {
+      "natalie", "Natalie123", "Leesa34", "example01", "Leesa.Example", "eXamPle", "2022james"
+    };
+    for (String testPassword : testPasswords) {
+      String passwordLower = testPassword.toLowerCase();
+      LdapProvisioning.validatePasswordEntropyForPersonalData(passwordLower, acct);
+    }
   }
-  
+
+  /**
+   * This test is to validate functionality of ({@link
+   * com.zimbra.cs.account.ldap.LdapProvisioning#validatePasswordEntropyForPersonalData(String,
+   * Account)}) for specific case in which user might be using a dotless domain
+   *
+   * @throws ServiceException
+   */
+  @Test(expected = AccountServiceException.class)
+  public void shouldFindPersonalDataInPasswordForDotLessDomainsEmails() throws ServiceException {
+
+    Account acct = Provisioning.getInstance().getAccount("milano@lamborghini");
+
+    // fake passwords for test against created user account
+    String[] testPasswords = {
+      "milano", "Lamborghini123", "Milano34", "lamborghini01", "Milano.Lamborghini", "lAmbOrgHini", "2022cars"
+    };
+
+    for (String testPassword : testPasswords) {
+      String passwordLower = testPassword.toLowerCase();
+      LdapProvisioning.validatePasswordEntropyForPersonalData(passwordLower, acct);
+    }
+  }
 }
