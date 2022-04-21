c805cbdee407a40d3c372db3ffebe9b959821629
Davide Frison <frisonisland@gmail.com>
2022-04-21 12:48:21 +0200

    test[CalendarItemTest]: more static import

--- store/src/java-test/com/zimbra/cs/mailbox/CalendarItemTest.java
+++ store/src/java-test/com/zimbra/cs/mailbox/CalendarItemTest.java
@@ -4,59 +4,60 @@
 
 package com.zimbra.cs.mailbox;
 
-import java.util.HashMap;
-
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.CALLS_REAL_METHODS;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 import com.zimbra.cs.account.MockProvisioning;
 import com.zimbra.cs.account.Provisioning;
 import com.zimbra.cs.mailbox.MailItem.UnderlyingData;
-import org.mockito.Mockito;
+import java.util.HashMap;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
 
 public class CalendarItemTest {
-    private OperationContext octxt;
-    private CalendarItem calItem;
-    private final Integer SEQ = 123;
+  private OperationContext octxt;
+  private CalendarItem calItem;
+  private final Integer SEQ = 123;
 
-    /**
-     * @throws java.lang.Exception
-     */
-    @BeforeClass
-    public static void init() throws Exception {
-        MailboxTestUtil.initServer();
-        Provisioning prov = Provisioning.getInstance();
-        prov.createAccount("test@zimbra.com", "secret", new HashMap<String, Object>());
-    }
+  /**
+   * @throws java.lang.Exception
+   */
+  @BeforeClass
+  public static void init() throws Exception {
+    MailboxTestUtil.initServer();
+    Provisioning prov = Provisioning.getInstance();
+    prov.createAccount("test@zimbra.com", "secret", new HashMap<String, Object>());
+  }
 
-    /**
-     * @throws java.lang.Exception
-     */
-    @Before
-    public void setUp() throws Exception {
-        octxt = Mockito.mock(OperationContext.class);
-        Mailbox mbox = MailboxManager.getInstance().getMailboxByAccountId(MockProvisioning.DEFAULT_ACCOUNT_ID);
-        // have to spy because later real method is called
-        calItem = Mockito.mock(CalendarItem.class, Mockito.CALLS_REAL_METHODS);
-        calItem.mData = new UnderlyingData();
-        calItem.mMailbox = mbox;
-    }
+  /**
+   * @throws java.lang.Exception
+   */
+  @Before
+  public void setUp() throws Exception {
+    octxt = mock(OperationContext.class);
+    Mailbox mbox =
+        MailboxManager.getInstance().getMailboxByAccountId(MockProvisioning.DEFAULT_ACCOUNT_ID);
+    // have to spy because later real method is called
+    calItem = mock(CalendarItem.class, CALLS_REAL_METHODS);
+    calItem.mData = new UnderlyingData();
+    calItem.mMailbox = mbox;
+  }
 
-    /**
-     * @throws java.lang.Exception
-     */
-    @Test
-    public void testPerformSetPrevFoldersOperation() throws Exception {
-        Mockito.when(calItem.getModifiedSequence()).thenReturn(SEQ);
+  /**
+   * @throws java.lang.Exception
+   */
+  @Test
+  public void testPerformSetPrevFoldersOperation() throws Exception {
+    when(calItem.getModifiedSequence()).thenReturn(SEQ);
 
-        calItem.performSetPrevFoldersOperation(octxt);
-        // above method adds 2 in mod sequence, so verify SEQ+2 and Mailbox.ID_FOLDER_TRASH
+    calItem.performSetPrevFoldersOperation(octxt);
+    // above method adds 2 in mod sequence, so verify SEQ+2 and Mailbox.ID_FOLDER_TRASH
 
-        Assert.assertEquals(calItem.mData.getPrevFolders(), (SEQ+2)+":" + Mailbox.ID_FOLDER_TRASH);
-        Assert.assertEquals(calItem.getPrevFolders(), (SEQ+2)+":" + Mailbox.ID_FOLDER_TRASH);
-        Assert.assertEquals(calItem.mData.getPrevFolders(), calItem.getPrevFolders());
-    }
+    assertEquals(calItem.mData.getPrevFolders(), (SEQ + 2) + ":" + Mailbox.ID_FOLDER_TRASH);
+    assertEquals(calItem.getPrevFolders(), (SEQ + 2) + ":" + Mailbox.ID_FOLDER_TRASH);
+    assertEquals(calItem.mData.getPrevFolders(), calItem.getPrevFolders());
+  }
 }
