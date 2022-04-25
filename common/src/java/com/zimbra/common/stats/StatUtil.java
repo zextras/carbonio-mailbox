ee16eaa1af14bfc3646e6fe2b51b54953ca1e8d7
Keshav Bhatt <keshavnrj@gmail.com>
2022-04-25 20:55:18 +0530

    chore: update comment to make thread stats class general

--- common/src/java/com/zimbra/common/stats/StatUtil.java
+++ common/src/java/com/zimbra/common/stats/StatUtil.java
@@ -7,13 +7,10 @@ package com.zimbra.common.stats;
 
 import java.util.Date;
 
-
 public class StatUtil {
 
-    /**
-     * Returns the current time formatted as <tt>MM/dd/yyyy hh:mm:ss</tt>.
-     */
-    public static String getTimestampString() {
-        return String.format("%1$tm/%1$td/%1$tY %1$tH:%1$tM:%1$tS", new Date());
-    }
+  /** Returns the current time formatted as <tt>MM/dd/yyyy hh:mm:ss</tt>. */
+  public static String getTimestampString() {
+    return String.format("%1$tm/%1$td/%1$tY %1$tH:%1$tM:%1$tS", new Date());
+  }
 }
