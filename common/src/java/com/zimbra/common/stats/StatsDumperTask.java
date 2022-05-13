30ca4710f9373a5f8d5d4f12e4c38bc0d084846a
Keshav Bhatt <keshavnrj@gmail.com>
2022-05-13 14:25:08 +0530

    chore: modify StatsDumperTask to use new StatsScheduler

--- common/src/java/com/zimbra/common/stats/StatsDumperTask.java
+++ common/src/java/com/zimbra/common/stats/StatsDumperTask.java
@@ -5,44 +5,27 @@ import java.util.concurrent.Callable;
 
 /**
  * StatsDumperTask class
+ *
  * @author Keshav Bhatt
  * @since 4.0.7
- * */
+ */
 public class StatsDumperTask implements Runnable {
-  private final long intervalMillis;
-  private final StatsDumperDataSource dataSource;
-  private final Callable<Void> dumper;
+
+  private final Dumper dumper;
 
   /**
-   * @param intervalMillis interval in milli-seconds this thread will wait to call the dumper
-   * @param dataSource {@link StatsDumperDataSource} data source for which the task is concerned with
    * @param dumper dumper {@link Callable} instance which the task is concerned with
    */
-  public StatsDumperTask(
-      long intervalMillis, StatsDumperDataSource dataSource, Callable<Void> dumper) {
-    this.intervalMillis = intervalMillis;
-    this.dataSource = dataSource;
+  public StatsDumperTask(Dumper dumper) {
     this.dumper = dumper;
   }
 
   @Override
   public void run() {
-    //noinspection InfiniteLoopStatement
-    while (true) {
-      try {
-        //noinspection BusyWait
-        Thread.sleep(intervalMillis);
-        try {
-          dumper.call();
-        } catch (Exception e) {
-          ZimbraLog.perf.warn("Exception in stats thread: %s", dataSource.getFilename(), e);
-        }
-      } catch (InterruptedException e) {
-        ZimbraLog.perf.info("Stats thread interrupted: %s", dataSource.getFilename(), e);
-      }
-      if (Thread.currentThread().isInterrupted()) {
-        ZimbraLog.perf.info("Stats thread was interrupted: %s", dataSource.getFilename());
-      }
+    try {
+      dumper.call();
+    } catch (Exception e) {
+      ZimbraLog.perf.warn("Exception in stats thread: %s", dumper.getDataSource().getFilename(), e);
     }
   }
 }
