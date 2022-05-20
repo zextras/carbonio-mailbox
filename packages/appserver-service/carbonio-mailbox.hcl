2eaf47190fddf99e61d99350e2b0db8aef1e7e36
Davide Frison <frisonisland@gmail.com>
2022-05-20 10:45:02 +0200

    chore(carbonio-mailbox.hcl): add acrbonio-mta upstream chore(SmtpConfig.java): change default SMTP port

--- packages/appserver-service/carbonio-mailbox.hcl
+++ packages/appserver-service/carbonio-mailbox.hcl
@@ -23,6 +23,11 @@ services {
             local_bind_port    = 20002
             local_bind_address = "127.78.0.7"
           },
+          {
+            destination_name   = "carbonio-mta"
+            local_bind_port    = 20025
+            local_bind_address = "127.78.0.7"
+          },
         ]
       }
     }
--- store/src/java/com/zimbra/cs/mailclient/smtp/SmtpConfig.java
+++ store/src/java/com/zimbra/cs/mailclient/smtp/SmtpConfig.java
@@ -9,76 +9,72 @@ import com.google.common.base.MoreObjects;
 import com.zimbra.common.util.ZimbraLog;
 import com.zimbra.cs.mailclient.MailConfig;
 
-/**
- * SMTP client configuration.
- */
+/** SMTP client configuration. */
 public final class SmtpConfig extends MailConfig {
-    public static final String PROTOCOL = "smtp";
-    public static final int DEFAULT_PORT = 25;
-    public static final int DEFAULT_SSL_PORT = 465;
+  public static final String PROTOCOL = "smtp";
+  public static final int DEFAULT_PORT = 20025;
+  public static final int DEFAULT_SSL_PORT = DEFAULT_PORT;
 
-    private String domain;
-    private boolean allowPartialSend;
-    private String dsn;
+  private String domain;
+  private boolean allowPartialSend;
+  private String dsn;
 
-    public SmtpConfig(String host, int port, String domain) {
-        super(ZimbraLog.smtp, host);
-        setPort(port);
-        setDomain(domain);
+  public SmtpConfig(String host, int port, String domain) {
+    super(ZimbraLog.smtp, host);
+    setPort(port);
+    setDomain(domain);
+  }
+
+  public SmtpConfig(String host) {
+    super(ZimbraLog.smtp, host);
+    setPort(DEFAULT_PORT);
+  }
+
+  public SmtpConfig() {
+    super(ZimbraLog.smtp);
+  }
+
+  @Override
+  public String getProtocol() {
+    return PROTOCOL;
+  }
+
+  public void setDomain(String domain) {
+    this.domain = domain;
+  }
+
+  public String getDomain() {
+    return MoreObjects.firstNonNull(domain, "localhost");
+  }
+
+  public void setAllowPartialSend(boolean allow) {
+    this.allowPartialSend = allow;
+  }
+
+  public boolean isPartialSendAllowed() {
+    return allowPartialSend;
+  }
+
+  public void setDsn(String dsn) {
+    this.dsn = dsn;
+  }
+
+  public String getDsn() {
+    return dsn;
+  }
+
+  @Override
+  public MoreObjects.ToStringHelper addToStringInfo(MoreObjects.ToStringHelper helper) {
+    helper = super.addToStringInfo(helper);
+    helper.add("domain", domain).add("allowPartialSend", allowPartialSend);
+    if (null != dsn) {
+      helper.add("dsn", dsn);
     }
+    return helper;
+  }
 
-    public SmtpConfig(String host) {
-        super(ZimbraLog.smtp, host);
-        setPort(DEFAULT_PORT);
-    }
-
-    public SmtpConfig() {
-        super(ZimbraLog.smtp);
-    }
-
-    @Override
-    public String getProtocol() {
-        return PROTOCOL;
-    }
-
-    public void setDomain(String domain) {
-        this.domain = domain;
-    }
-
-    public String getDomain() {
-        return MoreObjects.firstNonNull(domain, "localhost");
-    }
-
-    public void setAllowPartialSend(boolean allow) {
-        this.allowPartialSend = allow;
-    }
-
-    public boolean isPartialSendAllowed() {
-        return allowPartialSend;
-    }
-
-    public void setDsn(String dsn) {
-        this.dsn = dsn;
-    }
-
-    public String getDsn() {
-        return dsn;
-    }
-
-    @Override
-    public MoreObjects.ToStringHelper addToStringInfo(MoreObjects.ToStringHelper helper) {
-        helper = super.addToStringInfo(helper);
-        helper
-            .add("domain", domain)
-            .add("allowPartialSend", allowPartialSend);
-        if (null != dsn) {
-            helper.add("dsn", dsn);
-        }
-        return helper;
-    }
-
-    @Override
-    public String toString() {
-        return addToStringInfo(MoreObjects.toStringHelper(this)).toString();
-    }
+  @Override
+  public String toString() {
+    return addToStringInfo(MoreObjects.toStringHelper(this)).toString();
+  }
 }
