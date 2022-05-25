b9402f081726a2c4ceea6f3c3a9bba09a62cc1f9
Davide Frison <davide.frison@zextras.com>
2022-05-25 16:52:14 +0200

    chore: revert SmtpConfig and SmtpTransport as in main

--- store/src/java/com/zimbra/cs/mailclient/smtp/SmtpConfig.java
+++ store/src/java/com/zimbra/cs/mailclient/smtp/SmtpConfig.java
@@ -9,73 +9,76 @@ import com.google.common.base.MoreObjects;
 import com.zimbra.common.util.ZimbraLog;
 import com.zimbra.cs.mailclient.MailConfig;
 
-/** SMTP client configuration. */
+/**
+ * SMTP client configuration.
+ */
 public final class SmtpConfig extends MailConfig {
-  public static final String PROTOCOL = "smtp";
-  public static final int DEFAULT_PORT = 20025;
-  public static final int DEFAULT_SSL_PORT = DEFAULT_PORT;
-  public static final String DEFAULT_HOST = "127.78.0.7";
+    public static final String PROTOCOL = "smtp";
+    public static final int DEFAULT_PORT = 25;
+    public static final int DEFAULT_SSL_PORT = 465;
 
-  private String domain;
-  private boolean allowPartialSend;
-  private String dsn;
+    private String domain;
+    private boolean allowPartialSend;
+    private String dsn;
 
-  public SmtpConfig(String host, int port, String domain) {
-    super(ZimbraLog.smtp, host);
-    setPort(port);
-    setDomain(domain);
-  }
-
-  public SmtpConfig(String host) {
-    super(ZimbraLog.smtp, host);
-    setPort(DEFAULT_PORT);
-  }
-
-  public SmtpConfig() {
-    super(ZimbraLog.smtp);
-  }
-
-  @Override
-  public String getProtocol() {
-    return PROTOCOL;
-  }
-
-  public void setDomain(String domain) {
-    this.domain = domain;
-  }
-
-  public String getDomain() {
-    return MoreObjects.firstNonNull(domain, "localhost");
-  }
-
-  public void setAllowPartialSend(boolean allow) {
-    this.allowPartialSend = allow;
-  }
-
-  public boolean isPartialSendAllowed() {
-    return allowPartialSend;
-  }
-
-  public void setDsn(String dsn) {
-    this.dsn = dsn;
-  }
-
-  public String getDsn() {
-    return dsn;
-  }
-
-  @Override
-  public MoreObjects.ToStringHelper addToStringInfo(MoreObjects.ToStringHelper helper) {
-    helper = super.addToStringInfo(helper);
-    helper.add("domain", domain).add("allowPartialSend", allowPartialSend);
-    if (null != dsn) {
-      helper.add("dsn", dsn);
+    public SmtpConfig(String host, int port, String domain) {
+        super(ZimbraLog.smtp, host);
+        setPort(port);
+        setDomain(domain);
     }
-    return helper;
-  }
 
-  @Override
-  public String toString() {
-    return addToStringInfo(MoreObjects.toStringHelper(this)).toString();
-  }
+    public SmtpConfig(String host) {
+        super(ZimbraLog.smtp, host);
+        setPort(DEFAULT_PORT);
+    }
+
+    public SmtpConfig() {
+        super(ZimbraLog.smtp);
+    }
+
+    @Override
+    public String getProtocol() {
+        return PROTOCOL;
+    }
+
+    public void setDomain(String domain) {
+        this.domain = domain;
+    }
+
+    public String getDomain() {
+        return MoreObjects.firstNonNull(domain, "localhost");
+    }
+
+    public void setAllowPartialSend(boolean allow) {
+        this.allowPartialSend = allow;
+    }
+
+    public boolean isPartialSendAllowed() {
+        return allowPartialSend;
+    }
+
+    public void setDsn(String dsn) {
+        this.dsn = dsn;
+    }
+
+    public String getDsn() {
+        return dsn;
+    }
+
+    @Override
+    public MoreObjects.ToStringHelper addToStringInfo(MoreObjects.ToStringHelper helper) {
+        helper = super.addToStringInfo(helper);
+        helper
+            .add("domain", domain)
+            .add("allowPartialSend", allowPartialSend);
+        if (null != dsn) {
+            helper.add("dsn", dsn);
+        }
+        return helper;
+    }
+
+    @Override
+    public String toString() {
+        return addToStringInfo(MoreObjects.toStringHelper(this)).toString();
+    }
 }
