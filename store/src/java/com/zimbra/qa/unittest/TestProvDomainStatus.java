6b3f0f7ba6f607766593386c186cf239566da7ad
Keshav Bhatt <keshavnrj@gmail.com>
2022-08-02 13:35:27 +0530

    chore: remove commented code

--- store/src/java/com/zimbra/qa/unittest/TestProvDomainStatus.java
+++ store/src/java/com/zimbra/qa/unittest/TestProvDomainStatus.java
@@ -45,10 +45,10 @@ public class TestProvDomainStatus {
   private SoapProvisioning mSoapProvAdmin;
 
   private String TEST_ID;
-  private static String TEST_CLASS_NAME = "test-domainstatus";
+  private static final String TEST_CLASS_NAME = "test-domainstatus";
 
-  private static String PASSWORD = "test123";
-  private static String NAMEPREFIX_ACCOUNT = "acct-";
+  private static final String PASSWORD = "test123";
+  private static final String NAMEPREFIX_ACCOUNT = "acct-";
   private String DOMAIN_NAME;
 
   private static final String NAME_PREFIX = TestLmtp.class.getSimpleName();
@@ -60,9 +60,9 @@ public class TestProvDomainStatus {
   public static void init() throws Exception {
     CliUtil.toolSetup();
 
-    sStatusMap = new HashMap<String, Map<String, String>>();
+    sStatusMap = new HashMap<>();
 
-    Map<String, String> domainActive = new HashMap<String, String>();
+    Map<String, String> domainActive = new HashMap<>();
     domainActive.put(Provisioning.ACCOUNT_STATUS_ACTIVE, Provisioning.ACCOUNT_STATUS_ACTIVE);
     domainActive.put(Provisioning.ACCOUNT_STATUS_LOCKOUT, Provisioning.ACCOUNT_STATUS_LOCKOUT);
     domainActive.put(Provisioning.ACCOUNT_STATUS_LOCKED, Provisioning.ACCOUNT_STATUS_LOCKED);
@@ -114,26 +114,26 @@ public class TestProvDomainStatus {
     sStatusMap.put(Provisioning.DOMAIN_STATUS_CLOSED, domainClosed);
   }
 
-  private static enum DomainStatus {
+  private enum DomainStatus {
     // must match ldap value
     EMPTY, // pseudo value for testing, meaning no value in zimbraDomainStatus
     active,
     locked,
     maintenance,
     suspended,
-    closed;
+    closed
   }
 
-  private static enum AccountStatus {
+  private enum AccountStatus {
     // must match ldap value
     active,
     lockout,
     locked,
     maintenance,
-    closed;
+    closed
   }
 
-  private static enum AccountType {
+  private enum AccountType {
     ACCT_USER,
     ACCT_DOMAIN_ADMIN,
     ACCT_GLOBAL_ADMIN
@@ -151,11 +151,10 @@ public class TestProvDomainStatus {
 
     mSoapProvAdmin = new SoapProvisioning();
     mSoapProvAdmin.soapSetURI(TestUtil.getAdminSoapUrl());
-    // mSoapProvAdmin.soapSetURI("https://phoebe.local:7071/service/admin/soap/");
-    // mSoapProvAdmin.soapSetURI("https://localhost:7071/service/admin/soap/");
     mSoapProvAdmin.soapZimbraAdminAuthenticate();
 
     mProv = mSoapProvAdmin;
+    System.out.println("\n===== assertion failed =====");
 
     DOMAIN_NAME = TestProvisioningUtil.baseDomainName(TEST_CLASS_NAME, TEST_ID);
 
@@ -323,14 +322,14 @@ public class TestProvDomainStatus {
   }
 
   static class SoapTestContext {
-    private SoapClient mSoapClient;
-    private AccountType mAuthedAcctType;
-    private Account mTargetAcct;
-    private boolean mSuspended;
-    private String mDomainName;
-    private String mDomainId;
-    private String[] mTesters;
-    private String mTestId;
+    private final SoapClient mSoapClient;
+    private final AccountType mAuthedAcctType;
+    private final Account mTargetAcct;
+    private final boolean mSuspended;
+    private final String mDomainName;
+    private final String mDomainId;
+    private final String[] mTesters;
+    private final String mTestId;
 
     SoapTestContext(
         SoapClient soapClient,
@@ -354,15 +353,15 @@ public class TestProvDomainStatus {
 
   static class SoapCommands {
 
-    private SoapTestContext mCtx;
+    private final SoapTestContext mCtx;
 
-    public static enum TesterObjs {
+    public enum TesterObjs {
       TESTER_ID_ACCOUNT_FOR_DELETE_ACCOUNT_REQUEST
     }
 
     // signature APIs only takes id, not name,
     // remember the signature ids, we need them for modify/delete signatures
-    private Map<String, String> mSignatureNameIdMap;
+    private final Map<String, String> mSignatureNameIdMap;
 
     SoapCommands(SoapTestContext ctx) {
       mCtx = ctx;
@@ -682,7 +681,7 @@ public class TestProvDomainStatus {
       mCtx.mSoapClient.createCos(cosName(), null);
     }
 
-    private static enum Result {
+    private enum Result {
       GOOD("G_O_O_D", "G_O_O_D"),
       BAD("B_A_D", "B_A_D"),
 
@@ -720,12 +719,6 @@ public class TestProvDomainStatus {
         assertNotNull(code);
         assertNotNull(msg);
 
-        /*
-        String key = R.getKey(code, msg);
-        Result r = R.sMap.get(key);
-        assertNotNull(key, r);  // unknown exception
-        */
-
         String key = R.getKey(code, msg);
         for (String k : R.sMap.keySet()) {
           if (key.startsWith(k)) return R.sMap.get(k);
@@ -735,7 +728,7 @@ public class TestProvDomainStatus {
       }
     }
 
-    private static enum Command {
+    private enum Command {
       ACCOUNT_FIRST_COMMAND(),
       ACCOUNT_AUTH_REQUEST(
           "AUTH_REQUEST",
@@ -1149,124 +1142,6 @@ public class TestProvDomainStatus {
       }
     }
 
-    /*
-    dispatcher.registerHandler(AdminConstants.GET_COS_REQUEST, new GetCos());
-    dispatcher.registerHandler(AdminConstants.GET_ALL_COS_REQUEST, new GetAllCos());
-    dispatcher.registerHandler(AdminConstants.MODIFY_COS_REQUEST, new ModifyCos());
-    dispatcher.registerHandler(AdminConstants.DELETE_COS_REQUEST, new DeleteCos());
-    dispatcher.registerHandler(AdminConstants.RENAME_COS_REQUEST, new RenameCos());
-
-    dispatcher.registerHandler(AdminConstants.CREATE_SERVER_REQUEST, new CreateServer());
-    dispatcher.registerHandler(AdminConstants.GET_SERVER_REQUEST, new GetServer());
-    dispatcher.registerHandler(AdminConstants.GET_ALL_SERVERS_REQUEST, new GetAllServers());
-    dispatcher.registerHandler(AdminConstants.MODIFY_SERVER_REQUEST, new ModifyServer());
-    dispatcher.registerHandler(AdminConstants.DELETE_SERVER_REQUEST, new DeleteServer());
-
-    dispatcher.registerHandler(AdminConstants.GET_CONFIG_REQUEST, new GetConfig());
-    dispatcher.registerHandler(AdminConstants.GET_ALL_CONFIG_REQUEST, new GetAllConfig());
-    dispatcher.registerHandler(AdminConstants.MODIFY_CONFIG_REQUEST, new ModifyConfig());
-
-    dispatcher.registerHandler(AdminConstants.GET_SERVICE_STATUS_REQUEST, new GetServiceStatus());
-
-    dispatcher.registerHandler(AdminConstants.PURGE_MESSAGES_REQUEST, new PurgeMessages());
-    dispatcher.registerHandler(AdminConstants.DELETE_MAILBOX_REQUEST, new DeleteMailbox());
-    dispatcher.registerHandler(AdminConstants.GET_MAILBOX_REQUEST, new GetMailbox());
-
-    dispatcher.registerHandler(AdminConstants.MAINTAIN_TABLES_REQUEST, new MaintainTables());
-
-    dispatcher.registerHandler(AdminConstants.RUN_UNIT_TESTS_REQUEST, new RunUnitTests());
-
-    dispatcher.registerHandler(AdminConstants.CHECK_AUTH_CONFIG_REQUEST, new CheckAuthConfig());
-    dispatcher.registerHandler(AdminConstants.CHECK_GAL_CONFIG_REQUEST, new CheckGalConfig());
-    dispatcher.registerHandler(AdminConstants.CHECK_HOSTNAME_RESOLVE_REQUEST, new CheckHostnameResolve());
-
-    dispatcher.registerHandler(AdminConstants.CREATE_VOLUME_REQUEST, new CreateVolume());
-    dispatcher.registerHandler(AdminConstants.GET_VOLUME_REQUEST, new GetVolume());
-    dispatcher.registerHandler(AdminConstants.GET_ALL_VOLUMES_REQUEST, new GetAllVolumes());
-    dispatcher.registerHandler(AdminConstants.MODIFY_VOLUME_REQUEST, new ModifyVolume());
-    dispatcher.registerHandler(AdminConstants.DELETE_VOLUME_REQUEST, new DeleteVolume());
-    dispatcher.registerHandler(AdminConstants.GET_CURRENT_VOLUMES_REQUEST, new GetCurrentVolumes());
-    dispatcher.registerHandler(AdminConstants.SET_CURRENT_VOLUME_REQUEST, new SetCurrentVolume());
-
-    dispatcher.registerHandler(AdminConstants.CREATE_DISTRIBUTION_LIST_REQUEST, new CreateDistributionList());
-    dispatcher.registerHandler(AdminConstants.GET_DISTRIBUTION_LIST_REQUEST, new GetDistributionList());
-    dispatcher.registerHandler(AdminConstants.GET_ALL_DISTRIBUTION_LISTS_REQUEST, new GetAllDistributionLists());
-    dispatcher.registerHandler(AdminConstants.ADD_DISTRIBUTION_LIST_MEMBER_REQUEST, new AddDistributionListMember());
-    dispatcher.registerHandler(AdminConstants.REMOVE_DISTRIBUTION_LIST_MEMBER_REQUEST, new RemoveDistributionListMember());
-    dispatcher.registerHandler(AdminConstants.MODIFY_DISTRIBUTION_LIST_REQUEST, new ModifyDistributionList());
-    dispatcher.registerHandler(AdminConstants.DELETE_DISTRIBUTION_LIST_REQUEST, new DeleteDistributionList());
-    dispatcher.registerHandler(AdminConstants.ADD_DISTRIBUTION_LIST_ALIAS_REQUEST, new AddDistributionListAlias());
-    dispatcher.registerHandler(AdminConstants.REMOVE_DISTRIBUTION_LIST_ALIAS_REQUEST, new RemoveDistributionListAlias());
-    dispatcher.registerHandler(AdminConstants.RENAME_DISTRIBUTION_LIST_REQUEST, new RenameDistributionList());
-    dispatcher.registerHandler(AdminConstants.GET_DISTRIBUTION_LIST_MEMBERSHIP_REQUEST, new GetDistributionListMembership());
-
-    dispatcher.registerHandler(AdminConstants.GET_VERSION_INFO_REQUEST, new GetVersionInfo());
-
-    dispatcher.registerHandler(AdminConstants.REINDEX_REQUEST, new ReIndex());
-
-    // zimlet
-    dispatcher.registerHandler(AdminConstants.GET_ZIMLET_REQUEST, new GetZimlet());
-    dispatcher.registerHandler(AdminConstants.CREATE_ZIMLET_REQUEST, new CreateZimlet());
-    dispatcher.registerHandler(AdminConstants.DELETE_ZIMLET_REQUEST, new DeleteZimlet());
-    dispatcher.registerHandler(AdminConstants.GET_ADMIN_EXTENSION_ZIMLETS_REQUEST, new GetAdminExtensionZimlets());
-    dispatcher.registerHandler(AdminConstants.GET_ZIMLET_STATUS_REQUEST, new GetZimletStatus());
-    dispatcher.registerHandler(AdminConstants.GET_ALL_ZIMLETS_REQUEST, new GetAllZimlets());
-    dispatcher.registerHandler(AdminConstants.DEPLOY_ZIMLET_REQUEST, new DeployZimlet());
-    dispatcher.registerHandler(AdminConstants.UNDEPLOY_ZIMLET_REQUEST, new UndeployZimlet());
-    dispatcher.registerHandler(AdminConstants.CONFIGURE_ZIMLET_REQUEST, new ConfigureZimlet());
-    dispatcher.registerHandler(AdminConstants.MODIFY_ZIMLET_REQUEST, new ModifyZimlet());
-    dispatcher.registerHandler(AdminConstants.DUMP_SESSIONS_REQUEST, new DumpSessions());
-    dispatcher.registerHandler(AdminConstants.GET_SESSIONS_REQUEST, new GetSessions());
-
-    // calendar resources
-    dispatcher.registerHandler(AdminConstants.CREATE_CALENDAR_RESOURCE_REQUEST,   new CreateCalendarResource());
-    dispatcher.registerHandler(AdminConstants.DELETE_CALENDAR_RESOURCE_REQUEST,   new DeleteCalendarResource());
-    dispatcher.registerHandler(AdminConstants.MODIFY_CALENDAR_RESOURCE_REQUEST,   new ModifyCalendarResource());
-    dispatcher.registerHandler(AdminConstants.RENAME_CALENDAR_RESOURCE_REQUEST,   new RenameCalendarResource());
-    dispatcher.registerHandler(AdminConstants.GET_CALENDAR_RESOURCE_REQUEST,      new GetCalendarResource());
-    dispatcher.registerHandler(AdminConstants.GET_ALL_CALENDAR_RESOURCES_REQUEST, new GetAllCalendarResources());
-    dispatcher.registerHandler(AdminConstants.SEARCH_CALENDAR_RESOURCES_REQUEST,  new SearchCalendarResources());
-
-    // QUOTA
-    dispatcher.registerHandler(AdminConstants.GET_QUOTA_USAGE_REQUEST, new GetQuotaUsage());
-
-    // Mail queue management
-    dispatcher.registerHandler(AdminConstants.GET_MAIL_QUEUE_INFO_REQUEST, new GetMailQueueInfo());
-    dispatcher.registerHandler(AdminConstants.GET_MAIL_QUEUE_REQUEST, new GetMailQueue());
-    dispatcher.registerHandler(AdminConstants.MAIL_QUEUE_ACTION_REQUEST, new MailQueueAction());
-    dispatcher.registerHandler(AdminConstants.MAIL_QUEUE_FLUSH_REQUEST, new MailQueueFlush());
-
-    dispatcher.registerHandler(AdminConstants.INIT_NOTEBOOK_REQUEST, new InitNotebook());
-
-    dispatcher.registerHandler(AdminConstants.AUTO_COMPLETE_GAL_REQUEST, new AutoCompleteGal());
-    dispatcher.registerHandler(AdminConstants.SEARCH_GAL_REQUEST, new SearchGal());
-
-    // throttling
-    dispatcher.registerHandler(AdminConstants.SET_THROTTLE_REQUEST, new SetThrottle());
-
-    // data source
-    dispatcher.registerHandler(AdminConstants.GET_DATA_SOURCES_REQUEST, new GetDataSources());
-    dispatcher.registerHandler(AdminConstants.CREATE_DATA_SOURCE_REQUEST, new CreateDataSource());
-    dispatcher.registerHandler(AdminConstants.MODIFY_DATA_SOURCE_REQUEST, new ModifyDataSource());
-    dispatcher.registerHandler(AdminConstants.DELETE_DATA_SOURCE_REQUEST, new DeleteDataSource());
-
-    // calendar time zone fixup
-    dispatcher.registerHandler(AdminConstants.FIX_CALENDAR_TIME_ZONE_REQUEST, new FixCalendarTimeZone());
-
-    // admin saved searches
-    dispatcher.registerHandler(AdminConstants.GET_ADMIN_SAVED_SEARCHES_REQUEST, new GetAdminSavedSearches());
-    dispatcher.registerHandler(AdminConstants.MODIFY_ADMIN_SAVED_SEARCHES_REQUEST, new ModifyAdminSavedSearches());
-
-    dispatcher.registerHandler(AdminConstants.ADD_ACCOUNT_LOGGER_REQUEST, new AddAccountLogger());
-    dispatcher.registerHandler(AdminConstants.REMOVE_ACCOUNT_LOGGER_REQUEST, new RemoveAccountLogger());
-    dispatcher.registerHandler(AdminConstants.GET_ACCOUNT_LOGGERS_REQUEST, new GetAccountLoggers());
-    dispatcher.registerHandler(AdminConstants.GET_ALL_ACCOUNT_LOGGERS_REQUEST, new GetAllAccountLoggers());
-
-    dispatcher.registerHandler(AdminConstants.CHECK_DIRECTORY_REQUEST, new CheckDirectory());
-
-    dispatcher.registerHandler(AdminConstants.FLUSH_CACHE_REQUEST, new FlushCache());
-     */
-
     public static void run(SoapTestContext ctx) throws Exception {
       SoapCommands cmds = new SoapCommands(ctx);
 
@@ -1484,11 +1359,7 @@ public class TestProvDomainStatus {
                   : " (cause: " + cause.getClass().getName() + " " + cause.getMessage() + ")"));
       e.printStackTrace(System.out);
       System.out.println("\nTest " + TEST_ID + " failed!");
-    } /* catch (AssertionFailedError e) {
-          System.out.println("\n===== assertion failed =====");
-          System.out.println(e.getMessage());
-          e.printStackTrace(System.out);
-      } */
+    }
   }
 }
 
