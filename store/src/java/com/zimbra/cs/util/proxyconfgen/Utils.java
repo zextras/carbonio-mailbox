1fae9b3b3147c7d067c283333096a8f47d6721da
Keshav Bhatt <keshavnrj@gmail.com>
2022-11-23 19:58:18 +0530

    fix:[CO-457] delete orphaned certificate key pair from domain cert dir

--- store/src/java/com/zimbra/cs/util/proxyconfgen/Utils.java
+++ store/src/java/com/zimbra/cs/util/proxyconfgen/Utils.java
@@ -3,11 +3,16 @@ package com.zimbra.cs.util.proxyconfgen;
 import com.zimbra.common.account.ZAttrProvisioning;
 import com.zimbra.cs.account.Server;
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 import java.util.TreeSet;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public final class Utils {
 
@@ -48,5 +53,40 @@ public final class Utils {
     if (!directory.exists() && !directory.mkdirs()) {
       throw new ProxyConfException("Unable to create folder in " + folderPath);
     }
+  }
+
+  /**
+   * Delete a file specified by the given path
+   *
+   * @param filePath path of file to delete
+   * @author Keshav Bhatt
+   * @since 22.12.0
+   */
+  public static void deleteFileIfExists(final String filePath) {
+    File file = new File(filePath);
+    if (file.exists()) {
+      try {
+        Files.delete(file.toPath());
+      } catch (final IOException ignored) {
+        // ignore
+      }
+    }
+  }
+
+  /**
+   * get file paths from the given directory path
+   *
+   * @param directoryPath path to get directory
+   * @return String List of file paths
+   * @author Keshav Bhatt
+   * @since 22.12.0
+   */
+  public static List<String> getFilesPathInDirectory(final String directoryPath) {
+    try (Stream<Path> paths = Files.walk(Paths.get(directoryPath), 1)) {
+      return paths.filter(Files::isRegularFile).map(Path::toString).collect(Collectors.toList());
+    } catch (IOException ignored) {
+      // ignore
+    }
+    return List.of();
   }
 }
