1be6ffa5e0220897ab55c75e407d0fb3caedc0fe
Davide Frison <davide.frison@zextras.com>
2022-11-29 16:26:39 +0100

    refactor: import pom changes from feature/maven-build branch

---
+++ store/utils/docker/carbonio-db/scripts/02-update_config_db_and_index_version.sql
@@ -0,0 +1,14 @@
+-- SPDX-FileCopyrightText: 2021 Synacor, Inc.
+-- SPDX-FileCopyrightText: 2021 Zextras <https://www.zextras.com>
+--
+-- SPDX-License-Identifier: AGPL-3.0-only
+-- SPDX-License-Identifier: GPL-2.0-only
+
+-- This script updates db and index version values in order to make the mailbox start
+-- See Versions.java file for reference on current accepted values
+
+USE zimbra;
+
+INSERT INTO config (name, value, description, modified)
+       VALUES ("db.version", "111", "DB version", CURRENT_TIMESTAMP()),
+               ("index.version", "2", "Index version", CURRENT_TIMESTAMP());
