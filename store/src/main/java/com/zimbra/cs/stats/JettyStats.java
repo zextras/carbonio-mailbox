3825f253cf242cd772ad0b978faaf21143981c7f
Davide Frison <davide.frison@zextras.com>
2022-11-29 16:16:57 +0100

    refactor: move java -> main/java, java-test -> test/java

---
+++ store/src/main/java/com/zimbra/cs/stats/JettyStats.java
@@ -0,0 +1,38 @@
+// SPDX-FileCopyrightText: 2022 Synacor, Inc.
+// SPDX-FileCopyrightText: 2022 Zextras <https://www.zextras.com>
+//
+// SPDX-License-Identifier: GPL-2.0-only
+
+package com.zimbra.cs.stats;
+
+import java.util.Map;
+
+import org.eclipse.jetty.util.thread.ThreadPool;
+
+import com.google.common.collect.Maps;
+import com.zimbra.common.jetty.JettyMonitor;
+import com.zimbra.common.stats.RealtimeStatsCallback;
+import com.zimbra.common.util.Log;
+import com.zimbra.common.util.LogFactory;
+
+/**
+ * Returns stats about Jetty threads and connections.
+ */
+public class JettyStats
+implements RealtimeStatsCallback {
+
+    private static final Log log = LogFactory.getLog(JettyStats.class);
+    
+    @Override
+    public Map<String, Object> getStatData() {
+        ThreadPool pool = JettyMonitor.getThreadPool();
+        if (pool == null) {
+            log.debug("Thread pool has not been initialized.  Not returning stat data.");
+            return null;
+        }
+        Map<String, Object> data = Maps.newHashMap();
+        data.put(ZimbraPerf.RTS_HTTP_THREADS, pool.getThreads());
+        data.put(ZimbraPerf.RTS_HTTP_IDLE_THREADS, pool.getIdleThreads());
+        return data;
+    }
+}
