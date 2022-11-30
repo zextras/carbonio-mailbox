4e119e1104ca1480081cd30fadb831983eba84ee
Davide Frison <davide.frison@zextras.com>
2022-11-30 10:05:59 +0100

    fix: MimeTest carriage return

--- store/src/test/java/com/zimbra/cs/mime/MimeTest.java
+++ store/src/test/java/com/zimbra/cs/mime/MimeTest.java
@@ -47,9 +47,11 @@ public class MimeTest {
     private void testCP932(String contentType) throws IOException {
         Reader reader = Mime.getTextReader(getClass().getResourceAsStream("cp932.txt"), contentType, null);
         String result = IOUtils.toString(reader);
-        Assert.assertTrue(result
-                        .equals("2010/4/2,\u2161\u53f7\u5e97  \u30ab\u30aa\u30b9\u9928,\u3054\u672c\u4eba,1\u56de\u6255\u3044,,'10/05,9960,9960,,,,,\r\n"));
-    }
+        Assert.assertTrue(
+        result.equals(
+            "2010/4/2,\u2161\u53f7\u5e97 "
+                + " \u30ab\u30aa\u30b9\u9928,\u3054\u672c\u4eba,1\u56de\u6255\u3044,,'10/05,9960,9960,,,,," + System.lineSeparator()));
+  }
 
     @Test
     public void getTextReader() throws Exception {
