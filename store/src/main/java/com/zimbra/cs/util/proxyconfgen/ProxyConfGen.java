13d846e65cce347180e234fa5ef37e9164b60614
Davide Frison <davide.frison@zextras.com>
2023-02-02 16:50:05 +0100

    chore: do not expand docs.upstream template
    * ProxyConfGen remove docs.upstream expansion

--- store/src/main/java/com/zimbra/cs/util/proxyconfgen/ProxyConfGen.java
+++ store/src/main/java/com/zimbra/cs/util/proxyconfgen/ProxyConfGen.java
@@ -2373,9 +2373,6 @@ public class ProxyConfGen {
           new File(mTemplateDir, getWebHttpSModeConfTemplate("mixed")),
           new File(mConfIncludesDir, getWebHttpSModeConf("mixed")));
       expandTemplate(
-          new File(mTemplateDir, getConfTemplateFileName("docs.upstream")),
-          new File(mConfIncludesDir, getConfFileName("docs.upstream")));
-      expandTemplate(
           new File(mTemplateDir, getConfTemplateFileName("web.carbonio.admin.default")),
           new File(mConfIncludesDir, getConfFileName("web.carbonio.admin.default")));
       expandTemplate(
