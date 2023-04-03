8e7c69aa1588ff5d0967108167ede2d50e15daa5
Davide Polonio <davide.polonio@zextras.com>
2023-04-03 09:40:59 +0200

    feat(CO-626): remove LDAP attributes for Briefcase (#188)

--- client/src/main/java/com/zimbra/client/ZFeatures.java
+++ client/src/main/java/com/zimbra/client/ZFeatures.java
@@ -5,139 +5,226 @@
 
 package com.zimbra.client;
 
-import java.util.Collection;
-import java.util.Map;
-
 import com.google.common.collect.Iterables;
 import com.zimbra.common.account.ProvisioningConstants;
 import com.zimbra.common.account.ZAttrProvisioning;
+import java.util.Collection;
+import java.util.Map;
 
 public class ZFeatures {
 
-    private Map<String, Collection<String>> mAttrs;
+  private Map<String, Collection<String>> mAttrs;
 
-    public ZFeatures(Map<String, Collection<String>> attrs) {
-        mAttrs = attrs;
+  public ZFeatures(Map<String, Collection<String>> attrs) {
+    mAttrs = attrs;
+  }
+
+  /**
+   * @param name name of attr to get
+   * @return null if unset, or first value in list
+   */
+  private String get(String name) {
+    Collection<String> value = mAttrs.get(name);
+    if (value == null || value.isEmpty()) {
+      return null;
     }
+    return Iterables.get(value, 0);
+  }
 
-    /**
-     * @param name name of attr to get
-     * @return null if unset, or first value in list
-     */
-    private String get(String name) {
-        Collection<String> value = mAttrs.get(name);
-        if (value == null || value.isEmpty()) {
-            return null;
-        }
-        return Iterables.get(value, 0);
+  public boolean getBool(String name) {
+    return ProvisioningConstants.TRUE.equals(get(name));
+  }
 
-    }
+  public Map<String, Collection<String>> getAttrs() {
+    return mAttrs;
+  }
 
-    public boolean getBool(String name) {
-        return ProvisioningConstants.TRUE.equals(get(name));
-    }
+  public boolean getTouchClient() {
+    return getBool(ZAttrProvisioning.A_zimbraFeatureTouchClientEnabled);
+  }
 
-    public Map<String, Collection<String>> getAttrs() { return mAttrs; }
-    
-    public boolean getTouchClient() { return getBool(ZAttrProvisioning.A_zimbraFeatureTouchClientEnabled); }
+  public boolean getContacts() {
+    return getBool(ZAttrProvisioning.A_zimbraFeatureContactsEnabled);
+  }
 
-    public boolean getContacts() { return getBool(ZAttrProvisioning.A_zimbraFeatureContactsEnabled); }
+  public boolean getMail() {
+    return getBool(ZAttrProvisioning.A_zimbraFeatureMailEnabled);
+  }
 
-    public boolean getMail() { return getBool(ZAttrProvisioning.A_zimbraFeatureMailEnabled); }
+  public boolean getAdminMail() {
+    return getBool(ZAttrProvisioning.A_zimbraFeatureAdminMailEnabled);
+  }
 
-    public boolean getAdminMail() {return getBool(ZAttrProvisioning.A_zimbraFeatureAdminMailEnabled); }
+  public boolean getVoice() {
+    return getBool(ZAttrProvisioning.A_zimbraFeatureVoiceEnabled);
+  }
 
-    public boolean getVoice() { return getBool(ZAttrProvisioning.A_zimbraFeatureVoiceEnabled); }
+  public boolean getCalendar() {
+    return getBool(ZAttrProvisioning.A_zimbraFeatureCalendarEnabled);
+  }
 
-    public boolean getCalendar() { return getBool(ZAttrProvisioning.A_zimbraFeatureCalendarEnabled); }
+  public boolean getCalendarUpsell() {
+    return getBool(ZAttrProvisioning.A_zimbraFeatureCalendarUpsellEnabled);
+  }
 
-    public boolean getCalendarUpsell() { return getBool(ZAttrProvisioning.A_zimbraFeatureCalendarUpsellEnabled); }
+  public String getCalendarUpsellURL() {
+    return get(ZAttrProvisioning.A_zimbraFeatureCalendarUpsellURL);
+  }
 
-    public String getCalendarUpsellURL() { return get(ZAttrProvisioning.A_zimbraFeatureCalendarUpsellURL); }    
+  public boolean getTasks() {
+    return getBool(ZAttrProvisioning.A_zimbraFeatureTasksEnabled);
+  }
 
-    public boolean getTasks() { return getBool(ZAttrProvisioning.A_zimbraFeatureTasksEnabled); }
+  public boolean getTagging() {
+    return getBool(ZAttrProvisioning.A_zimbraFeatureTaggingEnabled);
+  }
 
-    public boolean getTagging() { return getBool(ZAttrProvisioning.A_zimbraFeatureTaggingEnabled); }
+  public boolean getOptions() {
+    return getBool(ZAttrProvisioning.A_zimbraFeatureOptionsEnabled);
+  }
 
-    public boolean getOptions() { return getBool(ZAttrProvisioning.A_zimbraFeatureOptionsEnabled); }
-    
-    public boolean getAdvancedSearch() { return getBool(ZAttrProvisioning.A_zimbraFeatureAdvancedSearchEnabled); }
+  public boolean getAdvancedSearch() {
+    return getBool(ZAttrProvisioning.A_zimbraFeatureAdvancedSearchEnabled);
+  }
 
-    public boolean getSavedSearches() { return getBool(ZAttrProvisioning.A_zimbraFeatureSavedSearchesEnabled); }
+  public boolean getSavedSearches() {
+    return getBool(ZAttrProvisioning.A_zimbraFeatureSavedSearchesEnabled);
+  }
 
-    public boolean getConversations() { return getBool(ZAttrProvisioning.A_zimbraFeatureConversationsEnabled); }
+  public boolean getConversations() {
+    return getBool(ZAttrProvisioning.A_zimbraFeatureConversationsEnabled);
+  }
 
-    public boolean getChangePassword() { return getBool(ZAttrProvisioning.A_zimbraFeatureChangePasswordEnabled); }
+  public boolean getChangePassword() {
+    return getBool(ZAttrProvisioning.A_zimbraFeatureChangePasswordEnabled);
+  }
 
-    public boolean getInitialSearchPreference() { return getBool(ZAttrProvisioning.A_zimbraFeatureInitialSearchPreferenceEnabled); }
+  public boolean getInitialSearchPreference() {
+    return getBool(ZAttrProvisioning.A_zimbraFeatureInitialSearchPreferenceEnabled);
+  }
 
-    public boolean getFilters() { return getBool(ZAttrProvisioning.A_zimbraFeatureFiltersEnabled); }
+  public boolean getFilters() {
+    return getBool(ZAttrProvisioning.A_zimbraFeatureFiltersEnabled);
+  }
 
-    public boolean getGal() { return getBool(ZAttrProvisioning.A_zimbraFeatureGalEnabled); }
+  public boolean getGal() {
+    return getBool(ZAttrProvisioning.A_zimbraFeatureGalEnabled);
+  }
 
-    public boolean getHtmlCompose() { return getBool(ZAttrProvisioning.A_zimbraFeatureHtmlComposeEnabled); }
+  public boolean getHtmlCompose() {
+    return getBool(ZAttrProvisioning.A_zimbraFeatureHtmlComposeEnabled);
+  }
 
-    public boolean getIM() { return getBool(ZAttrProvisioning.A_zimbraFeatureIMEnabled); }
+  public boolean getIM() {
+    return getBool(ZAttrProvisioning.A_zimbraFeatureIMEnabled);
+  }
 
-    public boolean getViewInHtml() { return getBool(ZAttrProvisioning.A_zimbraFeatureViewInHtmlEnabled); }
+  public boolean getViewInHtml() {
+    return getBool(ZAttrProvisioning.A_zimbraFeatureViewInHtmlEnabled);
+  }
 
-    public boolean getSharing() { return getBool(ZAttrProvisioning.A_zimbraFeatureSharingEnabled); }
+  public boolean getSharing() {
+    return getBool(ZAttrProvisioning.A_zimbraFeatureSharingEnabled);
+  }
 
-    public boolean getMailForwarding() { return getBool(ZAttrProvisioning.A_zimbraFeatureMailForwardingEnabled); }
+  public boolean getMailForwarding() {
+    return getBool(ZAttrProvisioning.A_zimbraFeatureMailForwardingEnabled);
+  }
 
-    public boolean getMailForwardingInFilter() { return getBool(ZAttrProvisioning.A_zimbraFeatureMailForwardingInFiltersEnabled); }
+  public boolean getMailForwardingInFilter() {
+    return getBool(ZAttrProvisioning.A_zimbraFeatureMailForwardingInFiltersEnabled);
+  }
 
-    public boolean getMobileSync() { return getBool(ZAttrProvisioning.A_zimbraFeatureMobileSyncEnabled); }
+  public boolean getMobileSync() {
+    return getBool(ZAttrProvisioning.A_zimbraFeatureMobileSyncEnabled);
+  }
 
-    public boolean getSkinChange() { return getBool(ZAttrProvisioning.A_zimbraFeatureSkinChangeEnabled); }
+  public boolean getSkinChange() {
+    return getBool(ZAttrProvisioning.A_zimbraFeatureSkinChangeEnabled);
+  }
 
-    public boolean getNotebook() { return false; } //bug:56196 getBool(ZAttrProvisioning.A_zimbraFeatureNotebookEnabled);
+  public boolean getNotebook() {
+    return false;
+  } // bug:56196 getBool(ZAttrProvisioning.A_zimbraFeatureNotebookEnabled);
 
-    public boolean getBriefcases() { return getBool(ZAttrProvisioning.A_zimbraFeatureBriefcasesEnabled); }
+  public boolean getGalAutoComplete() {
+    return getBool(ZAttrProvisioning.A_zimbraFeatureGalAutoCompleteEnabled);
+  }
 
-    public boolean getGalAutoComplete() { return getBool(ZAttrProvisioning.A_zimbraFeatureGalAutoCompleteEnabled); }
+  public boolean getOutOfOfficeReply() {
+    return getBool(ZAttrProvisioning.A_zimbraFeatureOutOfOfficeReplyEnabled);
+  }
 
-    public boolean getOutOfOfficeReply() { return getBool(ZAttrProvisioning.A_zimbraFeatureOutOfOfficeReplyEnabled); }
+  public boolean getNewMailNotification() {
+    return getBool(ZAttrProvisioning.A_zimbraFeatureNewMailNotificationEnabled);
+  }
 
-    public boolean getNewMailNotification() { return getBool(ZAttrProvisioning.A_zimbraFeatureNewMailNotificationEnabled); }
+  public boolean getIdentities() {
+    return getBool(ZAttrProvisioning.A_zimbraFeatureIdentitiesEnabled);
+  }
 
-    public boolean getIdentities() { return getBool(ZAttrProvisioning.A_zimbraFeatureIdentitiesEnabled); }
+  public boolean getPop3DataSource() {
+    return getBool(ZAttrProvisioning.A_zimbraFeaturePop3DataSourceEnabled);
+  }
 
-    public boolean getPop3DataSource() { return getBool(ZAttrProvisioning.A_zimbraFeaturePop3DataSourceEnabled); }
-    
-    public boolean getGroupcalendarEnabled() { return getBool(ZAttrProvisioning.A_zimbraFeatureGroupCalendarEnabled); }
+  public boolean getGroupcalendarEnabled() {
+    return getBool(ZAttrProvisioning.A_zimbraFeatureGroupCalendarEnabled);
+  }
 
-    public boolean getDataSourceImportOnLogin() { return getBool(ZAttrProvisioning.A_zimbraDataSourceImportOnLogin); }
+  public boolean getDataSourceImportOnLogin() {
+    return getBool(ZAttrProvisioning.A_zimbraDataSourceImportOnLogin);
+  }
 
-    public boolean getFlagging() { return getBool(ZAttrProvisioning.A_zimbraFeatureFlaggingEnabled); }
+  public boolean getFlagging() {
+    return getBool(ZAttrProvisioning.A_zimbraFeatureFlaggingEnabled);
+  }
 
-    public boolean getMailPriority() { return getBool(ZAttrProvisioning.A_zimbraFeatureMailPriorityEnabled); }
+  public boolean getMailPriority() {
+    return getBool(ZAttrProvisioning.A_zimbraFeatureMailPriorityEnabled);
+  }
 
-    public boolean getPortalEnabled() { return getBool(ZAttrProvisioning.A_zimbraFeaturePortalEnabled); }
+  public boolean getPortalEnabled() {
+    return getBool(ZAttrProvisioning.A_zimbraFeaturePortalEnabled);
+  }
 
-    public boolean getContactsDetailedSearch() { return getBool(ZAttrProvisioning.A_zimbraFeatureContactsDetailedSearchEnabled); }
+  public boolean getContactsDetailedSearch() {
+    return getBool(ZAttrProvisioning.A_zimbraFeatureContactsDetailedSearchEnabled);
+  }
 
-    public boolean getDiscardFilterEnabled() { return getBool(ZAttrProvisioning.A_zimbraFeatureDiscardInFiltersEnabled); }
-    
-    // defaults to TRUE
-    public boolean getWebSearchEnabled() { return get(ZAttrProvisioning.A_zimbraFeatureWebSearchEnabled) == null ||
-    											  getBool(ZAttrProvisioning.A_zimbraFeatureWebSearchEnabled); }
+  public boolean getDiscardFilterEnabled() {
+    return getBool(ZAttrProvisioning.A_zimbraFeatureDiscardInFiltersEnabled);
+  }
 
-    // defaults to TRUE
-    public boolean getWebClientShowOfflineLink() { return get(ZAttrProvisioning.A_zimbraWebClientShowOfflineLink) == null ||
-                                                          getBool(ZAttrProvisioning.A_zimbraWebClientShowOfflineLink); }
+  // defaults to TRUE
+  public boolean getWebSearchEnabled() {
+    return get(ZAttrProvisioning.A_zimbraFeatureWebSearchEnabled) == null
+        || getBool(ZAttrProvisioning.A_zimbraFeatureWebSearchEnabled);
+  }
 
-	// defaults to TRUE
-	public boolean getNewAddrBookEnabled() { return get(ZAttrProvisioning.A_zimbraFeatureNewAddrBookEnabled) == null ||
-													getBool(ZAttrProvisioning.A_zimbraFeatureNewAddrBookEnabled); }
-	// defaults to TRUE
-	public boolean getPop3Enabled() { return	get(ZAttrProvisioning.A_zimbraPop3Enabled) == null ||
-												getBool(ZAttrProvisioning.A_zimbraPop3Enabled); }
-	// defaults to TRUE
-	public boolean getSpam() { return	get(ZAttrProvisioning.A_zimbraFeatureAntispamEnabled) == null ||
-												getBool(ZAttrProvisioning.A_zimbraFeatureAntispamEnabled); }
-	// defaults to TRUE
-	public boolean getWebClientEnabled() { return	get(ZAttrProvisioning.A_zimbraFeatureWebClientEnabled) == null ||
-												getBool(ZAttrProvisioning.A_zimbraFeatureWebClientEnabled); }
+  // defaults to TRUE
+  public boolean getWebClientShowOfflineLink() {
+    return get(ZAttrProvisioning.A_zimbraWebClientShowOfflineLink) == null
+        || getBool(ZAttrProvisioning.A_zimbraWebClientShowOfflineLink);
+  }
+
+  // defaults to TRUE
+  public boolean getNewAddrBookEnabled() {
+    return get(ZAttrProvisioning.A_zimbraFeatureNewAddrBookEnabled) == null
+        || getBool(ZAttrProvisioning.A_zimbraFeatureNewAddrBookEnabled);
+  }
+  // defaults to TRUE
+  public boolean getPop3Enabled() {
+    return get(ZAttrProvisioning.A_zimbraPop3Enabled) == null
+        || getBool(ZAttrProvisioning.A_zimbraPop3Enabled);
+  }
+  // defaults to TRUE
+  public boolean getSpam() {
+    return get(ZAttrProvisioning.A_zimbraFeatureAntispamEnabled) == null
+        || getBool(ZAttrProvisioning.A_zimbraFeatureAntispamEnabled);
+  }
+  // defaults to TRUE
+  public boolean getWebClientEnabled() {
+    return get(ZAttrProvisioning.A_zimbraFeatureWebClientEnabled) == null
+        || getBool(ZAttrProvisioning.A_zimbraFeatureWebClientEnabled);
+  }
 }
-
--- store/src/main/java/com/zimbra/cs/account/ldap/upgrade/BUG_50465.java
+++ store/src/main/java/com/zimbra/cs/account/ldap/upgrade/BUG_50465.java
@@ -5,8 +5,6 @@
 
 package com.zimbra.cs.account.ldap.upgrade;
 
-import java.util.Map;
-
 import com.zimbra.common.service.ServiceException;
 import com.zimbra.cs.account.Provisioning;
 import com.zimbra.cs.ldap.IAttributes;
@@ -15,95 +13,85 @@ import com.zimbra.cs.ldap.LdapConstants;
 import com.zimbra.cs.ldap.LdapServerType;
 import com.zimbra.cs.ldap.LdapUsage;
 import com.zimbra.cs.ldap.SearchLdapOptions;
+import com.zimbra.cs.ldap.SearchLdapOptions.SearchLdapVisitor;
 import com.zimbra.cs.ldap.ZLdapContext;
 import com.zimbra.cs.ldap.ZLdapFilterFactory;
 import com.zimbra.cs.ldap.ZMutableEntry;
-import com.zimbra.cs.ldap.SearchLdapOptions.SearchLdapVisitor;
+import java.util.Map;
 
 public class BUG_50465 extends UpgradeOp {
+  private static final String ATTR_NOTEBOOK = Provisioning.A_zimbraFeatureNotebookEnabled;
 
-    private static String ATTR_SPREADSHEET = Provisioning.A_zimbraFeatureBriefcaseSpreadsheetEnabled;
-    private static String ATTR_SLIDES = Provisioning.A_zimbraFeatureBriefcaseSlidesEnabled;
-    private static String ATTR_NOTEBOOK = Provisioning.A_zimbraFeatureNotebookEnabled;
-    
+  @Override
+  void doUpgrade() throws ServiceException {
+    ZLdapContext zlc = LdapClient.getContext(LdapServerType.MASTER, LdapUsage.UPGRADE);
+    try {
+      doCos(zlc);
+      doAccount(zlc);
+    } finally {
+      LdapClient.closeContext(zlc);
+    }
+  }
+
+  private static class Bug50465Visitor extends SearchLdapVisitor {
+    private UpgradeOp upgradeOp;
+    private ZLdapContext modZlc;
+
+    private Bug50465Visitor(UpgradeOp upgradeOp, ZLdapContext modZlc) {
+      this.upgradeOp = upgradeOp;
+      this.modZlc = modZlc;
+    }
+
     @Override
-    void doUpgrade() throws ServiceException {
-        ZLdapContext zlc = LdapClient.getContext(LdapServerType.MASTER, LdapUsage.UPGRADE);
-        try {
-            doCos(zlc);
-            doAccount(zlc);
-        } finally {
-            LdapClient.closeContext(zlc);
-        }
-    }
-    
-    private static class Bug50465Visitor extends SearchLdapVisitor {
-        private UpgradeOp upgradeOp;
-        private ZLdapContext modZlc;
-        
-        private Bug50465Visitor(UpgradeOp upgradeOp, ZLdapContext modZlc) {
-            this.upgradeOp = upgradeOp;
-            this.modZlc = modZlc;
-        }
-        
-        @Override
-        public void visit(String dn, Map<String, Object> attrs, IAttributes ldapAttrs) {
-            ZMutableEntry entry = LdapClient.createMutableEntry();
-            
-            try {
-                if (ldapAttrs.getAttrString(ATTR_SPREADSHEET) != null)
-                    entry.setAttr(ATTR_SPREADSHEET, LdapConstants.LDAP_FALSE);
-                
-                if (ldapAttrs.getAttrString(ATTR_SLIDES) != null)
-                    entry.setAttr(ATTR_SLIDES, LdapConstants.LDAP_FALSE);
-                
-                if (ldapAttrs.getAttrString(ATTR_NOTEBOOK) != null)
-                    entry.setAttr(ATTR_NOTEBOOK, LdapConstants.LDAP_FALSE);
-                
-                upgradeOp.printer.println("Modifying " + dn);
-                upgradeOp.replaceAttrs(modZlc, dn, entry);
-                
-            } catch (ServiceException e) {
-                // log and continue
-                upgradeOp.printer.println("Caught ServiceException while modifying " + dn);
-                upgradeOp.printer.printStackTrace(e);
-            }
-        }
-    }
-    
-    private void upgrade(ZLdapContext modZlc, String bases[], String query) {
-        SearchLdapOptions.SearchLdapVisitor visitor = new Bug50465Visitor(this, modZlc);
+    public void visit(String dn, Map<String, Object> attrs, IAttributes ldapAttrs) {
+      ZMutableEntry entry = LdapClient.createMutableEntry();
 
-        String attrs[] = new String[] {ATTR_SPREADSHEET, ATTR_SLIDES, ATTR_NOTEBOOK};
-        
-        for (String base : bases) {
-            try {
-                prov.searchLdapOnMaster(base, query, attrs, visitor);
-            } catch (ServiceException e) {
-                // log and continue
-                printer.println("Caught ServiceException while searching " + query + " under base " + base);
-                printer.printStackTrace(e);
-            }
-        }
-    }
-    
-    private String query() {
-        return "(|(" + ATTR_SPREADSHEET + "=" + LdapConstants.LDAP_TRUE + ")" + 
-                 "(" + ATTR_SLIDES + "=" + LdapConstants.LDAP_TRUE + ")" + 
-                 "(" + ATTR_NOTEBOOK + "=" + LdapConstants.LDAP_TRUE + ")" +
-               ")";
-    }
-    
-    private void doCos(ZLdapContext modZlc) {
-        String bases[] = prov.getDIT().getSearchBases(Provisioning.SD_COS_FLAG);
-        String query = "(&" + ZLdapFilterFactory.getInstance().allCoses().toFilterString() + query() + ")";
-        upgrade(modZlc, bases, query);
-    }
-    
-    private void doAccount(ZLdapContext modZlc) {
-        String bases[] = prov.getDIT().getSearchBases(Provisioning.SD_ACCOUNT_FLAG);
-        String query = "(&" + ZLdapFilterFactory.getInstance().allAccounts().toFilterString() + query() + ")";
-        upgrade(modZlc, bases, query);
-    }
+      try {
+        if (ldapAttrs.getAttrString(ATTR_NOTEBOOK) != null)
+          entry.setAttr(ATTR_NOTEBOOK, LdapConstants.LDAP_FALSE);
 
+        upgradeOp.printer.println("Modifying " + dn);
+        upgradeOp.replaceAttrs(modZlc, dn, entry);
+
+      } catch (ServiceException e) {
+        // log and continue
+        upgradeOp.printer.println("Caught ServiceException while modifying " + dn);
+        upgradeOp.printer.printStackTrace(e);
+      }
+    }
+  }
+
+  private void upgrade(ZLdapContext modZlc, String bases[], String query) {
+    SearchLdapOptions.SearchLdapVisitor visitor = new Bug50465Visitor(this, modZlc);
+
+    String attrs[] = new String[] {ATTR_NOTEBOOK};
+
+    for (String base : bases) {
+      try {
+        prov.searchLdapOnMaster(base, query, attrs, visitor);
+      } catch (ServiceException e) {
+        // log and continue
+        printer.println("Caught ServiceException while searching " + query + " under base " + base);
+        printer.printStackTrace(e);
+      }
+    }
+  }
+
+  private String query() {
+    return "(|(" + ATTR_NOTEBOOK + "=" + LdapConstants.LDAP_TRUE + "))";
+  }
+
+  private void doCos(ZLdapContext modZlc) {
+    String bases[] = prov.getDIT().getSearchBases(Provisioning.SD_COS_FLAG);
+    String query =
+        "(&" + ZLdapFilterFactory.getInstance().allCoses().toFilterString() + query() + ")";
+    upgrade(modZlc, bases, query);
+  }
+
+  private void doAccount(ZLdapContext modZlc) {
+    String bases[] = prov.getDIT().getSearchBases(Provisioning.SD_ACCOUNT_FLAG);
+    String query =
+        "(&" + ZLdapFilterFactory.getInstance().allAccounts().toFilterString() + query() + ")";
+    upgrade(modZlc, bases, query);
+  }
 }
