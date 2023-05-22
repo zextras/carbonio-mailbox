43399fec792f7422e63c1fbac1d8a87af4561ad8
Keshav Bhatt <keshavnrj@gmail.com>
2023-05-22 20:07:30 +0530

    chore: replace ternary op with if else

--- store/src/main/java/com/zimbra/cs/mailbox/DeliveryOptions.java
+++ store/src/main/java/com/zimbra/cs/mailbox/DeliveryOptions.java
@@ -5,80 +5,104 @@
 
 package com.zimbra.cs.mailbox;
 
+import com.zimbra.cs.mailbox.MailItem.CustomMetadata;
 import java.util.Collection;
 
-import com.zimbra.cs.mailbox.MailItem.CustomMetadata;
-
-/**
- * Specifies options for new messages created with {@link Mailbox#addMessage}.
- */
+/** Specifies options for new messages created with {@link Mailbox#addMessage}. */
 public class DeliveryOptions {
 
-    private int mFolderId = -1;
-    private boolean mNoICal = false;
-    private int mFlags = 0;
-    private String[] mTags = null;
-    private int mConversationId = Mailbox.ID_AUTO_INCREMENT;
-    private String mRecipientEmail = ":API:";
-    private Message.DraftInfo mDraftInfo = null;
-    private CustomMetadata mCustomMetadata = null;
-    
-    public int getFolderId() { return mFolderId; }
-    public boolean getNoICal() { return mNoICal; }
-    public int getFlags() { return mFlags; }
-    public String[] getTags() { return mTags; }
-    public int getConversationId() { return mConversationId; }
-    public String getRecipientEmail() { return mRecipientEmail; }
-    public Message.DraftInfo getDraftInfo() { return mDraftInfo; }
-    public CustomMetadata getCustomMetadata() { return mCustomMetadata; }
+  private int mFolderId = -1;
+  private boolean mNoICal = false;
+  private int mFlags = 0;
+  private String[] mTags = null;
+  private int mConversationId = Mailbox.ID_AUTO_INCREMENT;
+  private String mRecipientEmail = ":API:";
+  private Message.DraftInfo mDraftInfo = null;
+  private CustomMetadata mCustomMetadata = null;
 
-    public DeliveryOptions setFolderId(int folderId) {
-        mFolderId = folderId;
-        return this;
-    }
+  public int getFolderId() {
+    return mFolderId;
+  }
 
-    public DeliveryOptions setFolderId(Folder folder) {
-        mFolderId = folder.getId();
-        return this;
-    }
-    
-    public DeliveryOptions setNoICal(boolean noICal) {
-        mNoICal = noICal;
-        return this;
-    }
-    
-    public DeliveryOptions setFlags(int flags) {
-        mFlags = flags;
-        return this;
-    }
+  public boolean getNoICal() {
+    return mNoICal;
+  }
 
-    public DeliveryOptions setTags(Collection<String> tags) {
-        mTags = tags == null ? null : tags.toArray(new String[0]);
-        return this;
-    }
+  public int getFlags() {
+    return mFlags;
+  }
 
-    public DeliveryOptions setTags(String[] tags) {
-        mTags = tags;
-        return this;
+  public String[] getTags() {
+    return mTags;
+  }
+
+  public int getConversationId() {
+    return mConversationId;
+  }
+
+  public String getRecipientEmail() {
+    return mRecipientEmail;
+  }
+
+  public Message.DraftInfo getDraftInfo() {
+    return mDraftInfo;
+  }
+
+  public CustomMetadata getCustomMetadata() {
+    return mCustomMetadata;
+  }
+
+  public DeliveryOptions setFolderId(int folderId) {
+    mFolderId = folderId;
+    return this;
+  }
+
+  public DeliveryOptions setFolderId(Folder folder) {
+    mFolderId = folder.getId();
+    return this;
+  }
+
+  public DeliveryOptions setNoICal(boolean noICal) {
+    mNoICal = noICal;
+    return this;
+  }
+
+  public DeliveryOptions setFlags(int flags) {
+    mFlags = flags;
+    return this;
+  }
+
+  public DeliveryOptions setTags(Collection<String> tags) {
+    if (tags == null) {
+      mTags = null;
+    } else {
+      mTags = tags.toArray(new String[0]);
     }
-    
-    public DeliveryOptions setConversationId(int conversationId) {
-        mConversationId = conversationId;
-        return this;
-    }
-    
-    public DeliveryOptions setRecipientEmail(String recipientEmail) {
-        mRecipientEmail = recipientEmail;
-        return this;
-    }
-    
-    public DeliveryOptions setDraftInfo(Message.DraftInfo draftInfo) {
-        mDraftInfo = draftInfo;
-        return this;
-    }
-    
-    public DeliveryOptions setCustomMetadata(CustomMetadata customMetadata) {
-        mCustomMetadata = customMetadata;
-        return this;
-    }
+    return this;
+  }
+
+  public DeliveryOptions setTags(String[] tags) {
+    mTags = tags;
+    return this;
+  }
+
+  public DeliveryOptions setConversationId(int conversationId) {
+    mConversationId = conversationId;
+    return this;
+  }
+
+  public DeliveryOptions setRecipientEmail(String recipientEmail) {
+    mRecipientEmail = recipientEmail;
+    return this;
+  }
+
+  public DeliveryOptions setDraftInfo(Message.DraftInfo draftInfo) {
+    mDraftInfo = draftInfo;
+    return this;
+  }
+
+  public DeliveryOptions setCustomMetadata(CustomMetadata customMetadata) {
+    mCustomMetadata = customMetadata;
+    return this;
+  }
 }
