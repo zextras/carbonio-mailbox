564ed76fdb45cc34b8d62d63b949ea5fdeb1a68c
Davide Frison <davide.frison@zextras.com>
2023-06-16 17:06:08 +0200

    chore: remove junit4 import

--- store/src/test/java/com/zimbra/cs/service/mail/CopyToFilesIT.java
+++ store/src/test/java/com/zimbra/cs/service/mail/CopyToFilesIT.java
@@ -1,6 +1,5 @@
 package com.zimbra.cs.service.mail;
 
-import static org.junit.Assert.assertEquals;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyInt;
