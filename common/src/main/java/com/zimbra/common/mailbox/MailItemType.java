8b6533f62f19dddef2bccbaec2bd14764f6c9192
Yuliya Aheeva <yuliya.aheeva@mail.ru>
2023-07-19 10:31:25 +0200

    chore: CO-774 remove Comment

--- common/src/main/java/com/zimbra/common/mailbox/MailItemType.java
+++ common/src/main/java/com/zimbra/common/mailbox/MailItemType.java
@@ -6,22 +6,34 @@
 package com.zimbra.common.mailbox;
 
 public enum MailItemType {
-        UNKNOWN,
-        FOLDER, /** Item is a standard Folder. */
-        SEARCHFOLDER, /** Item is a saved search - SearchFolder. */
-        TAG, /** Item is a user-created Tag. */
-        CONVERSATION, /** Item is a real, persisted Conversation. */
-        MESSAGE, /** Item is a mail Message. */
-        CONTACT, /** Item is a Contact. */
-        @Deprecated INVITE, /** Item is a InviteMessage with a {@code text/calendar} MIME part. */
-        NOTE, /** Item is a Note. */
-        FLAG, /** Item is a memory-only system Flag. */
-        APPOINTMENT, /** Item is a calendar Appointment. */
-        VIRTUAL_CONVERSATION, /** Item is a memory-only, 1-message VirtualConversation. */
-        MOUNTPOINT, /** Item is a Mountpoint pointing to a Folder, possibly in another user's Mailbox. */
-        @Deprecated WIKI, /** Item is a WikiItem */
-        TASK, /** Item is a Task */
-        CHAT, /** Item is a Chat */
-        COMMENT, /** Item is a Comment */
-        LINK; /** Item is a Link pointing to a Document */
+  UNKNOWN,
+  FOLDER,
+  /** Item is a standard Folder. */
+  SEARCHFOLDER,
+  /** Item is a saved search - SearchFolder. */
+  TAG,
+  /** Item is a user-created Tag. */
+  CONVERSATION,
+  /** Item is a real, persisted Conversation. */
+  MESSAGE,
+  /** Item is a mail Message. */
+  CONTACT,
+  /** Item is a Contact. */
+  @Deprecated
+  INVITE,
+  /** Item is a InviteMessage with a {@code text/calendar} MIME part. */
+  NOTE,
+  /** Item is a Note. */
+  FLAG,
+  /** Item is a memory-only system Flag. */
+  APPOINTMENT,
+  /** Item is a calendar Appointment. */
+  VIRTUAL_CONVERSATION,
+  /** Item is a memory-only, 1-message VirtualConversation. */
+  MOUNTPOINT,
+  /** Item is a Mountpoint pointing to a Folder, possibly in another user's Mailbox. */
+  TASK,
+  /** Item is a Task */
+  CHAT
+  /** Item is a Chat */
 }
