e0a70a4422367813c17b0f5eee9fe8bf359ccb8e
Dima Dymkovets <dima.dymkovets@innowise-group.com>
2023-08-17 14:57:16 +0200

    feat: implement RefreshFolderActionUseCase, should shouldBeSuccessAfterRefreshFolder test ref: CO-732

--- store/src/main/java/com/zextras/mailbox/usecase/RefreshFolderActionUseCase.java
+++ store/src/main/java/com/zextras/mailbox/usecase/RefreshFolderActionUseCase.java
@@ -1,6 +1,11 @@
 package com.zextras.mailbox.usecase;
 
+import com.zimbra.cs.mailbox.Mailbox;
 import com.zimbra.cs.mailbox.MailboxManager;
+import com.zimbra.cs.mailbox.OperationContext;
+import com.zimbra.cs.service.util.ItemId;
+import io.vavr.control.Try;
+import java.util.Optional;
 import javax.inject.Inject;
 
 public class RefreshFolderActionUseCase {
@@ -12,5 +17,20 @@ public class RefreshFolderActionUseCase {
   public RefreshFolderActionUseCase(MailboxManager mailboxManager, ItemIdFactory itemIdFactory) {
     this.mailboxManager = mailboxManager;
     this.itemIdFactory = itemIdFactory;
+  }
+
+  public Try<Void> refresh(OperationContext operationContext, String accountId, String folderId) {
+    return Try.run(
+        () -> {
+          final Mailbox userMailbox =
+              Optional.ofNullable(mailboxManager.getMailboxByAccountId(accountId, true))
+                  .orElseThrow(
+                      () ->
+                          new IllegalArgumentException(
+                              "unable to locate the mailbox for the given accountId"));
+
+          final ItemId itemId = itemIdFactory.create(folderId, accountId);
+          userMailbox.synchronizeFolder(operationContext, itemId.getId());
+        });
   }
 }
--- store/src/test/java/com/zextras/mailbox/usecase/RefreshFolderActionUseCaseTest.java
+++ store/src/test/java/com/zextras/mailbox/usecase/RefreshFolderActionUseCaseTest.java
@@ -1,10 +1,17 @@
 package com.zextras.mailbox.usecase;
 
-import static org.mockito.Mockito.mock;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.times;
 
+import com.zimbra.cs.mailbox.MailServiceException;
+import com.zimbra.cs.mailbox.Mailbox;
 import com.zimbra.cs.mailbox.MailboxManager;
-import org.junit.Test;
+import com.zimbra.cs.mailbox.OperationContext;
+import com.zimbra.cs.service.util.ItemId;
+import io.vavr.control.Try;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.parallel.Execution;
 import org.junit.jupiter.api.parallel.ExecutionMode;
 
@@ -12,17 +19,54 @@ import org.junit.jupiter.api.parallel.ExecutionMode;
 class RefreshFolderActionUseCaseTest {
 
   private MailboxManager mailboxManager;
-  private EmptyFolderActionUseCase emptyFolderActionUseCase;
+  private RefreshFolderActionUseCase refreshFolderActionUseCase;
   private ItemIdFactory itemIdFactory;
 
   @BeforeEach
   void setUp() {
     mailboxManager = mock(MailboxManager.class);
     itemIdFactory = mock(ItemIdFactory.class);
-
-    emptyFolderActionUseCase = new EmptyFolderActionUseCase(mailboxManager, itemIdFactory);
+    refreshFolderActionUseCase = new RefreshFolderActionUseCase(mailboxManager, itemIdFactory);
   }
 
   @Test
-  void shouldBeSuccessAfterRefreshFolder() throws Exception {}
+  void shouldBeSuccessAfterRefreshFolder() throws Exception {
+    final String accountId = "account-id123";
+    final String folderId = accountId + ":1";
+    final Mailbox userMailbox = mock(Mailbox.class);
+    final OperationContext operationContext = mock(OperationContext.class);
+    final ItemId itemId = mock(ItemId.class);
+
+    when(itemId.getId()).thenReturn(1);
+    when(mailboxManager.getMailboxByAccountId(accountId, true)).thenReturn(userMailbox);
+    when(itemIdFactory.create(folderId, accountId)).thenReturn(itemId);
+
+    refreshFolderActionUseCase.refresh(operationContext, accountId, folderId);
+
+    verify(userMailbox, times(1)).synchronizeFolder(operationContext, itemId.getId());
+  }
+
+  @Test
+  void shouldReturnFailureIfTheFolderIdDoesntExist() throws Exception {
+    final String accountId = "account-id123";
+    final String folderId = accountId + ":1";
+    final Mailbox userMailbox = mock(Mailbox.class);
+    final OperationContext operationContext = mock(OperationContext.class);
+    final ItemId itemId = mock(ItemId.class);
+
+    when(itemId.getId()).thenReturn(1);
+    doThrow(MailServiceException.NO_SUCH_FOLDER(folderId))
+        .when(userMailbox)
+        .emptyFolder(operationContext, 1, false);
+    when(mailboxManager.getMailboxByAccountId(accountId, true)).thenReturn(userMailbox);
+    when(itemIdFactory.create(folderId, accountId)).thenReturn(itemId);
+
+    final Try<Void> operationResult =
+        refreshFolderActionUseCase.refresh(operationContext, accountId, folderId);
+
+    assertTrue(operationResult.isFailure());
+    final Throwable gotError = operationResult.getCause();
+    assertInstanceOf(MailServiceException.NoSuchItemException.class, gotError);
+    assertEquals("no such folder path: account-id123:1", gotError.getMessage());
+  }
 }
