755a6fdaff081d2007adef8225ed70883139c58d
Yuliya Aheeva <yuliya.aheeva@mail.ru>
2023-09-04 11:10:04 +0200

    feat: CheckFolderActionUseCase, CheckFolderActionUseCaseTest ref: CO-732

---
+++ store/src/main/java/com/zextras/mailbox/usecase/CheckFolderActionUseCase.java
@@ -0,0 +1,55 @@
+package com.zextras.mailbox.usecase;
+
+import com.zimbra.cs.mailbox.Flag;
+import com.zimbra.cs.mailbox.MailItem;
+import com.zimbra.cs.mailbox.Mailbox;
+import com.zimbra.cs.mailbox.MailboxManager;
+import com.zimbra.cs.mailbox.OperationContext;
+import com.zimbra.cs.service.util.ItemId;
+import io.vavr.control.Try;
+import java.util.Optional;
+import javax.inject.Inject;
+
+public class CheckFolderActionUseCase {
+
+  private final MailboxManager mailboxManager;
+  private final ItemIdFactory itemIdFactory;
+
+  @Inject
+  public CheckFolderActionUseCase(MailboxManager mailboxManager, ItemIdFactory itemIdFactory) {
+    this.mailboxManager = mailboxManager;
+    this.itemIdFactory = itemIdFactory;
+  }
+
+  public Try<Void> check(OperationContext operationContext, String accountId, String folderId) {
+    return innerCheckCall(operationContext, accountId, folderId, true);
+  }
+
+  public Try<Void> uncheck(OperationContext operationContext, String accountId, String folderId) {
+    return innerCheckCall(operationContext, accountId, folderId, false);
+  }
+
+  private Try<Void> innerCheckCall(
+      OperationContext operationContext, String accountId, String folderId, boolean checkFlag) {
+
+    return Try.run(
+        () -> {
+          final Mailbox userMailbox =
+              Optional.ofNullable(mailboxManager.getMailboxByAccountId(accountId, true))
+                  .orElseThrow(
+                      () ->
+                          new IllegalArgumentException(
+                              "unable to locate the mailbox for the given accountId"));
+
+          final ItemId itemId = itemIdFactory.create(folderId, accountId);
+
+          userMailbox.alterTag(
+              operationContext,
+              itemId.getId(),
+              MailItem.Type.FOLDER,
+              Flag.FlagInfo.CHECKED,
+              checkFlag,
+              null);
+        });
+  }
+}
---
+++ store/src/test/java/com/zextras/mailbox/usecase/CheckFolderActionUseCaseTest.java
@@ -0,0 +1,86 @@
+package com.zextras.mailbox.usecase;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import com.zimbra.common.service.ServiceException;
+import com.zimbra.cs.mailbox.Flag;
+import com.zimbra.cs.mailbox.MailItem;
+import com.zimbra.cs.mailbox.Mailbox;
+import com.zimbra.cs.mailbox.MailboxManager;
+import com.zimbra.cs.mailbox.OperationContext;
+import com.zimbra.cs.service.util.ItemId;
+import io.vavr.control.Try;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class CheckFolderActionUseCaseTest {
+  private MailboxManager mailboxManager;
+  private CheckFolderActionUseCase checkFolderActionUseCase;
+  private ItemIdFactory itemIdFactory;
+
+  @BeforeEach
+  void setUp() {
+    mailboxManager = mock(MailboxManager.class);
+    itemIdFactory = mock(ItemIdFactory.class);
+
+    checkFolderActionUseCase = new CheckFolderActionUseCase(mailboxManager, itemIdFactory);
+  }
+
+  @Test
+  void shouldBeSuccessWithCheckFolderTag() throws ServiceException {
+    final String accountId = "account-id123";
+    final String folderId = accountId + ":1";
+    final Mailbox userMailbox = mock(Mailbox.class);
+    final OperationContext operationContext = mock(OperationContext.class);
+    final ItemId itemId = mock(ItemId.class);
+    when(itemId.getId()).thenReturn(1);
+    when(mailboxManager.getMailboxByAccountId(accountId, true)).thenReturn(userMailbox);
+    when(itemIdFactory.create(folderId, accountId)).thenReturn(itemId);
+
+    final Try<Void> operationResult =
+        checkFolderActionUseCase.check(operationContext, accountId, folderId);
+
+    assertDoesNotThrow(operationResult::get);
+    assertTrue(operationResult.isSuccess(), "Folder should be successfully checked.");
+
+    verify(userMailbox, times(1))
+        .alterTag(
+            operationContext,
+            itemId.getId(),
+            MailItem.Type.FOLDER,
+            Flag.FlagInfo.CHECKED,
+            true,
+            null);
+  }
+
+  @Test
+  void shouldBeSuccessWithUncheckFolderTag() throws ServiceException {
+    final String accountId = "account-id123";
+    final String folderId = accountId + ":1";
+    final Mailbox userMailbox = mock(Mailbox.class);
+    final OperationContext operationContext = mock(OperationContext.class);
+    final ItemId itemId = mock(ItemId.class);
+    when(itemId.getId()).thenReturn(1);
+    when(mailboxManager.getMailboxByAccountId(accountId, true)).thenReturn(userMailbox);
+    when(itemIdFactory.create(folderId, accountId)).thenReturn(itemId);
+
+    final Try<Void> operationResult =
+        checkFolderActionUseCase.uncheck(operationContext, accountId, folderId);
+
+    assertDoesNotThrow(operationResult::get);
+    assertTrue(operationResult.isSuccess(), "Folder should be successfully unchecked.");
+
+    verify(userMailbox, times(1))
+        .alterTag(
+            operationContext,
+            itemId.getId(),
+            MailItem.Type.FOLDER,
+            Flag.FlagInfo.CHECKED,
+            false,
+            null);
+  }
+}
