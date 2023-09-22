d1ab06fe297c063cb17c271c7d4107eed5a2eacc
Davide Frison <davide.frison@zextras.com>
2023-09-22 10:50:23 +0200

    chore: working in-memory server
    - update LDAP admin password with value from localconfig

--- store/src/test/java/com/zextras/mailbox/usecase/service/MountpointServiceTest.java
+++ store/src/test/java/com/zextras/mailbox/usecase/service/MountpointServiceTest.java
@@ -4,30 +4,40 @@ import static com.zimbra.cs.account.Provisioning.SERVICE_MAILCLIENT;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.mockStatic;
+import static org.mockito.Mockito.times;
 
 import com.unboundid.ldap.listener.InMemoryDirectoryServer;
 import com.unboundid.ldap.listener.InMemoryDirectoryServerConfig;
 import com.unboundid.ldap.listener.InMemoryListenerConfig;
+import com.unboundid.ldap.sdk.Modification;
+import com.unboundid.ldap.sdk.ModificationType;
 import com.zextras.mailbox.usecase.factory.ItemIdFactory;
 import com.zextras.mailbox.usecase.factory.OperationContextFactory;
+import com.zimbra.common.account.Key;
 import com.zimbra.common.account.ZAttrProvisioning;
 import com.zimbra.common.localconfig.LC;
 import com.zimbra.common.service.ServiceException;
+import com.zimbra.common.soap.SoapProtocol;
 import com.zimbra.cs.account.Account;
 import com.zimbra.cs.account.Provisioning;
 import com.zimbra.cs.db.DbPool;
 import com.zimbra.cs.db.HSQLDB;
+import com.zimbra.cs.mailbox.ACL;
+import com.zimbra.cs.mailbox.Folder;
 import com.zimbra.cs.mailbox.MailItem.Type;
 import com.zimbra.cs.mailbox.Mailbox;
 import com.zimbra.cs.mailbox.MailboxManager;
 import com.zimbra.cs.mailbox.Mountpoint;
+import com.zimbra.cs.mailbox.OperationContext;
 import com.zimbra.cs.redolog.RedoLogProvider;
 import com.zimbra.cs.service.mail.ItemActionHelper;
+import com.zimbra.cs.service.util.ItemId;
 import com.zimbra.cs.store.StoreManager;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
+import java.util.UUID;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -43,20 +53,6 @@ public class MountpointServiceTest {
   private MountpointService mountpointService;
   private MockedStatic<ItemActionHelper> itemActionHelper;
 
-  //
-  //  @Container
-  //  GenericContainer ldapContainer =
-  //      new GenericContainer<>(DockerImageName.parse("carbonio/ce-ldap-u20:latest"))
-  //          .withCreateContainerCmdModifier(
-  //              cmd -> {
-  //                cmd.withHostName("ldap.mail.local");
-  //                cmd.withHostConfig(
-  //                    new HostConfig()
-  //                        .withPortBindings(
-  //                            new PortBinding(Binding.bindPort(389), new ExposedPort(389))));
-  //              })
-  //          .withExposedPorts(389);
-
   @BeforeEach
   public void setUp() throws Exception {
 
@@ -64,14 +60,7 @@ public class MountpointServiceTest {
         "zimbra.config",
         Objects.requireNonNull(this.getClass().getResource("/localconfig-api-test.xml")).getFile());
 
-    // get top level domain name setup in reset-all.
-    // In reset-all, the default domain name is the same as the local server name
-
-    // add list of known DNs
-
-    // add list of known DNs
-    InMemoryDirectoryServerConfig ldapServerConfig = null;
-    ldapServerConfig =
+    InMemoryDirectoryServerConfig ldapServerConfig =
         new InMemoryDirectoryServerConfig(
             "dc=com",
             "cn=config",
@@ -79,14 +68,11 @@ public class MountpointServiceTest {
             "cn=default,cn=cos,cn=zimbra",
             "cn=config,cn=zimbra",
             "cn=zimbra");
-    ldapServerConfig.setListenerConfigs(InMemoryListenerConfig.createLDAPConfig("LDAP", LDAP_PORT));
     ldapServerConfig.addAdditionalBindCredentials("cn=config", LC.ldap_root_password.value());
-
+    ldapServerConfig.setListenerConfigs(InMemoryListenerConfig.createLDAPConfig("LDAP", LDAP_PORT));
     ldapServerConfig.setSchema(null);
     ldapServerConfig.setGenerateOperationalAttributes(true);
 
-    // Create and start the server instance and populate it with an initial set
-    // of data from an LDIF file.
     InMemoryDirectoryServer server = new InMemoryDirectoryServer(ldapServerConfig);
     server.importFromLDIF(false, "./build/ldap/config/cn=config.ldif");
     server.importFromLDIF(false, "./build/ldap/zimbra_globalconfig.ldif");
@@ -94,6 +80,13 @@ public class MountpointServiceTest {
     server.importFromLDIF(false, "./build/ldap/zimbra_defaultexternalcos.ldif");
     server.importFromLDIF(false, "./build/ldap/carbonio.ldif");
     server.startListening();
+    // update password for admin
+    server
+        .getConnection()
+        .modify(
+            "uid=zimbra,cn=admins,cn=zimbra",
+            new Modification(
+                ModificationType.REPLACE, "userPassword", LC.zimbra_ldap_password.value()));
 
     LC.ldap_port.setDefault(LDAP_PORT);
     LC.zimbra_class_database.setDefault(HSQLDB.class.getName());
@@ -127,85 +120,80 @@ public class MountpointServiceTest {
     itemActionHelper.close();
   }
 
-  //
-  //  @Test
-  //  void shouldGetMountpointsByPath() throws ServiceException {
-  //    Provisioning prov = Provisioning.getInstance();
-  //
-  //    Map<String, Object> granteeAttrs = Maps.newHashMap();
-  //    granteeAttrs.put(Provisioning.A_zimbraId, UUID.randomUUID().toString());
-  //    prov.createAccount("grantee@test.com", "secret", granteeAttrs);
-  //
-  //    Map<String, Object> ownerAttrs = Maps.newHashMap();
-  //    ownerAttrs.put(Provisioning.A_zimbraId, UUID.randomUUID().toString());
-  //    prov.createAccount("owner@test.com", "secret", ownerAttrs);
-  //
-  //    Account granteeAccount = Provisioning.getInstance().get(Key.AccountBy.name,
-  // "grantee@test.com");
-  //    Account ownerAccount = Provisioning.getInstance().get(Key.AccountBy.name, "owner@test.com");
-  //
-  //    Mailbox granteeMailbox = MailboxManager.getInstance().getMailboxByAccount(granteeAccount);
-  //    Mailbox ownerMailbox = MailboxManager.getInstance().getMailboxByAccount(ownerAccount);
-  //
-  //    OperationContext granteeContext = new OperationContext(granteeAccount);
-  //    ItemId granteeRootFolder =
-  //        new ItemId(String.valueOf(Mailbox.ID_FOLDER_USER_ROOT), granteeAccount.getId());
-  //
-  //    int folderId = 2;
-  //    Folder folder = ownerMailbox.getFolderById(null, folderId);
-  //    String mountPointFolderName = "shared folder";
-  //    ownerMailbox.grantAccess(
-  //        null,
-  //        folderId,
-  //        granteeAccount.getId(),
-  //        ACL.GRANTEE_USER,
-  //        (short) (ACL.RIGHT_READ | ACL.RIGHT_WRITE),
-  //        null);
-  //
-  //    Mountpoint mountpoint =
-  //        granteeMailbox.createMountpoint(
-  //            null,
-  //            Mailbox.ID_FOLDER_USER_ROOT,
-  //            mountPointFolderName,
-  //            ownerAccount.getId(),
-  //            folderId,
-  //            folder.getUuid(),
-  //            Type.CONVERSATION,
-  //            0,
-  //            (byte) 2,
-  //            false);
-  //
-  //    List<Mountpoint> mountPoints =
-  //        mountpointService.getMountpointsByPath(granteeMailbox, granteeContext,
-  // granteeRootFolder);
-  //
-  //    assertFalse(mountPoints.isEmpty());
-  //    assertEquals(1, mountPoints.size());
-  //    assertEquals(mountpoint, mountPoints.get(0));
-  //  }
-  //
-  //  @Test
-  //  void shouldDeleteMountPoints() throws ServiceException {
-  //    Provisioning prov = Provisioning.getInstance();
-  //    prov.createAccount("test@test.com", "secret", new HashMap<>());
-  //    Account account = prov.getAccountById(MockProvisioning.DEFAULT_ACCOUNT_ID);
-  //    Mailbox mailbox = MailboxManager.getInstance().getMailboxByAccount(account);
-  //    OperationContext operationContext = new OperationContext(account);
-  //    List<Integer> mountpointsIds = List.of(1, 2, 3);
-  //
-  //    mountpointService.deleteMountpointsByIds(mailbox, operationContext, mountpointsIds);
-  //
-  //    itemActionHelper.verify(
-  //        () ->
-  //            ItemActionHelper.HARD_DELETE(
-  //                operationContext,
-  //                mailbox,
-  //                SoapProtocol.Soap12,
-  //                mountpointsIds,
-  //                Type.MOUNTPOINT,
-  //                null),
-  //        times(1));
-  //  }
+  @Test
+  void shouldGetMountpointsByPath() throws ServiceException {
+    Provisioning prov = Provisioning.getInstance();
+
+    Map<String, Object> granteeAttrs = new HashMap<>();
+    granteeAttrs.put(Provisioning.A_zimbraId, UUID.randomUUID().toString());
+    prov.createAccount("grantee@test.com", "secret", granteeAttrs);
+
+    Map<String, Object> ownerAttrs = new HashMap<>();
+    ownerAttrs.put(Provisioning.A_zimbraId, UUID.randomUUID().toString());
+    prov.createAccount("owner@test.com", "secret", ownerAttrs);
+
+    Account granteeAccount = Provisioning.getInstance().get(Key.AccountBy.name, "grantee@test.com");
+    Account ownerAccount = Provisioning.getInstance().get(Key.AccountBy.name, "owner@test.com");
+
+    Mailbox granteeMailbox = MailboxManager.getInstance().getMailboxByAccount(granteeAccount);
+    Mailbox ownerMailbox = MailboxManager.getInstance().getMailboxByAccount(ownerAccount);
+
+    OperationContext granteeContext = new OperationContext(granteeAccount);
+    ItemId granteeRootFolder =
+        new ItemId(String.valueOf(Mailbox.ID_FOLDER_USER_ROOT), granteeAccount.getId());
+
+    int folderId = 2;
+    Folder folder = ownerMailbox.getFolderById(null, folderId);
+    String mountPointFolderName = "shared folder";
+    ownerMailbox.grantAccess(
+        null,
+        folderId,
+        granteeAccount.getId(),
+        ACL.GRANTEE_USER,
+        (short) (ACL.RIGHT_READ | ACL.RIGHT_WRITE),
+        null);
+
+    Mountpoint mountpoint =
+        granteeMailbox.createMountpoint(
+            null,
+            Mailbox.ID_FOLDER_USER_ROOT,
+            mountPointFolderName,
+            ownerAccount.getId(),
+            folderId,
+            folder.getUuid(),
+            Type.CONVERSATION,
+            0,
+            (byte) 2,
+            false);
+
+    List<Mountpoint> mountPoints =
+        mountpointService.getMountpointsByPath(granteeMailbox, granteeContext, granteeRootFolder);
+
+    assertFalse(mountPoints.isEmpty());
+    assertEquals(1, mountPoints.size());
+    assertEquals(mountpoint, mountPoints.get(0));
+  }
+
+  @Test
+  void shouldDeleteMountPoints() throws ServiceException {
+    Provisioning prov = Provisioning.getInstance();
+    Mailbox mailbox = MailboxManager.getInstance().getMailboxByAccount(testAccount);
+    OperationContext operationContext = new OperationContext(testAccount);
+    List<Integer> mountpointsIds = List.of(1, 2, 3);
+
+    mountpointService.deleteMountpointsByIds(mailbox, operationContext, mountpointsIds);
+
+    itemActionHelper.verify(
+        () ->
+            ItemActionHelper.HARD_DELETE(
+                operationContext,
+                mailbox,
+                SoapProtocol.Soap12,
+                mountpointsIds,
+                Type.MOUNTPOINT,
+                null),
+        times(1));
+  }
 
   @Test
   void shouldFilterMountpointsByOwnerIdAndRemoteFolderId() throws ServiceException {
