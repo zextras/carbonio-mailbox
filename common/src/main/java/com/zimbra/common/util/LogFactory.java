2f7d502517d0df2d331310110b11888a65614a45
Dima Dymkovets <dima.dymkovets@innowise-group.com>
2023-09-27 14:54:41 +0200

    fix: change logger creation to use LoggerContext ref: CO-842

--- common/src/main/java/com/zimbra/common/util/LogFactory.java
+++ common/src/main/java/com/zimbra/common/util/LogFactory.java
@@ -12,7 +12,8 @@ package com.zimbra.common.util;
  * @since 17.2
  *     <!-- Last commit on this file before mine -->
  * @see LogManager to interact and manage {@link Log} classes
- * @see org.apache.logging.log4j.core.config.ConfigurationFactory to see how {@link org.apache.logging.log4j.Logger} are configured
+ * @see org.apache.logging.log4j.core.config.ConfigurationFactory to see how {@link
+ *     org.apache.logging.log4j.Logger} are configured
  */
 public class LogFactory {
 
@@ -28,6 +29,7 @@ public class LogFactory {
 
   public static Log getLog(String name) {
     return LogManager.getGlobalLogMapper()
-        .computeIfAbsent(name, n -> new Log(org.apache.logging.log4j.LogManager.getLogger(n)));
+        .computeIfAbsent(
+            name, n -> new Log(org.apache.logging.log4j.LogManager.getContext().getLogger(name)));
   }
 }
