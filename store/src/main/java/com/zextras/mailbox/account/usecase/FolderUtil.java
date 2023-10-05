78fe2ad945bc795ba77ad734120454133811beab
Davide Frison <davide.frison@zextras.com>
2023-10-05 14:52:49 +0200

    chore: add javadoc for FolderUtil
    - add javadoc + private constructor

--- store/src/main/java/com/zextras/mailbox/account/usecase/FolderUtil.java
+++ store/src/main/java/com/zextras/mailbox/account/usecase/FolderUtil.java
@@ -9,14 +9,35 @@ import com.zimbra.cs.mailbox.Mailbox.FolderNode;
 import java.util.HashSet;
 import java.util.Set;
 
+/**
+ * Utility class for operation on folders. Methods were extracted from {@link
+ * com.zimbra.cs.account.ShareInfo}
+ */
 public class FolderUtil {
 
+  private FolderUtil() {}
+
+  /**
+   * Flattens and sorts folder starting from the provided root. For example folders A > B > C, D, E
+   * > F, G will become a {@link Set} of A,B,C,D,E,F,G
+   *
+   * @param root start folder
+   * @return {@link Set} of {@link Folder}
+   */
   public static Set<Folder> flattenAndSortFolderTree(FolderNode root) {
     Set<Folder> folders = new HashSet<Folder>();
     flattenAndSortFolderTree(root, folders);
     return folders;
   }
 
+  /**
+   * Iterates folders starting from the current node and flattens recursively if node has
+   * sub-folders.
+   *
+   * @param node {@link FolderNode}
+   * @param flattened {@link Set} of already flattened {@link Folder} where to add new flattened
+   *     values
+   */
   private static void flattenAndSortFolderTree(FolderNode node, Set<Folder> flattened) {
     if (node.mFolder != null) flattened.add(node.mFolder);
     for (FolderNode subNode : node.mSubfolders) flattenAndSortFolderTree(subNode, flattened);
