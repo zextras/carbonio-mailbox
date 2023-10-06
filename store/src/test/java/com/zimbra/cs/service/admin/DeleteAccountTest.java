47237b3ed4da5790f2aad3e1ef84ce9265a6be99
Davide Frison <davide.frison@zextras.com>
2023-10-06 16:32:16 +0200

    test: run DeleteAccountTest without jetty
    - SoapServlets has many limitations that make impossible to do tests on maven due to calls to System.exit

--- store/src/test/java/com/zimbra/cs/service/admin/DeleteAccountTest.java
+++ store/src/test/java/com/zimbra/cs/service/admin/DeleteAccountTest.java
@@ -4,18 +4,15 @@
 
 package com.zimbra.cs.service.admin;
 
-import static com.zimbra.common.service.ServiceException.AUTH_REQUIRED;
-import static com.zimbra.common.service.ServiceException.PERM_DENIED;
-
-import com.zextras.mailbox.util.JettyServerFactory;
+import com.zextras.mailbox.account.usecase.DeleteUserUseCase;
+import com.zextras.mailbox.acl.AclService;
 import com.zextras.mailbox.util.MailboxTestUtil;
 import com.zimbra.common.account.ZAttrProvisioning;
 import com.zimbra.common.service.ServiceException;
 import com.zimbra.common.soap.Element;
 import com.zimbra.common.soap.SoapProtocol;
-import com.zimbra.common.util.ZimbraCookie;
+import com.zimbra.common.util.ZimbraLog;
 import com.zimbra.cs.account.Account;
-import com.zimbra.cs.account.AuthToken;
 import com.zimbra.cs.account.AuthTokenException;
 import com.zimbra.cs.account.Provisioning;
 import com.zimbra.cs.account.accesscontrol.ACLUtil;
@@ -23,81 +20,51 @@ import com.zimbra.cs.account.accesscontrol.GranteeType;
 import com.zimbra.cs.account.accesscontrol.RightModifier;
 import com.zimbra.cs.account.accesscontrol.ZimbraACE;
 import com.zimbra.cs.account.accesscontrol.generated.AdminRights;
+import com.zimbra.cs.mailbox.MailboxManager;
 import com.zimbra.cs.service.AuthProvider;
-import com.zimbra.cs.service.AuthProviderException;
-import com.zimbra.cs.servlet.FirstServlet;
 import com.zimbra.soap.JaxbUtil;
-import com.zimbra.soap.SoapServlet;
+import com.zimbra.soap.SoapEngine;
+import com.zimbra.soap.ZimbraSoapContext;
 import com.zimbra.soap.admin.message.DeleteAccountRequest;
 import io.vavr.control.Try;
-import java.io.File;
 import java.io.IOException;
-import java.io.UnsupportedEncodingException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 import java.util.UUID;
 import java.util.stream.Stream;
-import org.apache.http.HttpResponse;
-import org.apache.http.HttpStatus;
-import org.apache.http.client.CookieStore;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.entity.StringEntity;
-import org.apache.http.impl.client.BasicCookieStore;
-import org.apache.http.impl.client.HttpClientBuilder;
-import org.apache.http.impl.cookie.BasicClientCookie;
-import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.servlet.ServletHolder;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Tag;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
-@Tag("api")
-@Disabled
 class DeleteAccountTest {
 
-  private static final int ADMIN_PORT = 7071;
   private static final String OTHER_DOMAIN = "other.com";
-  private static Server mailboxServer;
   private static Provisioning provisioning;
+  private static DeleteAccount deleteAccount;
 
   /**
-   * Sets up the environment using {@link MailboxTestUtil}. Also starts the SoapServlet with {@link
-   * AdminService} only Starts also {@link FirstServlet} since it is required to make the API work.
+   * Sets up the environment using {@link MailboxTestUtil}.
+   * Note: unfortunately it is not possible to start the SoapServlet with {@link
+   * AdminService}. The reason is some code calls System.exit(1) presumably, so the VM exits and maven fails.
    *
    * @throws Exception
    */
   @BeforeAll
   static void setUp() throws Exception {
-    System.setProperty("java.library.path", new File("./../").getAbsolutePath() + "/native/target");
     MailboxTestUtil.setUp();
-    final ServletHolder firstServlet = new ServletHolder("FirstServlet", FirstServlet.class);
-    firstServlet.setInitOrder(0);
-    final ServletHolder adminServlet = new ServletHolder("AdminServlet", SoapServlet.class);
-    adminServlet.setInitParameter("allowed.ports", Integer.toString(ADMIN_PORT));
-    adminServlet.setInitOrder(1);
-    adminServlet.setInitParameter("engine.handler.0", "com.zimbra.cs.service.admin.AdminService");
+    final MailboxManager mailboxManager = MailboxManager.getInstance();
     provisioning = Provisioning.getInstance();
+    deleteAccount = new DeleteAccount(new DeleteUserUseCase(provisioning, mailboxManager,
+        new AclService(mailboxManager, provisioning), ZimbraLog.security));
     provisioning.createDomain(OTHER_DOMAIN, new HashMap<>());
-    mailboxServer =
-        JettyServerFactory.create(
-            ADMIN_PORT,
-            Map.of(
-                "/*", adminServlet,
-                "/firstServlet/*", firstServlet));
-    mailboxServer.start();
   }
 
   @AfterAll
   static void tearDown() throws Exception {
-    mailboxServer.stop();
     MailboxTestUtil.tearDown();
   }
 
@@ -244,41 +211,25 @@ class DeleteAccountTest {
             .get());
   }
 
+  private Element doDeleteAccount(Account caller, String toDeleteId) throws ServiceException {
+    Map<String, Object> context = new HashMap<String, Object>();
+    ZimbraSoapContext zsc =
+        new ZimbraSoapContext(
+            AuthProvider.getAuthToken(caller),
+            caller.getId(),
+            SoapProtocol.Soap12,
+            SoapProtocol.Soap12);
+    context.put(SoapEngine.ZIMBRA_CONTEXT, zsc);
+    return deleteAccount.handle(
+        JaxbUtil.jaxbToElement(new DeleteAccountRequest(toDeleteId)), context);
+  }
+
   @ParameterizedTest
   @MethodSource("getHappyPathCases")
   void shouldDeleteUser(Account caller, Account toDelete) throws Exception {
     final String toDeleteId = toDelete.getId();
-    final HttpResponse deleteUserResponse =
-        this.client()
-            .setDefaultCookieStore(getCookie(caller))
-            .build()
-            .execute(deleteRequest(toDeleteId));
-    Assertions.assertEquals(HttpStatus.SC_OK, deleteUserResponse.getStatusLine().getStatusCode());
+    this.doDeleteAccount(caller, toDeleteId);
     Assertions.assertNull(provisioning.getAccountById(toDeleteId));
-  }
-
-  private HttpClientBuilder client() {
-    return HttpClientBuilder.create();
-  }
-
-  private CookieStore getCookie(Account account) throws AuthProviderException, AuthTokenException {
-    final AuthToken authToken = AuthProvider.getAuthToken(account, true);
-    BasicCookieStore cookieStore = new BasicCookieStore();
-    BasicClientCookie cookie =
-        new BasicClientCookie(ZimbraCookie.authTokenCookieName(true), authToken.getEncoded());
-    cookie.setDomain("localhost");
-    cookie.setPath("/");
-    cookieStore.addCookie(cookie);
-    return cookieStore;
-  }
-
-  private HttpPost deleteRequest(String toDeleteId)
-      throws ServiceException, UnsupportedEncodingException {
-    final HttpPost httpPost = new HttpPost("http://localhost:" + ADMIN_PORT);
-    final Element element = JaxbUtil.jaxbToElement(new DeleteAccountRequest(toDeleteId));
-    Element envelope = SoapProtocol.Soap12.soapEnvelope(element, null);
-    httpPost.setEntity(new StringEntity(envelope.toString()));
-    return httpPost;
   }
 
   private static Stream<Arguments> getPermissionDeniedCases() throws ServiceException {
@@ -325,27 +276,18 @@ class DeleteAccountTest {
   void shouldGetPermissionDenied(Account caller, Account toDelete)
       throws ServiceException, AuthTokenException, IOException {
     final String toDeleteId = toDelete.getId();
-    final HttpResponse deleteUserResponse =
-        this.client()
-            .setDefaultCookieStore(getCookie(caller))
-            .build()
-            .execute(deleteRequest(toDeleteId));
-    Assertions.assertEquals(
-        HttpStatus.SC_INTERNAL_SERVER_ERROR, deleteUserResponse.getStatusLine().getStatusCode());
-    final String responseEnvelope =
-        new String(deleteUserResponse.getEntity().getContent().readAllBytes());
-    Assertions.assertTrue(responseEnvelope.contains("<Code>" + PERM_DENIED + "</Code>"));
+    Assertions.assertThrows(ServiceException.class, () -> this.doDeleteAccount(caller, toDeleteId));
     Assertions.assertNotNull(provisioning.getAccountById(toDeleteId));
   }
 
-  @Test
-  @DisplayName("No token when calling the API -> AUTH_REQUIRED")
-  void shouldGet500WithAuthRequiredIfNoToken() throws Exception {
-    final Account toDelete = MailboxTestUtil.createAccountDefaultDomain(Map.of());
-    final HttpResponse response = this.client().build().execute(deleteRequest(toDelete.getId()));
-    final String responseEnvelope = new String(response.getEntity().getContent().readAllBytes());
-    Assertions.assertTrue(responseEnvelope.contains("<Code>" + AUTH_REQUIRED + "</Code>"));
-    Assertions.assertEquals(
-        HttpStatus.SC_INTERNAL_SERVER_ERROR, response.getStatusLine().getStatusCode());
-  }
+//  @Test
+//  @DisplayName("No token when calling the API -> AUTH_REQUIRED")
+//  void shouldGet500WithAuthRequiredIfNoToken() throws Exception {
+//    final Account toDelete = MailboxTestUtil.createAccountDefaultDomain(Map.of());
+//    final HttpResponse response = this.client().build().execute(deleteRequest(toDelete.getId()));
+//    final String responseEnvelope = new String(response.getEntity().getContent().readAllBytes());
+//    Assertions.assertTrue(responseEnvelope.contains("<Code>" + AUTH_REQUIRED + "</Code>"));
+//    Assertions.assertEquals(
+//        HttpStatus.SC_INTERNAL_SERVER_ERROR, response.getStatusLine().getStatusCode());
+//  }
 }
