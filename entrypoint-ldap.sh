08475de191b74b8d757c30ebbf20d9983ba83915
Davide Frison <davide.frison@zextras.com>
2023-10-19 15:34:24 +0200

    chore: add ldap docker-compose with built schemas
    - adds docker-compose.yml definition with ldap
    - adds entrypoint-ldap.sh to update ldap schema

---
+++ entrypoint-ldap.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#
+# SPDX-FileCopyrightText: 2023 Zextras <https://www.zextras.com>
+#
+# SPDX-License-Identifier: AGPL-3.0-only
+#
+
+touch /.dockerenv
+su - zextras -c "/opt/zextras/bin/ldap stop"
+su - zextras -c "/opt/zextras/libexec/zmldapschema 2>/dev/null"
+su - zextras -c "/opt/zextras/libexec/zmldapupdateldif"
+su - zextras -c "/opt/zextras/libexec/ldapattributeupdate"
+su - zextras -c "/opt/zextras/bin/ldap start"
+monit -c /etc/monitrc
