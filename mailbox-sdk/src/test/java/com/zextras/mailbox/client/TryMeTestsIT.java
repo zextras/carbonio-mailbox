776818b12ba8fb779c82949ad8e13a55b9c194da
Matteo Baglini <matteo.baglini@gmail.com>
2023-10-25 12:24:54 +0200

    chore: setup unit and integration tests

--- mailbox-sdk/src/test/java/com/zextras/mailbox/client/TryMeTestsIT.java
+++ mailbox-sdk/src/test/java/com/zextras/mailbox/client/TryMeTestsIT.java
@@ -4,7 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
-public class TryMeTests {
+public class TryMeTestsIT {
   @Test
   void itWorks() {
 
