de622aa3a2b4724bad96bb03416051e12ac443f3
Matteo Baglini <matteo.baglini@gmail.com>
2023-10-30 10:32:51 +0100

    chore: add copyright and licensing information

--- mailbox-sdk/src/main/java/com/zextras/mailbox/client/PooledClient.java
+++ mailbox-sdk/src/main/java/com/zextras/mailbox/client/PooledClient.java
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2023 Zextras <https://www.zextras.com>
+//
+// SPDX-License-Identifier: AGPL-3.0-only
+
 package com.zextras.mailbox.client;
 
 import com.sun.xml.ws.api.message.Header;
