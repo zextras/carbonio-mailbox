733b8129141005c2de723ce17dbba0358b4ee1e8
Davide Frison <davide.frison@zextras.com>
2023-10-30 17:22:11 +0100

    chore: add prepare_data.sh script
    - add script to prepare data on container with common users for testing

--- Dockerfile
+++ Dockerfile
@@ -5,5 +5,7 @@ USER root
 RUN apt update
 
 COPY artifacts/*amd64.deb /packages/
+COPY prepare_data.sh /opt/zextras/
+RUN chown -R zextras:zextras /opt/zextras/prepare_data.sh
 
 RUN dpkg -i /packages/*deb
---
+++ prepare_data.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#
+# SPDX-FileCopyrightText: 2023 Zextras <https://www.zextras.com>
+#
+# SPDX-License-Identifier: AGPL-3.0-only
+#
+set -x
+
+DOMAIN="carbonio-system.svc.cluster.local"
+# Update zextras password
+carbonio prov sp zextras password
+
+# Create test user
+carbonio prov ca test@"${DOMAIN}" password
+
+# Create admin user
+carbonio prov ca admin@"${DOMAIN}" password zimbraIsAdminAccount TRUE
+
+# Create shared account
+carbonio prov ca shared@"${DOMAIN}" password displayName "Shared"
+
+carbonio prov grantRight account shared@"${DOMAIN}" usr test@"${DOMAIN}" sendAs
+zmmailbox -z -m shared@"${DOMAIN}" mfg / account test@"${DOMAIN}" rwidx
+MOUNTPOINT_ID=$(zmmailbox -z -m test@"${DOMAIN}" cm /shared@"${DOMAIN}" shared@"${DOMAIN}" /)
+carbonio prov createIdentity shared "Shared" zimbraPrefFromAddress shared@"${DOMAIN}" zimbraPrefFromDisplay "Shared" zimbraPrefWhenInFolderIds "${MOUNTPOINT_ID}" zimbraPrefWhenInFoldersEnabled TRUE zimbraPrefWhenSentToEnabled TRUE
