13cc4176d643473367488ac990162c9c08e0163d
Davide Frison <davide.frison@zextras.com>
2023-11-02 13:06:21 +0100

    chore: check mailbox healthy

--- docker/single-server/entrypoint.sh
+++ docker/single-server/entrypoint.sh
@@ -17,6 +17,32 @@ zmprov -l mcf zimbraSmtpHostname 127.0.0.1
 zmcontrol start
 
 # Wait until mailbox up
+echo mailboxIsHealthy
+HEALTHCHECK="false"
+
+until [ $HEALTHCHECK == "true" ] || command; do
+    echo "Checking mailbox health..."
+    HEALTHCHECK=mailboxIsHealthy
+done
+
+echo "Mailbox healthy"
+
+function mailboxIsHealthy() {
+    return $(curl -X POST -k https://localhost:7071/service/admin/soap -d '
+    {
+        "Body": {
+            "CheckHealthRequest": {
+                "_jsns": "urn:zimbraAdmin",
+            }
+        }
+    }' | jq ".Body.CheckHealthResponse.healthy")
+}
+
+curl -s -X POST -k https://localhost:7071/service/admin/soap -d '<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:urn="urn:zimbra" xmlns:urn1="urn:zimbraAdmin">
+   <soapenv:Body>
+      <urn1:CheckHealthRequest/>
+   </soapenv:Body>
+</soapenv:Envelope>' | grep -i healthy=\"0\"
 
 # Populate with some data
 /opt/zextras/prepare_data.sh
