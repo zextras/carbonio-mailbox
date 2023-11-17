59dc8b1daab30a24588398a7ddae0a082b5e8485
Davide Frison <davide.frison@zextras.com>
2023-11-17 16:47:44 +0100

    chore: cleanup comments

--- store/src/test/java/com/zimbra/cs/service/mail/AutoCompleteTest.java
+++ store/src/test/java/com/zimbra/cs/service/mail/AutoCompleteTest.java
@@ -39,18 +39,19 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 
-
+@Tag("api")
 public class AutoCompleteTest {
 
+  private static final int PORT = 8090;
   private static Server server;
   private static SoapClient soapClient;
   private static AccountCreator.Factory accountCreatorFactory;
   private static AccountAction.Factory accountActionFactory;
-  private static final int PORT = 8090;
-    public String testName;
+  public String testName;
 
   @BeforeAll
   static void beforeAll() throws Exception {
@@ -77,7 +78,7 @@ public class AutoCompleteTest {
     soapServlet.setInitParameter("engine.handler.0", "com.zimbra.cs.service.mail.MailService");
     soapServlet.setInitOrder(2);
     server = new JettyServerFactory().withPort(PORT)
-        .addServlet( "/firstServlet", firstServlet)
+        .addServlet("/firstServlet", firstServlet)
         .addServlet(AccountConstants.USER_SERVICE_URI + "*", soapServlet).create();
     server.start();
   }
@@ -88,15 +89,18 @@ public class AutoCompleteTest {
     com.zextras.mailbox.util.MailboxTestUtil.tearDown();
   }
 
- @Test
- void test3951() throws Exception {
-  Account account = accountCreatorFactory.get().create();
-  Element request = new Element.XMLElement(MailConstants.AUTO_COMPLETE_REQUEST);
-  request.addAttribute("name", " ");
-   final HttpResponse response = soapClient.newRequest().setCaller(account).setSoapBody(request).execute();
-   Assertions.assertEquals(HttpStatus.SC_INTERNAL_SERVER_ERROR, response.getStatusLine().getStatusCode());
-   Assertions.assertTrue(new String(response.getEntity().getContent().readAllBytes(), StandardCharsets.UTF_8).contains("invalid request: name parameter is empty"));
- }
+  @Test
+  void test3951() throws Exception {
+    Account account = accountCreatorFactory.get().create();
+    Element request = new Element.XMLElement(MailConstants.AUTO_COMPLETE_REQUEST);
+    request.addAttribute("name", " ");
+    final HttpResponse response = soapClient.newRequest().setCaller(account).setSoapBody(request)
+        .execute();
+    Assertions.assertEquals(HttpStatus.SC_INTERNAL_SERVER_ERROR,
+        response.getStatusLine().getStatusCode());
+    Assertions.assertTrue(new String(response.getEntity().getContent().readAllBytes(),
+        StandardCharsets.UTF_8).contains("invalid request: name parameter is empty"));
+  }
 
   @Test
   @DisplayName("Account 1, without READ permission on ROOT, requests account 2 contacts, get 500 permission denied.")
@@ -106,15 +110,19 @@ public class AutoCompleteTest {
     final Account account2 = accountCreatorFactory.get().create();
     accountActionFactory.forAccount(account2).grantFolderRightTo(account1, "r",
         Mailbox.ID_FOLDER_CALENDAR);
-    soapClient.executeSoap(account2, new CreateContactRequest(new ContactSpec().addEmail(prefix + UUID.randomUUID() + "something.com")));
-    soapClient.executeSoap(account2, new CreateContactRequest(new ContactSpec().addEmail(prefix + UUID.randomUUID() + "something.com")));
+    soapClient.executeSoap(account2, new CreateContactRequest(
+        new ContactSpec().addEmail(prefix + UUID.randomUUID() + "something.com")));
+    soapClient.executeSoap(account2, new CreateContactRequest(
+        new ContactSpec().addEmail(prefix + UUID.randomUUID() + "something.com")));
     final Element request = JaxbUtil.jaxbToElement(new AutoCompleteRequest(prefix));
     final HttpResponse execute = soapClient.newRequest()
         .setCaller(account1).setRequestedAccount(account2).setSoapBody(request).execute();
     final String responseBody = new String(execute.getEntity().getContent().readAllBytes(),
         StandardCharsets.UTF_8);
-    Assertions.assertEquals(HttpStatus.SC_INTERNAL_SERVER_ERROR, execute.getStatusLine().getStatusCode());
-    Assertions.assertTrue(responseBody.contains("Permission denied: cannot access requested folder"));
+    Assertions.assertEquals(HttpStatus.SC_INTERNAL_SERVER_ERROR,
+        execute.getStatusLine().getStatusCode());
+    Assertions.assertTrue(
+        responseBody.contains("Permission denied: cannot access requested folder"));
   }
 
   @Test
@@ -124,15 +132,19 @@ public class AutoCompleteTest {
     final Account account1 = accountCreatorFactory.get().create();
     final Account account2 = accountCreatorFactory.get().create();
     soapClient.newRequest()
-        .setCaller(account2).setSoapBody(new CreateContactRequest(new ContactSpec().addEmail(prefix + UUID.randomUUID() + "something.com"))).execute();
+        .setCaller(account2).setSoapBody(new CreateContactRequest(
+            new ContactSpec().addEmail(prefix + UUID.randomUUID() + "something.com"))).execute();
     soapClient.newRequest()
-        .setCaller(account2).setSoapBody(new CreateContactRequest(new ContactSpec().addEmail(prefix + UUID.randomUUID() + "something.com"))).execute();
-    final Element request = JaxbUtil.jaxbToElement(new AutoCompleteRequest(prefix));
-    final HttpResponse execute = soapClient.newRequest().setCaller(account1).setRequestedAccount(account2).setSoapBody(request).execute();
+        .setCaller(account2).setSoapBody(new CreateContactRequest(
+            new ContactSpec().addEmail(prefix + UUID.randomUUID() + "something.com"))).execute();
+    final HttpResponse execute = soapClient.newRequest().setCaller(account1)
+        .setRequestedAccount(account2).setSoapBody(new AutoCompleteRequest(prefix)).execute();
     final String responseBody = new String(execute.getEntity().getContent().readAllBytes(),
         StandardCharsets.UTF_8);
-    Assertions.assertEquals(HttpStatus.SC_INTERNAL_SERVER_ERROR, execute.getStatusLine().getStatusCode());
-    Assertions.assertTrue(responseBody.contains("permission denied: can not access account " + account2.getId()));
+    Assertions.assertEquals(HttpStatus.SC_INTERNAL_SERVER_ERROR,
+        execute.getStatusLine().getStatusCode());
+    Assertions.assertTrue(
+        responseBody.contains("permission denied: can not access account " + account2.getId()));
   }
 
   @Test
@@ -141,18 +153,54 @@ public class AutoCompleteTest {
     final String prefix = "test-";
     final Account account1 = accountCreatorFactory.get().create();
     final Account account2 = accountCreatorFactory.get().create();
-    accountActionFactory.forAccount(account2).grantFolderRightTo(account1, "r", Mailbox.ID_FOLDER_ROOT);
+    accountActionFactory.forAccount(account2)
+        .grantFolderRightTo(account1, "r", Mailbox.ID_FOLDER_ROOT);
     soapClient.newRequest()
-        .setCaller(account2).setSoapBody(new CreateContactRequest(new ContactSpec().addEmail(prefix + UUID.randomUUID() + "something.com"))).execute();
+        .setCaller(account2).setSoapBody(new CreateContactRequest(
+            new ContactSpec().addEmail(prefix + UUID.randomUUID() + "something.com"))).execute();
     soapClient.newRequest()
-        .setCaller(account2).setSoapBody(new CreateContactRequest(new ContactSpec().addEmail(prefix + UUID.randomUUID() + "something.com"))).execute();
-    final Element request = JaxbUtil.jaxbToElement(new AutoCompleteRequest(prefix));
-    final HttpResponse execute = soapClient.newRequest().setCaller(account1).setRequestedAccount(account2).setSoapBody(request).execute();
+        .setCaller(account2).setSoapBody(new CreateContactRequest(
+            new ContactSpec().addEmail(prefix + UUID.randomUUID() + "something.com"))).execute();
+    final HttpResponse execute = soapClient.newRequest().setCaller(account1)
+        .setRequestedAccount(account2).setSoapBody(new AutoCompleteRequest(prefix)).execute();
     final String responseBody = new String(execute.getEntity().getContent().readAllBytes(),
         StandardCharsets.UTF_8);
-    final AutoCompleteResponse autoCompleteResponse = JaxbUtil.elementToJaxb(Element.parseXML(responseBody).getElement("Body").getElement(
-        MailConstants.AUTO_COMPLETE_RESPONSE), AutoCompleteResponse.class);
+    final AutoCompleteResponse autoCompleteResponse = JaxbUtil.elementToJaxb(
+        Element.parseXML(responseBody).getElement("Body").getElement(
+            MailConstants.AUTO_COMPLETE_RESPONSE), AutoCompleteResponse.class);
     Assertions.assertEquals(2, autoCompleteResponse.getMatches().size());
+    Assertions.assertEquals(HttpStatus.SC_OK, execute.getStatusLine().getStatusCode());
+  }
+
+  @Test
+  @DisplayName("Account 1, with READ Grants on Contacts and Emailed Contacts, requests autocomplete on account2 7 and 13 folder, gets contacts.")
+  void shouldGetContactsOfSharedAccountWhenSettingFolders() throws Exception {
+    final String prefix = "test-";
+    final Account account1 = accountCreatorFactory.get().create();
+    final Account account2 = accountCreatorFactory.get().create();
+    accountActionFactory.forAccount(account2)
+        .grantFolderRightTo(account1, "r", Mailbox.ID_FOLDER_ROOT);
+    soapClient.newRequest()
+        .setCaller(account2).setSoapBody(new CreateContactRequest(
+            new ContactSpec().addEmail(prefix + UUID.randomUUID() + "something.com"))).execute();
+    soapClient.newRequest()
+        .setCaller(account2).setSoapBody(new CreateContactRequest(
+            new ContactSpec().addEmail(prefix + UUID.randomUUID() + "something.com"))).execute();
+    soapClient.newRequest()
+        .setCaller(account2).setSoapBody(new CreateContactRequest(
+            new ContactSpec().addEmail(prefix + UUID.randomUUID() + "something.com"))).execute();
+
+    final AutoCompleteRequest autoCompleteRequest = new AutoCompleteRequest(prefix);
+    autoCompleteRequest.setFolderList(
+        Mailbox.ID_FOLDER_AUTO_CONTACTS + "," + Mailbox.ID_FOLDER_CONTACTS);
+    final HttpResponse execute = soapClient.newRequest().setCaller(account1)
+        .setRequestedAccount(account2).setSoapBody(autoCompleteRequest).execute();
+    final String responseBody = new String(execute.getEntity().getContent().readAllBytes(),
+        StandardCharsets.UTF_8);
+    final AutoCompleteResponse autoCompleteResponse = JaxbUtil.elementToJaxb(
+        Element.parseXML(responseBody).getElement("Body").getElement(
+            MailConstants.AUTO_COMPLETE_RESPONSE), AutoCompleteResponse.class);
+    Assertions.assertEquals(3, autoCompleteResponse.getMatches().size());
     Assertions.assertEquals(HttpStatus.SC_OK, execute.getStatusLine().getStatusCode());
   }
 
