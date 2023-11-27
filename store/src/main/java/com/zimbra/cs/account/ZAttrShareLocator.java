08b9d76695252fdf921b898413e0c5c26b9b0a89
Davide Frison <davide.frison@zextras.com>
2023-11-27 20:04:52 +0100

    refactor: ZAttrShareLocator remove unused methods

--- store/src/main/java/com/zimbra/cs/account/ZAttrShareLocator.java
+++ store/src/main/java/com/zimbra/cs/account/ZAttrShareLocator.java
@@ -5,11 +5,9 @@
 
 package com.zimbra.cs.account;
 
-import com.zimbra.common.account.ZAttrProvisioning;
-import java.util.HashMap;
-import java.util.Map;
-
 import com.zimbra.common.account.ZAttr;
+import com.zimbra.common.account.ZAttrProvisioning;
+import java.util.Map;
 
 /**
  * AUTO-GENERATED. DO NOT EDIT.
@@ -34,58 +32,6 @@ public abstract class ZAttrShareLocator extends NamedEntry {
     }
 
     /**
-     * RFC2256: common name(s) for which the entity is known by
-     *
-     * @param cn new value
-     * @throws com.zimbra.common.service.ServiceException if error during update
-     */
-    @ZAttr(id=-1)
-    public void setCn(String cn) throws com.zimbra.common.service.ServiceException {
-        HashMap<String,Object> attrs = new HashMap<>();
-        attrs.put(ZAttrProvisioning.A_cn, cn);
-        getProvisioning().modifyAttrs(this, attrs);
-    }
-
-    /**
-     * RFC2256: common name(s) for which the entity is known by
-     *
-     * @param cn new value
-     * @param attrs existing map to populate, or null to create a new map
-     * @return populated map to pass into Provisioning.modifyAttrs
-     */
-    @ZAttr(id=-1)
-    public Map<String,Object> setCn(String cn, Map<String,Object> attrs) {
-        if (attrs == null) attrs = new HashMap<>();
-        attrs.put(ZAttrProvisioning.A_cn, cn);
-        return attrs;
-    }
-
-    /**
-     * RFC2256: common name(s) for which the entity is known by
-     *
-     * @throws com.zimbra.common.service.ServiceException if error during update
-     */
-    @ZAttr(id=-1)
-    public void unsetCn() throws com.zimbra.common.service.ServiceException {
-        HashMap<String,Object> attrs = new HashMap<>();
-        attrs.put(ZAttrProvisioning.A_cn, "");
-        getProvisioning().modifyAttrs(this, attrs);
-    }
-
-    /**
-     * RFC2256: common name(s) for which the entity is known by
-     *
-     * @param attrs existing map to populate, or null to create a new map
-     * @return populated map to pass into Provisioning.modifyAttrs
-     */
-    @ZAttr(id=-1)
-    public Map<String,Object> unsetCn(Map<String,Object> attrs) {
-        if (attrs == null) attrs = new HashMap<>();
-        attrs.put(ZAttrProvisioning.A_cn, "");
-        return attrs;
-    }
-
-    /**
      * account ID of the owner of the shared folder
      *
      * @return zimbraShareOwnerAccountId, or null if unset
@@ -95,66 +41,6 @@ public abstract class ZAttrShareLocator extends NamedEntry {
     @ZAttr(id=1375)
     public String getShareOwnerAccountId() {
         return getAttr(ZAttrProvisioning.A_zimbraShareOwnerAccountId, null, true);
-    }
-
-    /**
-     * account ID of the owner of the shared folder
-     *
-     * @param zimbraShareOwnerAccountId new value
-     * @throws com.zimbra.common.service.ServiceException if error during update
-     *
-     * @since ZCS 8.0.0
-     */
-    @ZAttr(id=1375)
-    public void setShareOwnerAccountId(String zimbraShareOwnerAccountId) throws com.zimbra.common.service.ServiceException {
-        HashMap<String,Object> attrs = new HashMap<>();
-        attrs.put(ZAttrProvisioning.A_zimbraShareOwnerAccountId, zimbraShareOwnerAccountId);
-        getProvisioning().modifyAttrs(this, attrs);
-    }
-
-    /**
-     * account ID of the owner of the shared folder
-     *
-     * @param zimbraShareOwnerAccountId new value
-     * @param attrs existing map to populate, or null to create a new map
-     * @return populated map to pass into Provisioning.modifyAttrs
-     *
-     * @since ZCS 8.0.0
-     */
-    @ZAttr(id=1375)
-    public Map<String,Object> setShareOwnerAccountId(String zimbraShareOwnerAccountId, Map<String,Object> attrs) {
-        if (attrs == null) attrs = new HashMap<>();
-        attrs.put(ZAttrProvisioning.A_zimbraShareOwnerAccountId, zimbraShareOwnerAccountId);
-        return attrs;
-    }
-
-    /**
-     * account ID of the owner of the shared folder
-     *
-     * @throws com.zimbra.common.service.ServiceException if error during update
-     *
-     * @since ZCS 8.0.0
-     */
-    @ZAttr(id=1375)
-    public void unsetShareOwnerAccountId() throws com.zimbra.common.service.ServiceException {
-        HashMap<String,Object> attrs = new HashMap<>();
-        attrs.put(ZAttrProvisioning.A_zimbraShareOwnerAccountId, "");
-        getProvisioning().modifyAttrs(this, attrs);
-    }
-
-    /**
-     * account ID of the owner of the shared folder
-     *
-     * @param attrs existing map to populate, or null to create a new map
-     * @return populated map to pass into Provisioning.modifyAttrs
-     *
-     * @since ZCS 8.0.0
-     */
-    @ZAttr(id=1375)
-    public Map<String,Object> unsetShareOwnerAccountId(Map<String,Object> attrs) {
-        if (attrs == null) attrs = new HashMap<>();
-        attrs.put(ZAttrProvisioning.A_zimbraShareOwnerAccountId, "");
-        return attrs;
     }
 
     ///// END-AUTO-GEN-REPLACE
