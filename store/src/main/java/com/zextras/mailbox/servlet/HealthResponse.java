21dc82eaf1a910ca26a11aab7dc732668b6d7aae
Keshav Bhatt <keshavnrj@gmail.com>
2023-12-08 15:49:57 +0530

    chore: refactor HealthResponse
    - improve builder
    - add javadoc

--- store/src/main/java/com/zextras/mailbox/servlet/HealthResponse.java
+++ store/src/main/java/com/zextras/mailbox/servlet/HealthResponse.java
@@ -10,48 +10,89 @@ import com.zextras.mailbox.health.ServiceDependency.ServiceType;
 import java.util.ArrayList;
 import java.util.List;
 
+
+/**
+ * Represents response body of {@link HealthServlet}
+ */
 public class HealthResponse {
 
-  public HealthResponse(boolean ready,
-      List<Dependency> dependencies) {
+  @SuppressWarnings("unused")
+  @JsonProperty("ready")
+  private final boolean ready;
+
+  @SuppressWarnings("unused")
+  @JsonProperty("dependencies")
+  private final List<Dependency> dependencies;
+
+  public HealthResponse(boolean ready, List<Dependency> dependencies) {
     this.ready = ready;
     this.dependencies = dependencies;
   }
 
-  static class Builder {
-    private boolean ready = false;
+  /**
+   * <p>Fluent Builder for {@link HealthResponse} class</p>
+   * <p>
+   * Usage:
+   * <pre>
+   * {@code
+   *   HealthResponse response = new HealthResponseBuilder.newInstance().
+   *   ...
+   *   ...
+   *   .build();
+   * }
+   * </pre>
+   */
+  static class HealthResponseBuilder {
+
     private final List<Dependency> dependencies = new ArrayList<>();
+    private boolean ready = false;
 
-    Builder withReadiness(boolean isReady) {
-      this.ready = isReady;
-      return this;
+    private HealthResponseBuilder() {
+      // to enforce the use of the static factory method newInstance
     }
 
-    Builder withDependency(Dependency dependency) {
-      this.dependencies.add(dependency);
-      return this;
-    }
-
-    Builder withDependency(ServiceDependency serviceDependency) {
-      final Dependency dependency = new Dependency(serviceDependency.getName(),
-          serviceDependency.getType(), serviceDependency.isReady(), serviceDependency.isLive());
-      this.dependencies.add(dependency);
-      return this;
+    public static HealthResponseBuilder newInstance() {
+      return new HealthResponseBuilder();
     }
 
     public HealthResponse build() {
       return new HealthResponse(this.ready, this.dependencies);
     }
 
+    HealthResponseBuilder withReadiness(boolean isReady) {
+      this.ready = isReady;
+      return this;
+    }
+
+    HealthResponseBuilder withDependencies(List<ServiceDependency> serviceDependencies) {
+      this.dependencies.clear();
+      serviceDependencies.forEach(this::addServiceDependency);
+      return this;
+    }
+
+    HealthResponseBuilder withDependency(Dependency dependency) {
+      this.dependencies.add(dependency);
+      return this;
+    }
+
+    HealthResponseBuilder withDependency(ServiceDependency serviceDependency) {
+      this.addServiceDependency(serviceDependency);
+      return this;
+    }
+
+    private void addServiceDependency(ServiceDependency serviceDependency) {
+      final Dependency dependency = new Dependency(serviceDependency.getName(),
+          serviceDependency.getType(), serviceDependency.isReady(), serviceDependency.isLive());
+      this.dependencies.add(dependency);
+    }
+
   }
 
-  @JsonProperty("ready")
-  private final boolean ready;
-
-  @JsonProperty("dependencies")
-  private final List<Dependency> dependencies;
-
-  public static class Dependency {
+  /**
+   * Mapper class for {@link ServiceDependency} implementations
+   */
+  @SuppressWarnings("unused")
+  static class Dependency {
 
     @JsonProperty("name")
     private final String name;
@@ -72,6 +113,4 @@ public class HealthResponse {
       this.live = live;
     }
   }
-
-
 }
