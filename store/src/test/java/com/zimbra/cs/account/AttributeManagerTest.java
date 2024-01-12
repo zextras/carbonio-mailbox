494072852c6c56b3a54eb841195e96d9b4f81e24
Keshav Bhatt <keshavnrj@gmail.com>
2024-01-12 20:04:57 +0530

    chore: cleanup test class

--- store/src/test/java/com/zimbra/cs/account/AttributeManagerTest.java
+++ store/src/test/java/com/zimbra/cs/account/AttributeManagerTest.java
@@ -3,71 +3,32 @@ package com.zimbra.cs.account;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import com.google.common.collect.Sets;
+import com.zimbra.common.localconfig.LC;
 import com.zimbra.common.service.ServiceException;
 import java.util.Map;
-import java.util.Set;
 import org.junit.jupiter.api.Test;
 
 class AttributeManagerTest {
 
   @Test
-  void addAttributeShouldAddAttribute() {
-    final AttributeManager attributeManager = new AttributeManager();
-    Set<AttributeClass> requiredIn =
-        Sets.newHashSet(
-            AttributeClass.account,
-            AttributeClass.alias,
-            AttributeClass.distributionList,
-            AttributeClass.domain,
-            AttributeClass.server,
-            AttributeClass.cos,
-            AttributeClass.xmppComponent,
-            AttributeClass.group,
-            AttributeClass.groupDynamicUnit,
-            AttributeClass.groupStaticUnit);
-    Set<AttributeFlag> flags = Sets.newHashSet(AttributeFlag.accountInfo);
-    AttributeInfo attributeInfo =
-        new AttributeInfo(
-            "zimbraId",
-            1,
-            null,
-            0,
-            null,
-            AttributeType.TYPE_ID,
-            null,
-            "",
-            true,
-            null,
-            null,
-            AttributeCardinality.single,
-            requiredIn,
-            null,
-            flags,
-            null,
-            null,
-            null,
-            null,
-            null,
-            "Zimbra Systems Unique ID",
-            null,
-            null,
-            null);
-    attributeManager.addAttribute(attributeInfo);
+  void shouldLoadAllAttributesFromAttrsFile() throws ServiceException {
+    final AttributeManager attributeManager = new AttributeManager("conf/attrs");
     final Map<String, AttributeInfo> allAttrs = attributeManager.getAttrs();
 
-    assertEquals(1, allAttrs.size());
-
-    for (String key : allAttrs.keySet()) {
-      assertEquals("zimbraId", allAttrs.get(key).getName());
-    }
+    assertLoadedAllAttributes(allAttrs);
   }
 
   @Test
-  void shouldLoadAllAttributesFromAttrsFile() throws ServiceException {
-    final AttributeManager attributeManager = new AttributeManager("../store/conf/attrs");
+  void shouldLoadAllAttributesWhenUsingSingleton() throws ServiceException {
+    LC.zimbra_attrs_directory.setDefault("conf/attrs");
+
+    final AttributeManager attributeManager = AttributeManager.getInstance();
     final Map<String, AttributeInfo> allAttrs = attributeManager.getAttrs();
 
+    assertLoadedAllAttributes(allAttrs);
+  }
+
+  private void assertLoadedAllAttributes(Map<String, AttributeInfo> allAttrs) {
     assertEquals(1841, allAttrs.size());
   }
 }
