46034a22be4678a49e30dc820fd76821cff85ffa
Davide Frison <36330277+frisonisland@users.noreply.github.com>
2024-01-26 09:11:19 +0100

    chore(docker): move from ldap-server to carbonio-ce-directory-server (#433)
    * chore(docker): move from ldap-server to carbonio-ce-directory-server
    
    - image was renamed
    - updated checks
    - updated hostname to match new one defined during bootstrap
    
    * chore: remove references and dockerenv from entrypoint-ldap.sh

--- docker/jetty-run/entrypoint-ldap.sh
+++ docker/jetty-run/entrypoint-ldap.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-# The container 'carbonio/ce-ldap-u20' is already bootstrapped with:
+# The container is already bootstrapped with:
 # - server: "ldap.mail.local" configured with zimbraServiceEnabled 'directory-server' and some other stuff.
 # - domain: "mail.local"
 # We only need to:
 # - update the schemas and the configurations with those we have just built
 # - create an appserver, a domain and a couple of accounts
 
-touch /.dockerenv
 su - zextras -c "/opt/zextras/bin/ldap stop"
 su - zextras -c "/opt/zextras/libexec/zmldapschema 2>/dev/null"
 su - zextras -c "/opt/zextras/libexec/zmldapupdateldif"
@@ -17,4 +16,4 @@ su - zextras -c "/opt/zextras/bin/zmprov createDomain demo.zextras.io"
 su - zextras -c "/opt/zextras/bin/zmprov createServer docker.app.local zimbraServiceEnabled service zimbraServiceEnabled mailbox"
 su - zextras -c "/opt/zextras/bin/zmprov createAccount docker-admin@demo.zextras.io admin-password zimbraMailHost docker.app.local zimbraIsAdminAccount TRUE"
 su - zextras -c "/opt/zextras/bin/zmprov createAccount docker-user@demo.zextras.io user-password zimbraMailHost docker.app.local"
-monit -c /etc/monitrc
+tail -f /dev/null
