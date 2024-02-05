7b762274b19a9cd82ad353131d1d2630e033b33b
Soner Sivri <sonersivri@gmail.com>
2024-02-05 14:29:35 +0300

    chore: sonarqube fix LdapUtilTest

--- store/src/test/java/com/zimbra/cs/ldap/LdapUtilTest.java
+++ store/src/test/java/com/zimbra/cs/ldap/LdapUtilTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
-public class LdapUtilTest {
+class LdapUtilTest {
 
     @Test
     void validateZimbraId_throws_IllegalArgumentException_when_length_is_too_large() {
