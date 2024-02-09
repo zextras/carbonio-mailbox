8c1b217fb034dae8d19b0c94aecab175c520f427
Davide Frison <davide.frison@zextras.com>
2024-02-09 11:38:19 +0100

    refactor: use inmemory directory server builder

---
+++ store/src/test/java/com/zextras/mailbox/util/InMemoryLdapServer.java
@@ -0,0 +1,91 @@
+// SPDX-FileCopyrightText: 2023 Zextras <https://www.zextras.com>
+//
+// SPDX-License-Identifier: GPL-2.0-only
+
+package com.zextras.mailbox.util;
+
+import com.unboundid.ldap.listener.InMemoryDirectoryServer;
+import com.unboundid.ldap.listener.InMemoryDirectoryServerConfig;
+import com.unboundid.ldap.listener.InMemoryListenerConfig;
+import com.unboundid.ldap.sdk.LDAPException;
+import com.unboundid.ldap.sdk.Modification;
+import com.unboundid.ldap.sdk.ModificationType;
+import com.zimbra.common.localconfig.LC;
+
+/** Util class to create an {@link InMemoryDirectoryServer} */
+public class InMemoryLdapServer {
+
+  private final InMemoryDirectoryServer inMemoryDirectoryServer;
+  private final String adminPassword;
+
+  public void clear() {
+    inMemoryDirectoryServer.clear();
+  }
+
+  public void shutDown(boolean shutdown) {
+    inMemoryDirectoryServer.shutDown(shutdown);
+  }
+
+  public static class Builder {
+    private int ldapPort = 389;
+    private String ldapRootPassword = LC.ldap_root_password.value();
+    private String adminPassword = LC.zimbra_ldap_password.value();
+
+    public Builder withLdapPort(int ldapPort) {
+      this.ldapPort = ldapPort;
+      return this;
+    }
+
+    public Builder withLdapRootPassword(String ldapRootPassword) {
+      this.ldapRootPassword = ldapRootPassword;
+      return this;
+    }
+
+    public Builder withAdminPassword(String adminPassword) {
+      this.adminPassword = adminPassword;
+      return this;
+    }
+
+    InMemoryLdapServer create() throws Exception {
+      InMemoryDirectoryServerConfig ldapServerConfig =
+          new InMemoryDirectoryServerConfig(
+              "dc=com",
+              "cn=config",
+              "cn=defaultExternal,cn=cos,cn=zimbra",
+              "cn=default,cn=cos,cn=zimbra",
+              "cn=config,cn=zimbra",
+              "cn=zimbra");
+      ldapServerConfig.addAdditionalBindCredentials("cn=config", ldapRootPassword);
+      ldapServerConfig.setListenerConfigs(InMemoryListenerConfig.createLDAPConfig("LDAP", ldapPort));
+      ldapServerConfig.setSchema(null);
+      ldapServerConfig.setGenerateOperationalAttributes(true);
+
+      InMemoryDirectoryServer inMemoryDirectoryServer = new InMemoryDirectoryServer(ldapServerConfig);
+      return new InMemoryLdapServer(inMemoryDirectoryServer, adminPassword);
+    }
+  }
+
+  public void initializeBasicData() throws LDAPException {
+    inMemoryDirectoryServer.importFromLDIF(true, "./build/ldap/config/cn=config.ldif");
+    inMemoryDirectoryServer.importFromLDIF(false, "./build/ldap/zimbra_globalconfig.ldif");
+    inMemoryDirectoryServer.importFromLDIF(false, "./build/ldap/zimbra_defaultcos.ldif");
+    inMemoryDirectoryServer.importFromLDIF(false, "./build/ldap/zimbra_defaultexternalcos.ldif");
+    inMemoryDirectoryServer.importFromLDIF(false, "./build/ldap/carbonio.ldif");
+    inMemoryDirectoryServer
+        .getConnection()
+        .modify(
+            "uid=zimbra,cn=admins,cn=zimbra",
+            new Modification(
+                ModificationType.REPLACE, "userPassword", adminPassword));
+  }
+
+  private InMemoryLdapServer(
+      InMemoryDirectoryServer inMemoryDirectoryServer, String adminPassword) {
+    this.inMemoryDirectoryServer = inMemoryDirectoryServer;
+    this.adminPassword = adminPassword;
+  }
+
+  public void start() throws LDAPException {
+    inMemoryDirectoryServer.startListening();
+  }
+}
