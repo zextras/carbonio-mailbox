8652cd00f8224e34ec9a43e09df04d26d0435290
Keshav Bhatt <keshavnrj@gmail.com>
2024-04-02 19:39:45 +0530

    feat: [CO-982] add orderedAccountIds element in FullAutocompleteRequest

--- soap/src/main/java/com/zimbra/soap/mail/message/FullAutocompleteRequest.java
+++ soap/src/main/java/com/zimbra/soap/mail/message/FullAutocompleteRequest.java
@@ -5,8 +5,6 @@
 package com.zimbra.soap.mail.message;
 
 import com.zimbra.common.soap.MailConstants;
-import java.util.ArrayList;
-import java.util.List;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlElement;
@@ -15,16 +13,25 @@ import javax.xml.bind.annotation.XmlRootElement;
 /**
  * @zm-api-command-auth-required true
  * @zm-api-command-admin-auth-required false
- * @zm-api-command-description FullAutoComplete
- * This API executes an AutoComplete on current logged in account and all extra requested accounts.
- * If one of the requests in extra accounts fails, the whole API will fail.
- * The API returns the same content of AutoComplete by merging the result and discarding duplicates
- * following the order of requested accounts, starting from logged user response.
- *
+ * @zm-api-command-description FullAutoComplete This API executes an AutoComplete on current logged in account and all
+ * extra requested accounts. If one of the requests in extra accounts fails, the whole API will fail. The API returns
+ * the same content of AutoComplete by merging the result and discarding duplicates following the order of requested
+ * accounts, starting from logged user response.
  */
 @XmlAccessorType(XmlAccessType.NONE)
-@XmlRootElement(name= MailConstants.E_FULL_AUTO_COMPLETE_REQUEST)
+@XmlRootElement(name = MailConstants.E_FULL_AUTO_COMPLETE_REQUEST)
 public class FullAutocompleteRequest {
+
+  @XmlElement(name = MailConstants.E_AUTO_COMPLETE_REQUEST, required = true)
+  private AutoCompleteRequest autoCompleteRequest;
+  /**
+   * @zm-api-field-tag orderedAccountIds
+   * @zm-api-field-description ordered, comma seperated list of account IDs whose matches will be included in the
+   * response.
+   */
+  @XmlElement(name = MailConstants.E_ORDERED_ACCOUNT_IDS, required = false)
+  private String orderedAccountIds;
+
 
   public FullAutocompleteRequest() {
   }
@@ -32,10 +39,6 @@ public class FullAutocompleteRequest {
   public FullAutocompleteRequest(AutoCompleteRequest autoCompleteRequest) {
     this.autoCompleteRequest = autoCompleteRequest;
   }
-
-
-  @XmlElement(name=MailConstants.E_AUTO_COMPLETE_REQUEST, required=true)
-  private AutoCompleteRequest autoCompleteRequest;
 
   public AutoCompleteRequest getAutoCompleteRequest() {
     return autoCompleteRequest;
@@ -45,18 +48,11 @@ public class FullAutocompleteRequest {
     this.autoCompleteRequest = autoCompleteRequest;
   }
 
-  /**
-   * @zm-api-field-tag extraAccountIds
-   * @zm-api-field-description Extra accounts where to execute the autocomplete on
-   */
-  @XmlElement(name=MailConstants.E_EXTRA_ACCOUNT_ID, required=false)
-  private final List<String> extraAccountIds = new ArrayList<>();
-
-  public void addAccount(String account) {
-    this.extraAccountIds.add(account);
+  public String getOrderedAccountIds() {
+    return orderedAccountIds;
   }
 
-  public List<String> getExtraAccountIds() {
-    return extraAccountIds;
+  public void setOrderedAccountIds(String orderedAccountIds) {
+    this.orderedAccountIds = orderedAccountIds;
   }
 }
