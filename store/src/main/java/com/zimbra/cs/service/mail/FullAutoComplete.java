7ec24bd3ed9398d84794737f31487d18712888e8
Keshav Bhatt <keshavnrj@gmail.com>
2024-04-04 21:51:29 +0530

    feat: various improvement and handling of corner cases in FullAutoComplete
    - avoid parsing duplicate account IDs in parsePreferredAccountsFrom helper method
    
    - do fail the whole FullAutoComplete if one of the remote call fails
    
    - consider matches from all asked accounts instead bailing out once match limit is reached

--- store/src/main/java/com/zimbra/cs/service/mail/FullAutoComplete.java
+++ store/src/main/java/com/zimbra/cs/service/mail/FullAutoComplete.java
@@ -20,6 +20,8 @@ import io.vavr.Tuple2;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
 
@@ -35,49 +37,44 @@ public class FullAutoComplete extends MailDocumentHandler {
     final var fullAutocompleteRequest = getFullAutocompleteRequestFrom(request);
     final var fullAutoCompleteMatches = new ArrayList<AutoCompleteMatch>();
     final var otherAutoCompleteMatches = new ArrayList<AutoCompleteMatch>();
+    final var authenticatedAccount = zsc.getAuthToken().getAccount();
+    final int contactAutoCompleteMaxResultsLimit = authenticatedAccount.getContactAutoCompleteMaxResults();
 
     try {
       final var autoCompleteRequest = fullAutocompleteRequest.getAutoCompleteRequest();
-      final var authenticatedAccount = zsc.getAuthToken().getAccount();
       final var zAuthToken = zsc.getAuthToken().toZAuthToken();
-      final int contactAutoCompleteMaxResultsLimit = authenticatedAccount.getContactAutoCompleteMaxResults();
       final var parsedAccountIds = parsePreferredAccountsFrom(
           fullAutocompleteRequest.getOrderedAccountIds());
       final var preferredAccountId = parsedAccountIds._1();
       final var otherPreferredAccountIds = parsedAccountIds._2();
 
       // process matches from "preferred account"
-      final var preferredAccountAutoCompleteResponse = doAutoCompleteOnAccount(authenticatedAccount,
-          zAuthToken, preferredAccountId, autoCompleteRequest);
-      for (var autoCompleteMatch : preferredAccountAutoCompleteResponse.getMatches()) {
-        if (contactAutoCompleteMaxResultsLimit > 0
-            && fullAutoCompleteMatches.size() >= contactAutoCompleteMaxResultsLimit) {
-          break;
-        }
-        fullAutoCompleteMatches.add(autoCompleteMatch);
-      }
+      doAutoCompleteOnAccount(authenticatedAccount, zAuthToken, preferredAccountId, autoCompleteRequest).getMatches()
+          .stream()
+          .takeWhile(autoCompleteMatch -> contactAutoCompleteMaxResultsLimit <= 0
+              || fullAutoCompleteMatches.size() < contactAutoCompleteMaxResultsLimit)
+          .forEachOrdered(fullAutoCompleteMatches::add);
 
       // process matches from "other preferred accounts"
-      for (var otherAccountId : otherPreferredAccountIds) {
-        final var otherAccountAutoCompleteResponse = doAutoCompleteOnAccount(authenticatedAccount,
-            zAuthToken, otherAccountId, autoCompleteRequest);
-        for (var autoCompleteMatch : otherAccountAutoCompleteResponse.getMatches()) {
-          if (contactAutoCompleteMaxResultsLimit > 0
-              && fullAutoCompleteMatches.size() + otherAutoCompleteMatches.size()
-              >= contactAutoCompleteMaxResultsLimit) {
-            break;
-          }
-          if (!fullAutoCompleteMatches.contains(autoCompleteMatch)) {
-            otherAutoCompleteMatches.add(autoCompleteMatch);
-          }
-        }
-      }
+      otherPreferredAccountIds.stream()
+          .map(otherAccountId -> doAutoCompleteOnAccount(authenticatedAccount, zAuthToken, otherAccountId,
+              autoCompleteRequest))
+          .forEachOrdered(otherAccountAutoCompleteResponse -> otherAccountAutoCompleteResponse.getMatches().stream()
+              .filter(autoCompleteMatch -> fullAutoCompleteMatches.stream()
+                  .noneMatch(m -> m.getEmail().equalsIgnoreCase(autoCompleteMatch.getEmail())))
+              .forEachOrdered(otherAutoCompleteMatches::add));
+
       otherAutoCompleteMatches.sort(Comparator.comparing(AutoCompleteMatch::getRanking).reversed()
           .thenComparing(AutoCompleteMatch::getEmail));
-    } catch (ServiceException | IOException e) {
+    } catch (ServiceException e) {
       throw ServiceException.FAILURE(e.getMessage());
     }
-    fullAutoCompleteMatches.addAll(otherAutoCompleteMatches);
+
+    otherAutoCompleteMatches.stream()
+        .filter(autoCompleteMatch -> fullAutoCompleteMatches.stream()
+            .noneMatch(m -> m.getEmail().equalsIgnoreCase(autoCompleteMatch.getEmail())))
+        .limit(Math.max(contactAutoCompleteMaxResultsLimit - fullAutoCompleteMatches.size(), 0))
+        .forEach(fullAutoCompleteMatches::add);
 
     return fullAutoCompleteResponseFor(fullAutoCompleteMatches, false);
   }
@@ -124,50 +121,56 @@ public class FullAutoComplete extends MailDocumentHandler {
    * @param autoCompleteRequest  The original {@link AutoCompleteRequest} element that will be used to perform {@link
    *                             AutoComplete} SOAP call
    * @return {@link AutoCompleteResponse}
-   * @throws ServiceException If something goes wrong.
-   * @throws IOException      If an I/O error occurs during the request transport.
    */
   private AutoCompleteResponse doAutoCompleteOnAccount(Account authenticatedAccount, ZAuthToken zAuthToken,
-      String requestedAccountId, AutoCompleteRequest autoCompleteRequest)
-      throws ServiceException, IOException {
-    final var soapUrl = URLUtil.getSoapURL(authenticatedAccount.getServer(), true);
-    final var autocompleteRequestElement = JaxbUtil.jaxbToElement(autoCompleteRequest);
+      String requestedAccountId, AutoCompleteRequest autoCompleteRequest) {
+    try {
+      final var soapUrl = URLUtil.getSoapURL(authenticatedAccount.getServer(), true);
+      final var autocompleteRequestElement = JaxbUtil.jaxbToElement(autoCompleteRequest);
 
-    final AutoCompleteResponse autoCompleteResponse;
-    if (authenticatedAccount.getId().equalsIgnoreCase(requestedAccountId)) {
-      autoCompleteResponse = JaxbUtil.elementToJaxb(new SoapHttpTransport(zAuthToken, soapUrl).invoke(
-          autocompleteRequestElement), AutoCompleteResponse.class);
-    } else {
-      autoCompleteResponse = JaxbUtil.elementToJaxb(new SoapHttpTransport(zAuthToken, soapUrl).invoke(
-          autocompleteRequestElement, requestedAccountId));
+      final AutoCompleteResponse autoCompleteResponse;
+      if (authenticatedAccount.getId().equalsIgnoreCase(requestedAccountId)) {
+        autoCompleteResponse = JaxbUtil.elementToJaxb(new SoapHttpTransport(zAuthToken, soapUrl).invoke(
+            autocompleteRequestElement), AutoCompleteResponse.class);
+      } else {
+        autoCompleteResponse = JaxbUtil.elementToJaxb(new SoapHttpTransport(zAuthToken, soapUrl).invoke(
+            autocompleteRequestElement, requestedAccountId));
+      }
+      return autoCompleteResponse;
+    } catch (ServiceException | IOException ignored) {
+      // TODO add logging
+      return new AutoCompleteResponse();
     }
-    return autoCompleteResponse;
   }
 
   /**
    * Parses {@link com.zimbra.common.soap.MailConstants#E_ORDERED_ACCOUNT_IDS} into a {@link Tuple2} object containing
-   * first object as "Preferred Account" and second object as "Other Preferred Account"
+   * the "preferred account" and "other preferred accounts". Duplicates account IDs are omitted.
    *
-   * @param preferredAccountsStr {@link String} containing comma seperated ordered list of accounts IDs
-   * @return a tuple containing first object as "Preferred Account" and second object as "Other Preferred Account".
+   * @param preferredAccountsStr A {@link String} containing a comma-separated ordered list of account IDs.
+   * @return A tuple containing the preferred account and other preferred accounts.
    */
-  Tuple2<String, List<String>> parsePreferredAccountsFrom(String preferredAccountsStr) {
-    String preferredAccount = null;
-    final var otherAccounts = new ArrayList<String>();
+  Tuple2<String, LinkedHashSet<String>> parsePreferredAccountsFrom(String preferredAccountsStr) {
+    if (preferredAccountsStr == null || preferredAccountsStr.isEmpty()) {
+      return new Tuple2<>(null, new LinkedHashSet<>());
+    }
 
-    if (preferredAccountsStr != null && !preferredAccountsStr.isEmpty()) {
-      String[] tokens = preferredAccountsStr.split(",");
-      for (String token : tokens) {
-        String trimmedToken = token.trim();
-        if (!trimmedToken.isEmpty()) {
-          if (preferredAccount == null) {
-            preferredAccount = trimmedToken;
-          } else {
-            otherAccounts.add(trimmedToken);
-          }
+    final var seenTokens = new HashSet<String>();
+    final var otherAccounts = new LinkedHashSet<String>();
+    String preferredAccount = null;
+
+    for (var token : preferredAccountsStr.split(",")) {
+      var trimmedToken = token.trim();
+      if (!trimmedToken.isEmpty() && !seenTokens.contains(trimmedToken)) {
+        if (preferredAccount == null) {
+          preferredAccount = trimmedToken;
+        } else {
+          otherAccounts.add(trimmedToken);
         }
+        seenTokens.add(trimmedToken);
       }
     }
+
     return new Tuple2<>(preferredAccount, otherAccounts);
   }
 }
