855d60b01319aa19dfc3fd568bdfb0cc877ab5ce
Keshav Bhatt <keshavnrj@gmail.com>
2024-04-04 22:50:45 +0530

    chore: remove imports from AutoCompleteMatch

--- soap/src/main/java/com/zimbra/soap/mail/type/AutoCompleteMatch.java
+++ soap/src/main/java/com/zimbra/soap/mail/type/AutoCompleteMatch.java
@@ -5,7 +5,6 @@
 
 package com.zimbra.soap.mail.type;
 
-import java.util.Objects;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlAttribute;
