896d1b66019c6831351033194f6226a1eceaeb07
Davide Frison <36330277+frisonisland@users.noreply.github.com>
2024-04-09 14:06:11 +0200

    feat: [ZIF-1008] add carbonio send analytics check (#478)
    * feat: add check on carbonioSendAnalytics when calling matomo (smartlinks)
    
    * test(refactor): CreateSmartLinksTest refactor/rearrange methods, vars
    
    * refactor: CreateSmartLinks extract private method send/track analytics

--- store/src/test/java/com/zimbra/cs/service/mail/CreateSmartLinksTest.java
+++ store/src/test/java/com/zimbra/cs/service/mail/CreateSmartLinksTest.java
@@ -4,16 +4,25 @@
 
 package com.zimbra.cs.service.mail;
 
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
 import com.zextras.mailbox.smartlinks.SmartLinksGenerator;
 import com.zextras.mailbox.tracking.Event;
 import com.zextras.mailbox.tracking.Tracking;
 import com.zextras.mailbox.util.MailboxTestUtil;
 import com.zextras.mailbox.util.MailboxTestUtil.AccountCreator;
+import com.zimbra.common.service.ServiceException;
 import com.zimbra.common.soap.Element;
 import com.zimbra.cs.account.Account;
+import com.zimbra.cs.account.Provisioning;
 import com.zimbra.soap.JaxbUtil;
 import com.zimbra.soap.mail.message.CreateSmartLinksRequest;
 import com.zimbra.soap.mail.type.AttachmentToConvert;
+import java.util.List;
+import java.util.Map;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -21,17 +30,12 @@ import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Mockito;
 
-import java.util.List;
-import java.util.Map;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-
 class CreateSmartLinksTest {
+
   private static Account account;
   private Tracking tracking;
   private SmartLinksGenerator smartLinksGenerator;
+  private Provisioning provisioning;
 
   @BeforeAll
   static void setupAll() throws Exception {
@@ -39,27 +43,25 @@ class CreateSmartLinksTest {
     account = AccountCreator.Factory.getDefault().get().create();
   }
 
-  @BeforeEach
-  void setup() {
-    tracking = Mockito.mock(Tracking.class);
-    smartLinksGenerator = Mockito.mock(SmartLinksGenerator.class);
-  }
-
   @AfterAll
   static void tearDownAll() throws Exception {
     MailboxTestUtil.tearDown();
   }
 
+  @BeforeEach
+  void setup() {
+    tracking = Mockito.mock(Tracking.class);
+    smartLinksGenerator = Mockito.mock(SmartLinksGenerator.class);
+    provisioning = Provisioning.getInstance();
+  }
+
   @Test
   void shouldCallTrackingWithCorrectParams() throws Exception {
-    final CreateSmartLinks createSmartLinks = new CreateSmartLinks(smartLinksGenerator, tracking);
-    final Element request = JaxbUtil.jaxbToElement(
-        new CreateSmartLinksRequest(
-            List.of(new AttachmentToConvert("1", "2"))
-        ));
+    final CreateSmartLinks createSmartLinks = new CreateSmartLinks(provisioning,
+        smartLinksGenerator, tracking);
     final Map<String, Object> requestContext = ServiceTestUtil.getRequestContext(account);
 
-    createSmartLinks.handle(request, requestContext);
+    createSmartLinks.handle(aCreateSmartLinkRequest(), requestContext);
 
     ArgumentCaptor<Event> eventCaptor = ArgumentCaptor.forClass(Event.class);
     verify(tracking, times(1)).sendEventIgnoringFailure(eventCaptor.capture());
@@ -67,5 +69,36 @@ class CreateSmartLinksTest {
     assertEquals("Mail", receivedEvent.getCategory());
     assertEquals("SendEmailWithSmartLink", receivedEvent.getAction());
     assertEquals(account.getId(), receivedEvent.getUserId());
+  }
+
+  @Test
+  void shouldNotCallTrackingIfCarbonioSendAnalyticsFalse() throws Exception {
+    final CreateSmartLinks createSmartLinks = new CreateSmartLinks(provisioning,
+        smartLinksGenerator, tracking);
+    provisioning.getConfig().setCarbonioSendAnalytics(false);
+    final Map<String, Object> requestContext = ServiceTestUtil.getRequestContext(account);
+
+    createSmartLinks.handle(aCreateSmartLinkRequest(), requestContext);
+
+    verify(tracking, times(0)).sendEventIgnoringFailure(any());
+  }
+
+  @Test
+  void shouldCallTrackingIfCarbonioSendAnalyticsIsNotSet() throws Exception {
+    final CreateSmartLinks createSmartLinks = new CreateSmartLinks(provisioning,
+        smartLinksGenerator, tracking);
+    provisioning.getConfig().unsetCarbonioSendAnalytics();
+    final Map<String, Object> requestContext = ServiceTestUtil.getRequestContext(account);
+
+    createSmartLinks.handle(aCreateSmartLinkRequest(), requestContext);
+
+    verify(tracking, times(1)).sendEventIgnoringFailure(any());
+  }
+
+  private Element aCreateSmartLinkRequest() throws ServiceException {
+    return JaxbUtil.jaxbToElement(
+        new CreateSmartLinksRequest(
+            List.of(new AttachmentToConvert("1", "2"))
+        ));
   }
 }
