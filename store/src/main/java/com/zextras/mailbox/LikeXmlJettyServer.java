ec530fe359b395a1ac4f1f2647f38eefb14cc97b
Davide Frison <davide.frison@zextras.com>
2024-04-11 20:16:44 +0200

    chore: working https connection
    - ssl was setup incorrectly

--- store/src/main/java/com/zextras/mailbox/LikeXmlJettyServer.java
+++ store/src/main/java/com/zextras/mailbox/LikeXmlJettyServer.java
@@ -6,6 +6,7 @@ package com.zextras.mailbox;
 
 import com.zimbra.common.jetty.JettyMonitor;
 import javax.servlet.DispatcherType;
+import org.eclipse.jetty.http.HttpVersion;
 import org.eclipse.jetty.rewrite.handler.MsieSslRule;
 import org.eclipse.jetty.rewrite.handler.RewriteHandler;
 import org.eclipse.jetty.rewrite.handler.RewritePatternRule;
@@ -42,12 +43,19 @@ public class LikeXmlJettyServer {
   public static class Builder {
 
     private String webDescriptor;
+    private HttpConfiguration httpsConfig;
+    private SslContextFactory sslContextFactory;
+    private ContextHandlerCollection contexts;
 
     public Server build() throws Exception {
       ThreadPool threadPool = createThreadPool();
       Server server = new Server(threadPool);
 
-      final ServerConnector userHttpConnector = createUserHttpConnector(server);
+      final HttpConfiguration httpConfig = createHttpConfig();
+      this.httpsConfig = createHttpsConfig(httpConfig);
+      this.sslContextFactory = createSSLContextFactory();
+
+      final ServerConnector userHttpConnector = createUserHttpConnector(server, httpConfig);
       server.addConnector(userHttpConnector);
 
       final ServerConnector adminHttpsConnector = createAdminHttpsConnector(server);
@@ -56,7 +64,9 @@ public class LikeXmlJettyServer {
       server.addConnector(createMtaAdminHttpsConnector(server));
       server.addConnector(createExtensionsHttpsConnector(server));
 
-      WebAppContext webAppContext = new WebAppContext(new ContextHandlerCollection(), "/opt/zextras/jetty_base/webapps/service", "/service");
+      final ContextHandlerCollection contexts = new ContextHandlerCollection();
+      this.contexts = contexts;
+      WebAppContext webAppContext = new WebAppContext(contexts, "/opt/zextras/jetty_base/webapps/service", "/service");
       webAppContext.setDescriptor(webDescriptor);
 
 
@@ -71,7 +81,7 @@ public class LikeXmlJettyServer {
 
       server.setHandler(webAppContext);
 
-      userHttpConnector.open();
+//      userHttpConnector.open();
       adminHttpsConnector.open();
 
       return server;
@@ -81,181 +91,142 @@ public class LikeXmlJettyServer {
       this.webDescriptor = webDescriptor;
       return this;
     }
+
+
+    private Handler createRewriteHandler() {
+      final RewriteHandler rewriteHandler = new RewriteHandler();
+      rewriteHandler.setRewriteRequestURI(true);
+      rewriteHandler.setRewritePathInfo(false);
+      rewriteHandler.setOriginalPathAttribute("requestedPath");
+
+      rewriteHandler.setDispatcherTypes(DispatcherType.REQUEST, DispatcherType.ASYNC, DispatcherType.ERROR, DispatcherType.FORWARD);
+      rewriteHandler.addRule(new MsieSslRule());
+
+      rewriteHandler.addRule(new RewritePatternRule("/Microsoft-Server-ActiveSync/*", "/service/extension/zimbrasync"));
+      rewriteHandler.addRule(new RewriteRegexRule("(?i)/ews/Exchange.asmx/*", "/service/extension/zimbraews"));
+      rewriteHandler.addRule(new RewritePatternRule("/principals/*", "/service/dav/principals"));
+      rewriteHandler.addRule(new RewritePatternRule("/dav/*", "/service/dav/home"));
+      rewriteHandler.addRule(new RewritePatternRule("/.well-known/*", "/service/.well-known"));
+      rewriteHandler.addRule(new RewritePatternRule("/.well-known/*", "/service/.well-known"));
+      rewriteHandler.addRule(new RewritePatternRule("/home/*", "/service/home/"));
+      rewriteHandler.addRule(new RewritePatternRule("//home/*", "/service/home"));
+      rewriteHandler.addRule(new RewritePatternRule("/user/*", "/service/user/"));
+      rewriteHandler.addRule(new RewritePatternRule("//user/*", "/service/user"));
+      rewriteHandler.addRule(new RewritePatternRule("/shf/*", "/service/shf/"));
+      rewriteHandler.addRule(new RewritePatternRule("/certauth/*", "/service/certauth"));
+      rewriteHandler.addRule(new RewritePatternRule("/spnegoauth/*", "/service/spnego"));
+      rewriteHandler.addRule(new RewritePatternRule("//service/spnego/*", "/service/spnego"));
+      rewriteHandler.addRule(new RewritePatternRule("//service/spnego/*", "/service/spnego"));
+      rewriteHandler.addRule(new RewritePatternRule("/autodiscover/*", "/service/extension/autodiscover"));
+      rewriteHandler.addRule(new RewritePatternRule("/Autodiscover/*", "/service/extension/autodiscover"));
+
+      final RewritePatternRule serviceRule = new RewritePatternRule("/service/*", "/service");
+      serviceRule.setTerminating(true);
+      rewriteHandler.addRule(serviceRule);
+
+      final RewritePatternRule spnegoRule = new RewritePatternRule("/spnego/*", "/spnego");
+      spnegoRule.setTerminating(true);
+      rewriteHandler.addRule(spnegoRule);
+
+      final RewritePatternRule carbonioAdminRule = new RewritePatternRule("/carbonioAdmin/*", "/carbonioAdmin");
+      carbonioAdminRule.setTerminating(true);
+      rewriteHandler.addRule(carbonioAdminRule);
+
+      final RewritePatternRule rootRule = new RewritePatternRule("//*", "/");
+      rootRule.setTerminating(true);
+      rewriteHandler.addRule(rootRule);
+
+      final RewritePatternRule rootRule2 = new RewritePatternRule("/*", "/");
+      rootRule2.setTerminating(true);
+      rewriteHandler.addRule(rootRule2);
+
+      rewriteHandler.setHandler(new HandlerCollection(new ContextHandlerCollection(), new DefaultHandler(), new RequestLogHandler()));
+
+      return rewriteHandler;
+    }
+
+    private ThreadPool createThreadPool() {
+      QueuedThreadPool threadPool = new QueuedThreadPool();
+      threadPool.setMinThreads(10);
+      threadPool.setMaxThreads(250);
+      threadPool.setIdleTimeout(10000);
+      threadPool.setDetailedDump(false);
+
+      JettyMonitor.setThreadPool(threadPool);
+      return threadPool;
+    }
+
+    private HttpConfiguration createHttpConfig() {
+      HttpConfiguration httpConfig = new HttpConfiguration();
+      httpConfig.setOutputBufferSize(32768);
+      httpConfig.setRequestHeaderSize(8192);
+      httpConfig.setResponseHeaderSize(8192);
+      httpConfig.setSendServerVersion(false);
+      httpConfig.setSendDateHeader(true);
+      httpConfig.setHeaderCacheSize(512);
+      httpConfig.setSecurePort(SECURE_PORT);
+
+      final ForwardedRequestCustomizer forwardedRequestCustomizer = new ForwardedRequestCustomizer();
+      forwardedRequestCustomizer.setForwardedForHeader("bogus");
+      httpConfig.addCustomizer(forwardedRequestCustomizer);
+
+      final HostHeaderCustomizer hostHeaderCustomizer = new HostHeaderCustomizer("carbonio-ce-proxy.carbonio-system.svc.cluster.local");
+      httpConfig.addCustomizer(hostHeaderCustomizer);
+
+      return httpConfig;
+    }
+
+    private HttpConfiguration createHttpsConfig(HttpConfiguration baseConfig) {
+      HttpConfiguration sslHttpConfig = new HttpConfiguration(baseConfig);
+      sslHttpConfig.addCustomizer(new SecureRequestCustomizer());
+      return sslHttpConfig;
+    }
+
+    private ServerConnector createUserHttpConnector(Server server, HttpConfiguration httpConfig) {
+      ServerConnector serverConnector = new ServerConnector(server, new HttpConnectionFactory(httpConfig));
+      serverConnector.setPort(APP_USER_SERVER_PORT);
+      serverConnector.setIdleTimeout(60000);
+      return serverConnector;
+    }
+
+    private static SslContextFactory createSSLContextFactory() {
+      SslContextFactory sslContextFactory = new SslContextFactory();
+      sslContextFactory.setKeyStorePath("/opt/zextras/mailboxd/etc/keystore");
+      sslContextFactory.setKeyStorePassword("password");
+      sslContextFactory.setKeyManagerPassword("password");
+      sslContextFactory.setRenegotiationAllowed(true);
+      sslContextFactory.setIncludeProtocols("TLSv1.2");
+      sslContextFactory.setExcludeCipherSuites(".*_RC4_.*", "^.*_(MD5|SHA|SHA1)$", "^TLS_RSA_.*");
+      return sslContextFactory;
+    }
+
+    private ServerConnector createAdminHttpsConnector(Server server) {
+      ServerConnector serverConnector = createHttpsConnector(server);
+      serverConnector.setPort(APP_ADMIN_SERVER_PORT);
+      serverConnector.setIdleTimeout(0);
+      return serverConnector;
+    }
+
+    private ServerConnector createMtaAdminHttpsConnector(Server server) {
+      ServerConnector serverConnector = createHttpsConnector(server);
+      serverConnector.setPort(MTA_APP_ADMIN_SERVER_PORT);
+      serverConnector.setIdleTimeout(0);
+      return serverConnector;
+    }
+
+    private ServerConnector createExtensionsHttpsConnector(Server server) {
+      ServerConnector serverConnector = createHttpsConnector(server);
+      serverConnector.setPort(EXTENSIONS_SERVER_PORT);
+      serverConnector.setIdleTimeout(60000);
+      return serverConnector;
+    }
+
+    private ServerConnector createHttpsConnector(Server server) {
+      return new ServerConnector(server,
+          new SslConnectionFactory(this.sslContextFactory, HttpVersion.HTTP_1_1.asString()),
+          new HttpConnectionFactory(this.httpsConfig));
+    }
   }
 
-  private static Handler createRewriteHandler() {
-    final RewriteHandler rewriteHandler = new RewriteHandler();
-    rewriteHandler.setRewriteRequestURI(true);
-    rewriteHandler.setRewritePathInfo(false);
-    rewriteHandler.setOriginalPathAttribute("requestedPath");
-
-    rewriteHandler.setDispatcherTypes(DispatcherType.REQUEST, DispatcherType.ASYNC, DispatcherType.ERROR, DispatcherType.FORWARD);
-    rewriteHandler.addRule(new MsieSslRule());
-
-    rewriteHandler.addRule(new RewritePatternRule("/Microsoft-Server-ActiveSync/*", "/service/extension/zimbrasync"));
-    rewriteHandler.addRule(new RewriteRegexRule("(?i)/ews/Exchange.asmx/*", "/service/extension/zimbraews"));
-    rewriteHandler.addRule(new RewritePatternRule("/principals/*", "/service/dav/principals"));
-    rewriteHandler.addRule(new RewritePatternRule("/dav/*", "/service/dav/home"));
-    rewriteHandler.addRule(new RewritePatternRule("/.well-known/*", "/service/.well-known"));
-    rewriteHandler.addRule(new RewritePatternRule("/.well-known/*", "/service/.well-known"));
-    rewriteHandler.addRule(new RewritePatternRule("/home/*", "/service/home/"));
-    rewriteHandler.addRule(new RewritePatternRule("//home/*", "/service/home"));
-    rewriteHandler.addRule(new RewritePatternRule("/user/*", "/service/user/"));
-    rewriteHandler.addRule(new RewritePatternRule("//user/*", "/service/user"));
-    rewriteHandler.addRule(new RewritePatternRule("/shf/*", "/service/shf/"));
-    rewriteHandler.addRule(new RewritePatternRule("/certauth/*", "/service/certauth"));
-    rewriteHandler.addRule(new RewritePatternRule("/spnegoauth/*", "/service/spnego"));
-    rewriteHandler.addRule(new RewritePatternRule("//service/spnego/*", "/service/spnego"));
-    rewriteHandler.addRule(new RewritePatternRule("//service/spnego/*", "/service/spnego"));
-    rewriteHandler.addRule(new RewritePatternRule("/autodiscover/*", "/service/extension/autodiscover"));
-    rewriteHandler.addRule(new RewritePatternRule("/Autodiscover/*", "/service/extension/autodiscover"));
-
-    final RewritePatternRule serviceRule = new RewritePatternRule("/service/*", "/service");
-    serviceRule.setTerminating(true);
-    rewriteHandler.addRule(serviceRule);
-
-    final RewritePatternRule spnegoRule = new RewritePatternRule("/spnego/*", "/spnego");
-    spnegoRule.setTerminating(true);
-    rewriteHandler.addRule(spnegoRule);
-
-    final RewritePatternRule carbonioAdminRule = new RewritePatternRule("/carbonioAdmin/*", "/carbonioAdmin");
-    carbonioAdminRule.setTerminating(true);
-    rewriteHandler.addRule(carbonioAdminRule);
-
-    final RewritePatternRule rootRule = new RewritePatternRule("//*", "/");
-    rootRule.setTerminating(true);
-    rewriteHandler.addRule(rootRule);
-
-    final RewritePatternRule rootRule2 = new RewritePatternRule("/*", "/");
-    rootRule2.setTerminating(true);
-    rewriteHandler.addRule(rootRule2);
-
-    rewriteHandler.setHandler(new HandlerCollection(new ContextHandlerCollection(), new DefaultHandler(), new RequestLogHandler()));
-
-    return rewriteHandler;
-  }
-
-  private static ThreadPool createThreadPool() {
-//    <Get name="ThreadPool" id="pool">
-//		<Set name="minThreads" type="int">10</Set>
-//		<Set name="maxThreads" type="int">250</Set>
-//		<Set name="idleTimeout" type="int">10000</Set>
-//		<Set name="detailedDump">false</Set>
-//	</Get>
-//
-//	<Call class="com.zimbra.common.jetty.JettyMonitor" name="setThreadPool">
-//		<Arg><Ref id="pool"/></Arg>
-//	</Call>
-    QueuedThreadPool threadPool = new QueuedThreadPool();
-    threadPool.setMinThreads(10);
-    threadPool.setMaxThreads(250);
-    threadPool.setIdleTimeout(10000);
-    threadPool.setDetailedDump(false);
-
-    JettyMonitor.setThreadPool(threadPool);
-    return threadPool;
-  }
-
-  private static HttpConfiguration basicHttpConfig() {
-//    <Set name="outputBufferSize">32768</Set>
-//		<Set name="requestHeaderSize">8192</Set>
-//		<Set name="responseHeaderSize">8192</Set>
-//		<Set name="sendServerVersion">false</Set>
-//		<Set name="sendDateHeader">true</Set>
-//		<Set name="headerCacheSize">512</Set>
-//		<Set name="securePort">8443</Set>
-    HttpConfiguration httpConfig = new HttpConfiguration();
-    httpConfig.setOutputBufferSize(32768);
-    httpConfig.setRequestHeaderSize(8192);
-    httpConfig.setResponseHeaderSize(8192);
-    httpConfig.setSendServerVersion(false);
-    httpConfig.setSendDateHeader(true);
-    httpConfig.setHeaderCacheSize(512);
-    httpConfig.setSecurePort(SECURE_PORT);
-
-    final ForwardedRequestCustomizer forwardedRequestCustomizer = new ForwardedRequestCustomizer();
-    forwardedRequestCustomizer.setForwardedForHeader("bogus");
-    httpConfig.addCustomizer(forwardedRequestCustomizer);
-
-    final HostHeaderCustomizer hostHeaderCustomizer = new HostHeaderCustomizer("carbonio-ce-proxy.carbonio-system.svc.cluster.local");
-    httpConfig.addCustomizer(hostHeaderCustomizer);
-
-    return httpConfig;
-  }
-
-  private static HttpConfiguration createSSLHttpConfig() {
-    HttpConfiguration sslHttpConfig = basicHttpConfig();
-    sslHttpConfig.addCustomizer(new SecureRequestCustomizer());
-    return sslHttpConfig;
-  }
-
-  private static ServerConnector createUserHttpConnector(Server server) {
-    ServerConnector serverConnector = new ServerConnector(server);
-    serverConnector.addConnectionFactory(new HttpConnectionFactory(basicHttpConfig()));
-    serverConnector.setPort(APP_USER_SERVER_PORT);
-    serverConnector.setIdleTimeout(60000);
-    return serverConnector;
-  }
-
-  private static SslContextFactory createSSLContextFactory() {
-//    <Set name="KeyStorePath"><SystemProperty name="jetty.base" default="." />/etc/keystore</Set>
-//		<Set name="KeyStorePassword">password</Set>
-//		<Set name="KeyManagerPassword">password</Set>
-//		<Set name="EndpointIdentificationAlgorithm"></Set>
-//		<Set name="renegotiationAllowed">TRUE</Set>
-//		<!-- SSLPROTOCOLSBEGIN -->
-//		<Set name="IncludeProtocols">
-//			<Array type="java.lang.String">
-//				<Item>TLSv1.2</Item>
-//			</Array>
-//		</Set>
-//		<!-- SSLPROTOCOLSEND -->
-//		<Set name="ExcludeCipherSuites">
-//			<Array type="java.lang.String">
-//				<Item>.*_RC4_.*</Item>
-//<Item>^.*_(MD5|SHA|SHA1)$</Item>
-//<Item>^TLS_RSA_.*</Item>
-//			</Array>
-//		</Set>
-    SslContextFactory sslContextFactory = new SslContextFactory();
-    sslContextFactory.setKeyStorePath("/opt/zextras/mailboxd/etc/keystore");
-    sslContextFactory.setKeyStorePassword("password");
-    sslContextFactory.setKeyManagerPassword("password");
-    sslContextFactory.setEndpointIdentificationAlgorithm("password");
-    sslContextFactory.setRenegotiationAllowed(true);
-    sslContextFactory.setIncludeProtocols("TLSv1.2");
-    sslContextFactory.setExcludeCipherSuites(".*_RC4_.*", "^.*_(MD5|SHA|SHA1)$", "^TLS_RSA_.*");
-    return sslContextFactory;
-  }
-
-  private static ServerConnector createAdminHttpsConnector(Server server) {
-    ServerConnector serverConnector = new ServerConnector(server);
-    serverConnector.addConnectionFactory(new HttpConnectionFactory(createSSLHttpConfig()));
-    final SslConnectionFactory sslConnectionFactory = new SslConnectionFactory(createSSLContextFactory(),  "http/1.1");
-    serverConnector.addConnectionFactory(sslConnectionFactory);
-    serverConnector.setPort(APP_ADMIN_SERVER_PORT);
-    serverConnector.setIdleTimeout(0);
-    return serverConnector;
-  }
-
-  private static ServerConnector createMtaAdminHttpsConnector(Server server) {
-    ServerConnector serverConnector = new ServerConnector(server);
-    serverConnector.addConnectionFactory(new HttpConnectionFactory(createSSLHttpConfig()));
-    final SslConnectionFactory sslConnectionFactory = new SslConnectionFactory(createSSLContextFactory(),  "http/1.1");
-    serverConnector.addConnectionFactory(sslConnectionFactory);
-    serverConnector.setPort(MTA_APP_ADMIN_SERVER_PORT);
-    serverConnector.setIdleTimeout(0);
-    return serverConnector;
-  }
-
-  private static ServerConnector createExtensionsHttpsConnector(Server server) {
-    ServerConnector serverConnector = new ServerConnector(server);
-    serverConnector.addConnectionFactory(new HttpConnectionFactory(createSSLHttpConfig()));
-    final SslConnectionFactory sslConnectionFactory = new SslConnectionFactory(createSSLContextFactory(),  "http/1.1");
-    serverConnector.addConnectionFactory(sslConnectionFactory);
-    serverConnector.setPort(EXTENSIONS_SERVER_PORT);
-    serverConnector.setIdleTimeout(60000);
-    return serverConnector;
-  }
 
 }
