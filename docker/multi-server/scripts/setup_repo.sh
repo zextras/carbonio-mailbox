9d2332b627da9d0ffcbfe8869022b21aa973ae5d
Davide Frison <davide.frison@zextras.com>
2024-04-12 17:18:41 +0200

    refactor: fat jar + jetty-libs jars
    - add also all jars from carbonio-jetty-libs, they are needed by other processes and utils in carbonio (zmconfigd)
    - perform autoremove in containers package upgrade

--- docker/multi-server/scripts/setup_repo.sh
+++ docker/multi-server/scripts/setup_repo.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
+apt update && apt install dpkg-dev -y
+
+mkdir -p /tmp/local_repo
+cp -r /artifacts/*deb /tmp/local_repo
+
+cd /tmp/local_repo && dpkg-scanpackages -m . > Packages
 
 echo "deb [trusted=yes] https://repo.zextras.io/release/ubuntu focal main" > /etc/apt/sources.list.d/zextras.list
+echo "deb [trusted=yes] file:/tmp/local_repo/ /" >> /etc/apt/sources.list.d/zextras.list
 
-apt update && apt upgrade -y
+apt update && apt full-upgrade -y
+apt autoremove -y
