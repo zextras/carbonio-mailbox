d5bf6695a9732aa7ee71ccd0a95949ed63f61276
Davide Frison <davide.frison@zextras.com>
2024-04-24 12:05:54 +0200

    chore: update packages to 4.16.0 + create specific jython/jars folder + jammy local build

--- build_packages.sh
+++ build_packages.sh
@@ -9,6 +9,6 @@ docker run -it --rm \
   --entrypoint=yap \
   -v $(pwd)/artifacts:/artifacts \
   -v $(pwd):/tmp/staging \
-  docker.io/m0rf30/yap-ubuntu-focal:1.6 \
-  build ubuntu-focal /tmp/staging/packages
+  docker.io/m0rf30/yap-ubuntu-jammy:1.8 \
+  build ubuntu-jammy /tmp/staging/packages
 
