fa515994e17c1da47cd47765947c7ee35b6a2186
Francesco Gottardi <37839534+Gammax92@users.noreply.github.com>
2024-05-03 10:29:58 +0200

    feat: [ZIF-1022] Convert the smart-link metrics to send info to posthog (instead of matomo) (#501)
    * feat: added PostHogTracking
    
    * feat: added PostHogTracking
    
    * fix: using PostHogTracking insted of MatomoTracking
    
    * chore: PostHogTracking POSTHOG_API_KEY package private
    
    * refactor: remove MatomoTracking
    
    * refactor: rename of postHog key
    
    ---------
    
    Co-authored-by: Davide Frison <davide.frison@zextras.com>
    Co-authored-by: Davide Frison <36330277+frisonisland@users.noreply.github.com>
    Co-authored-by: Daniele Megna <daniele.megna@qmates.tech>
    Co-authored-by: zextrasGiovanniDefacci <66628670+zextrasGiovanniDefacci@users.noreply.github.com>

---
+++ store/src/main/java/com/zextras/mailbox/tracking/PostHogTracking.java
@@ -0,0 +1,38 @@
+package com.zextras.mailbox.tracking;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URI;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+
+public class PostHogTracking implements Tracking {
+
+    static final String SITE_KEY = "phc_egpFZ14OKByQMK51wCTzYp8tLrg0VA8wa2QDagXCjDG";
+    private final String endPoint;
+
+    public PostHogTracking(String endPoint) {
+        this.endPoint = endPoint;
+    }
+
+    @Override
+    public void sendEventIgnoringFailure(Event event) {
+        try (CloseableHttpClient client = HttpClientBuilder.create().build()) {
+            client.execute(generateRequest(event));
+        } catch (Exception ignored) {
+        }
+    }
+
+    private HttpPost generateRequest(Event event) throws UnsupportedEncodingException {
+        final HttpPost httpPost = new HttpPost();
+        String url = endPoint + "/capture/";
+        httpPost.setEntity(new StringEntity("{\n" +
+                "        \"event\": \"" + event.getAction() + "\",\n" +
+                "        \"api_key\": \"" + SITE_KEY + "\",\n" +
+                "        \"distinct_id\": \"" + event.getUserId() + "\"\n" +
+                "        }"));
+        httpPost.setURI(URI.create(url));
+        return httpPost;
+    }
+}
