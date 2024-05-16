ecbaa4b4412e70441b5f3d8b1fe92c687a7075f0
Keshav Bhatt <keshavnrj@gmail.com>
2024-05-16 14:30:52 +0530

    chore: update unit test

--- store/src/main/java/com/zimbra/cs/service/admin/IssueCert.java
+++ store/src/main/java/com/zimbra/cs/service/admin/IssueCert.java
@@ -19,8 +19,8 @@ import java.util.Map;
 import java.util.Optional;
 
 /**
- * Admin Handler class to issue a LetsEncrypt certificate for a domain using {@link
- * com.zimbra.cs.rmgmt.RemoteManager}, {@link RemoteCertbot}.
+ * Admin Handler class to issue a LetsEncrypt certificate for a domain using {@link com.zimbra.cs.rmgmt.RemoteManager},
+ * {@link RemoteCertbot}.
  *
  * @author Yuliya Aheeva
  * @since 23.3.0
@@ -32,22 +32,19 @@ public class IssueCert extends AdminDocumentHandler {
           + "It will send the result to the Global and Domain notification recipients.";
 
   /**
-   * Handles the request. Searches a domain by id, checks admin rights (accessible to global and
-   * delegated admin of requested domain), searches a server with proxy node, creates certbot
-   * command, asynchronously executes it, creates response element, notifies global and domain
-   * recipients about the result of remote execution.
+   * Handles the request. Searches a domain by id, checks admin rights (accessible to global and delegated admin of
+   * requested domain), searches a server with proxy node, creates certbot command, asynchronously executes it, creates
+   * response element, notifies global and domain recipients about the result of remote execution.
    *
    * <p>Note: Executes certbot command only on ONE (first found) proxy even if there are multiple
    * proxy in the infrastructure.
    *
-   * @param request {@link Element} representation of {@link
-   *     com.zimbra.soap.admin.message.IssueCertRequest}
+   * @param request {@link Element} representation of {@link com.zimbra.soap.admin.message.IssueCertRequest}
    * @param context request context
-   * @return {@link Element} representation of {@link
-   *     com.zimbra.soap.admin.message.IssueCertResponse}
-   * @throws ServiceException in case if domain could not be found, domain doesn't have
-   *     PublicServiceHostname and at least one VirtualHostName, server with proxy node could not be
-   *     found or domain admin doesn't have rights to deal with this domain.
+   * @return {@link Element} representation of {@link com.zimbra.soap.admin.message.IssueCertResponse}
+   * @throws ServiceException in case if domain could not be found, domain doesn't have PublicServiceHostname and at
+   *                          least one VirtualHostName, server with proxy node could not be found or domain admin
+   *                          doesn't have rights to deal with this domain.
    */
   @Override
   public Element handle(final Element request, final Map<String, Object> context)
@@ -57,9 +54,8 @@ public class IssueCert extends AdminDocumentHandler {
     final Provisioning prov = Provisioning.getInstance();
     final Account callerAccount = zsc.getAuthToken().getAccount();
 
-    if (!prov.onLocalServer(callerAccount)){
-      ZimbraLog.soap.info("Proxying IssueCert request to " + prov.getServer(callerAccount).getHostname());
-      return proxyRequest(request, context, callerAccount.getId());
+    if (!prov.onLocalServer(callerAccount)) {
+      return proxyRequestToAccountServer(request, context, callerAccount);
     }
 
     final String domainId = request.getAttribute(AdminConstants.A_DOMAIN);
@@ -138,7 +134,9 @@ public class IssueCert extends AdminDocumentHandler {
     return response;
   }
 
-  Element proxy(final Element request, final Map<String, Object> context, String accountId) {
-    return null;
+  Element proxyRequestToAccountServer(Element request, Map<String, Object> context, Account account)
+      throws ServiceException {
+    ZimbraLog.soap.info("Proxying IssueCert request to " + account.getName() + "'s mail host.");
+    return proxyRequest(request, context, account.getId());
   }
 }
