a631e9deb354960474901f1f80c5a5f6c2bc7d6b
Keshav Bhatt <keshavnrj@gmail.com>
2024-05-24 23:37:11 +0530

    feat: add helper getSoapContextForAccount to SoapTestSuite

--- store/src/test/java/com/zextras/mailbox/soap/SoapTestSuite.java
+++ store/src/test/java/com/zextras/mailbox/soap/SoapTestSuite.java
@@ -5,6 +5,13 @@
 package com.zextras.mailbox.soap;
 
 import com.zextras.mailbox.util.SoapClient;
+import com.zimbra.common.service.ServiceException;
+import com.zimbra.common.soap.SoapProtocol;
+import com.zimbra.cs.account.Account;
+import com.zimbra.cs.service.AuthProvider;
+import com.zimbra.soap.SoapEngine;
+import com.zimbra.soap.ZimbraSoapContext;
+import java.util.HashMap;
 import org.junit.jupiter.api.extension.RegisterExtension;
 
 /**
@@ -20,8 +27,32 @@ public class SoapTestSuite {
       .addEngineHandler("com.zimbra.cs.service.mail.MailServiceWithoutTracking")
       .create();
 
+  /**
+   * @return {@link SoapClient} that can execute SOAP http requests
+   */
   public SoapClient getSoapClient() {
     return soapExtension.getSoapClient();
+  }
+
+  /**
+   * @param account {@link Account} who will make the SOAP request using the returned {@link ZimbraSoapContext}
+   * @param isAdmin whether the account is admin or not
+   * @return  {@code HashMap<String, Object>} holding {@link ZimbraSoapContext} as item
+   * @throws ServiceException if context creation fails
+   */
+  public static HashMap<String, Object> getSoapContextForAccount(Account account, boolean isAdmin)
+      throws ServiceException {
+    return new HashMap<>() {
+      {
+        put(
+            SoapEngine.ZIMBRA_CONTEXT,
+            new ZimbraSoapContext(
+                AuthProvider.getAuthToken(account, isAdmin),
+                account.getId(),
+                SoapProtocol.Soap12,
+                SoapProtocol.Soap12));
+      }
+    };
   }
 
 }
