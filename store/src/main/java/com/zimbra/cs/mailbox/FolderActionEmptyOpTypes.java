a93fdf96c2984646f1cd987a49707b4c5e33a0a9
Yuliya Aheeva <yuliya.aheeva@mail.ru>
2024-05-28 10:45:39 +0200

    chore: add javadoc for FolderActionEmptyOpTypes

--- store/src/main/java/com/zimbra/cs/mailbox/FolderActionEmptyOpTypes.java
+++ store/src/main/java/com/zimbra/cs/mailbox/FolderActionEmptyOpTypes.java
@@ -6,13 +6,21 @@ import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
-//Todo add java docs
+/**
+ * Enum representing different types supported by the empty operation in
+ * {@link com.zimbra.cs.service.mail.FolderAction}.
+ */
 public enum FolderActionEmptyOpTypes {
   EMAILS,
   CONTACTS,
   APPOINTMENTS;
 
-  //Todo add java docs
+  /**
+   * Checks if the enum contains a type with the specified name.
+   *
+   * @param name the name of the type to check
+   * @return true if the enum contains the specified type, false otherwise
+   */
   public static boolean contains(String name) {
     for (FolderActionEmptyOpTypes type : values()) {
       if (type.name().equalsIgnoreCase(name)) {
@@ -22,7 +30,12 @@ public enum FolderActionEmptyOpTypes {
     return false;
   }
 
-  //Todo add java docs
+  /**
+   * Gets the list of included types for a specified folder action type.
+   *
+   * @param type the folder action type
+   * @return a list of included types for the specified folder action type
+   */
   public static List<Type> getIncludedTypesFor(FolderActionEmptyOpTypes type) {
     switch (type) {
       case EMAILS:
@@ -37,7 +50,9 @@ public enum FolderActionEmptyOpTypes {
   }
 
   /**
-   * @return String of comma separated list of types
+   * Converts the enum values to a comma-separated string.
+   *
+   * @return a string representation of the enum values, separated by commas
    */
   public static String valueToString() {
     return Arrays.stream(
