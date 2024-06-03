e24c2713d023dab2d45afce12a900fcc86725b56
Davide Frison <davide.frison@zextras.com>
2024-06-03 19:10:38 +0200

    test: check smime compatibility on send msg

--- store/src/test/java/com/zimbra/cs/service/mail/SaveDraftApiTest.java
+++ store/src/test/java/com/zimbra/cs/service/mail/SaveDraftApiTest.java
@@ -1,32 +1,73 @@
 package com.zimbra.cs.service.mail;
 
+import com.icegreen.greenmail.util.GreenMail;
+import com.icegreen.greenmail.util.ServerSetup;
 import com.zextras.mailbox.soap.SoapTestSuite;
 import com.zextras.mailbox.util.MailboxTestUtil;
-import com.zimbra.common.service.ServiceException;
 import com.zimbra.cs.account.Account;
 import com.zimbra.cs.account.Provisioning;
-import com.zimbra.soap.account.message.AuthRequest;
+import com.zimbra.cs.mailbox.Mailbox;
+import com.zimbra.cs.mailbox.MailboxManager;
+import com.zimbra.cs.mailclient.smtp.SmtpConfig;
+import com.zimbra.cs.mime.ParsedMessage;
+import com.zimbra.cs.util.JMSession;
 import com.zimbra.soap.mail.message.GetMsgRequest;
 import com.zimbra.soap.mail.message.GetMsgResponse;
 import com.zimbra.soap.mail.message.SaveDraftRequest;
 import com.zimbra.soap.mail.message.SaveDraftResponse;
+import com.zimbra.soap.mail.message.SendMsgRequest;
+import com.zimbra.soap.mail.message.SendMsgResponse;
 import com.zimbra.soap.mail.type.MessageInfo;
+import com.zimbra.soap.mail.type.MimePartInfo;
 import com.zimbra.soap.mail.type.MsgSpec;
+import com.zimbra.soap.mail.type.MsgToSend;
 import com.zimbra.soap.mail.type.MsgWithGroupInfo;
 import com.zimbra.soap.mail.type.SaveDraftMsg;
-import com.zimbra.soap.type.AccountSelector;
-import com.zimbra.soap.type.UrlAndValue;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.io.UnsupportedEncodingException;
-import java.util.Arrays;
+import java.io.InputStream;
+import java.math.BigInteger;
+import java.nio.charset.StandardCharsets;
+import java.security.GeneralSecurityException;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.KeyStore;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.security.SecureRandom;
+import java.security.cert.X509Certificate;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
-import java.util.stream.Collectors;
-import org.apache.http.Header;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
 import org.apache.http.HttpResponse;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.entity.StringEntity;
-import org.apache.http.impl.client.HttpClientBuilder;
+import org.bouncycastle.asn1.ASN1EncodableVector;
+import org.bouncycastle.asn1.cms.AttributeTable;
+import org.bouncycastle.asn1.cms.IssuerAndSerialNumber;
+import org.bouncycastle.asn1.smime.SMIMECapabilitiesAttribute;
+import org.bouncycastle.asn1.smime.SMIMECapability;
+import org.bouncycastle.asn1.smime.SMIMECapabilityVector;
+import org.bouncycastle.asn1.smime.SMIMEEncryptionKeyPreferenceAttribute;
+import org.bouncycastle.asn1.x500.X500Name;
+import org.bouncycastle.asn1.x509.AuthorityKeyIdentifier;
+import org.bouncycastle.asn1.x509.SubjectKeyIdentifier;
+import org.bouncycastle.asn1.x509.SubjectPublicKeyInfo;
+import org.bouncycastle.asn1.x509.X509Extension;
+import org.bouncycastle.cert.X509v3CertificateBuilder;
+import org.bouncycastle.cert.bc.BcX509ExtensionUtils;
+import org.bouncycastle.cert.jcajce.JcaCertStore;
+import org.bouncycastle.cert.jcajce.JcaX509CertificateConverter;
+import org.bouncycastle.cert.jcajce.JcaX509v3CertificateBuilder;
+import org.bouncycastle.cms.jcajce.JcaSimpleSignerInfoGeneratorBuilder;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import org.bouncycastle.mail.smime.SMIMESignedGenerator;
+import org.bouncycastle.operator.OperatorCreationException;
+import org.bouncycastle.operator.jcajce.JcaContentSignerBuilder;
+import org.bouncycastle.util.Store;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Tag;
@@ -37,36 +78,237 @@ public class SaveDraftApiTest extends SoapTestSuite {
 
   private static MailboxTestUtil.AccountCreator.Factory accountCreatorFactory;
   private static Provisioning provisioning;
+  private static GreenMail mta;
 
   @BeforeAll
   public static void setUp() throws Exception {
+    java.security.Security.addProvider(new BouncyCastleProvider());
     provisioning = Provisioning.getInstance();
     accountCreatorFactory = new MailboxTestUtil.AccountCreator.Factory(provisioning);
+
+    mta =
+        new GreenMail(
+            new ServerSetup[] {
+                new ServerSetup(
+                    SmtpConfig.DEFAULT_PORT, SmtpConfig.DEFAULT_HOST, ServerSetup.PROTOCOL_SMTP),
+                new ServerSetup(9000, "127.0.0.1", ServerSetup.PROTOCOL_IMAP)
+            });
+    mta.start();
+  }
+
+  @AfterAll
+  public static void tearDown() {
+    mta.stop();
+  }
+
+  static AuthorityKeyIdentifier createAuthorityKeyId(
+      PublicKey pub)
+  {
+    SubjectPublicKeyInfo info = SubjectPublicKeyInfo.getInstance(pub.getEncoded());
+
+    return new AuthorityKeyIdentifier(info);
+  }
+
+  static SubjectKeyIdentifier createSubjectKeyId(
+      PublicKey pub)
+  {
+    SubjectPublicKeyInfo info = SubjectPublicKeyInfo.getInstance(pub.getEncoded());
+
+    return new BcX509ExtensionUtils().createSubjectKeyIdentifier(info);
+  }
+
+  static X509Certificate makeCertificate(
+      KeyPair subKP,
+      String  subDN,
+      KeyPair issKP,
+      String  issDN,
+      BigInteger serial)
+      throws GeneralSecurityException, IOException, OperatorCreationException
+  {
+    PublicKey subPub  = subKP.getPublic();
+    PrivateKey issPriv = issKP.getPrivate();
+    PublicKey  issPub  = issKP.getPublic();
+
+    X509v3CertificateBuilder v3CertGen = new JcaX509v3CertificateBuilder(new X500Name(issDN), serial, new Date(System.currentTimeMillis()), new Date(System.currentTimeMillis() + (1000L * 60 * 60 * 24 * 100)), new X500Name(subDN), subPub);
+
+    v3CertGen.addExtension(
+        X509Extension.subjectKeyIdentifier,
+        false,
+        createSubjectKeyId(subPub));
+
+    v3CertGen.addExtension(
+        X509Extension.authorityKeyIdentifier,
+        false,
+        createAuthorityKeyId(issPub));
+
+    return new JcaX509CertificateConverter().setProvider("BC").getCertificate(v3CertGen.build(new JcaContentSignerBuilder("MD5withRSA").setProvider("BC").build(issPriv)));
+  }
+
+
+  public SMIMESignedGenerator createSMIMESignedGenerator() throws GeneralSecurityException, IOException, OperatorCreationException {
+    KeyPairGenerator kpg  = KeyPairGenerator.getInstance("RSA", "BC");
+
+    kpg.initialize(1024);
+    InputStream stream = this.getClass().getResourceAsStream("/smime.p12");
+    KeyStore store = KeyStore.getInstance("PKCS12");
+    store.load(stream, "password".toCharArray());
+
+    kpg.initialize(1024, new SecureRandom());
+
+    //
+    // cert that issued the signing certificate (CA Authority, e.g.: Actalis)
+    //
+    String              signDN = "O=Test, C=IT";
+    KeyPair             signKP = kpg.generateKeyPair();
+    X509Certificate     signCert =  makeCertificate(
+        signKP, signDN, signKP, signDN, BigInteger.valueOf(1));
+
+    //
+    // cert we sign against
+    // certificato utente
+    //
+    String              origDN = "CN=Test, E=from@test.com, O=Test, C=IT";
+    KeyPair             origKP = kpg.generateKeyPair();
+    X509Certificate     x509Certificate = makeCertificate(
+        origKP, origDN, signKP, signDN, BigInteger.valueOf(2));
+
+    List                certList = new ArrayList();
+
+    certList.add(x509Certificate);
+    certList.add(signCert);
+
+    //
+    // create a CertStore containing the certificates we want carried
+    // in the signature
+    //
+    Store certs = new JcaCertStore(certList);
+
+    //
+    // create some smime capabilities in case someone wants to respond
+    //
+    ASN1EncodableVector         signedAttrs = new ASN1EncodableVector();
+    SMIMECapabilityVector       caps = new SMIMECapabilityVector();
+
+    caps.addCapability(SMIMECapability.dES_EDE3_CBC);
+    caps.addCapability(SMIMECapability.rC2_CBC, 128);
+    caps.addCapability(SMIMECapability.dES_CBC);
+
+    signedAttrs.add(new SMIMECapabilitiesAttribute(caps));
+
+    //
+    // add an encryption key preference for encrypted responses -
+    // normally this would be different from the signing certificate...
+    //
+    IssuerAndSerialNumber   issAndSer = new IssuerAndSerialNumber(
+        new X500Name(signDN), x509Certificate.getSerialNumber());
+
+    signedAttrs.add(new SMIMEEncryptionKeyPreferenceAttribute(issAndSer));
+
+    //
+    // create the generator for creating a smime/signed message
+    //
+    SMIMESignedGenerator gen = new SMIMESignedGenerator();
+
+    //
+    // add a signer to the generator - this specifies we are using SHA1 and
+    // adding the smime attributes above to the signed attributes that
+    // will be generated as part of the signature. The encryption algorithm
+    // used is taken from the key - in this RSA with PKCS1Padding
+    //
+    final PrivateKey privateKey = origKP.getPrivate();
+    gen.addSignerInfoGenerator(new JcaSimpleSignerInfoGeneratorBuilder().setProvider("BC").setSignedAttributeGenerator(new AttributeTable(signedAttrs))
+        .build(
+            "SHA1withRSA",
+            privateKey,
+            x509Certificate
+        )
+    );
+
+    //
+    // add our pool of certs and cerls (if any) to go with the signature
+    //
+    gen.addCertificates(certs);
+    return gen;
   }
 
 
   @Test
-  public void shouldSaveDraft() throws Exception {
+  public void signMessage() throws Exception {
     final String password = "password";
     Account testAccount = accountCreatorFactory.get().withPassword(password).create();
+    final MsgWithGroupInfo msgWithGroupInfo = saveDraftMessage(testAccount);
+    String originalMessage = msgWithGroupInfo.getContent().getValue();
 
+    final MimeBodyPart mimeBodyPart = new MimeBodyPart(new ByteArrayInputStream(originalMessage.getBytes(StandardCharsets.UTF_8)));
+
+    var smimeSignedGenerator = createSMIMESignedGenerator();
+    MimeMultipart generatedSignedEmail = smimeSignedGenerator.generate(mimeBodyPart);
+    final MimeMessage signedMessage = new MimeMessage(JMSession.getSmtpSession(testAccount));
+    signedMessage.setContent(generatedSignedEmail);
+
+    final MsgWithGroupInfo draftSignedMessage = saveDraft(testAccount, msgWithGroupInfo.getId(),
+        signedMessage);
+    System.out.println(draftSignedMessage.getContent().getValue());
+    final HttpResponse sendMsgResponse = sendMessage(testAccount, draftSignedMessage.getId());
+    Assertions.assertEquals(200, sendMsgResponse.getStatusLine().getStatusCode());
+
+
+  }
+
+  private MsgWithGroupInfo saveDraftMessage(Account testAccount) throws Exception {
     final SaveDraftRequest saveDraftRequest = new SaveDraftRequest();
     final SaveDraftMsg msg = new SaveDraftMsg();
     msg.setSubject("Test SMIME email");
-    msg.setFragment("Hello there");
+    MimePartInfo partInfo = MimePartInfo.createForContentTypeAndContent("text/plain; charset=UTF-8; format=flowed", "hello there");
+
+    msg.setMimePart(partInfo);
     saveDraftRequest.setMsg(msg);
 
     final MessageInfo draftMsg = getSoapClient().execute(testAccount, saveDraftRequest, SaveDraftResponse.class)
         .getMessage();
     final String draftId = draftMsg.getId();
 
+    final MsgWithGroupInfo rawMessage = getRawMessage(testAccount, draftId);
+    return rawMessage;
+  }
 
+  private MsgWithGroupInfo saveDraft(Account testAccount, String draftId, MimeMessage message) throws Exception {
+    final ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+    message.writeTo(byteArrayOutputStream);
+    final Mailbox mailbox = MailboxManager.getInstance().getMailboxByAccount(testAccount);
+    final ParsedMessage parsedMessage = new ParsedMessage(byteArrayOutputStream.toByteArray(),
+        false);
+    mailbox.saveDraft(null, parsedMessage, Integer.parseInt(draftId));
+
+    return getRawMessage(testAccount, draftId);
+  }
+
+  private MsgWithGroupInfo getRawMessage(Account testAccount, String draftId) throws Exception {
     final MsgSpec msgSpec = new MsgSpec(draftId);
     msgSpec.setRaw(true);
     final MsgWithGroupInfo gotMsg = getSoapClient().execute(testAccount,
             new GetMsgRequest(msgSpec), GetMsgResponse.class)
         .getMsg();
-    final UrlAndValue content = gotMsg.getContent();
-    final String emlMessage = content.getValue();
+    return gotMsg;
   }
+
+  private MsgWithGroupInfo getMessage(Account testAccount, String draftId) throws Exception {
+    final MsgSpec msgSpec = new MsgSpec(draftId);
+    msgSpec.setRaw(false);
+    final MsgWithGroupInfo gotMsg = getSoapClient().execute(testAccount,
+            new GetMsgRequest(msgSpec), GetMsgResponse.class)
+        .getMsg();
+    return gotMsg;
+  }
+
+  private HttpResponse sendMessage(Account testAccount, String draftId) throws Exception {
+    final MsgToSend msgToSend = new MsgToSend();
+    msgToSend.setDraftId(draftId);
+    msgToSend.setSendFromDraft(true);
+    final SendMsgRequest soapBodyPOJO = new SendMsgRequest();
+    soapBodyPOJO.setMsg(msgToSend);
+    return getSoapClient().executeSoap(testAccount,
+            soapBodyPOJO);
+  }
+
 }
