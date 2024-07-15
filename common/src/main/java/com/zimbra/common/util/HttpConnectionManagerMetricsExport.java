f80296edfd3b98b64a2afb2534eb745899449c95
Soner Sivri <sonersivri@gmail.com>
2024-07-15 15:22:18 +0300

    chore: test fix

--- common/src/main/java/com/zimbra/common/util/HttpConnectionManagerMetricsExport.java
+++ common/src/main/java/com/zimbra/common/util/HttpConnectionManagerMetricsExport.java
@@ -20,45 +20,49 @@ import java.util.List;
  */
 public class HttpConnectionManagerMetricsExport extends CarbonioCollector {
 
+    private static final String POOL = "pool";
+
     /**
      * @return List<MetricFamilySamples>
      */
     @Override
     public List<MetricFamilySamples> collect() {
         List<MetricFamilySamples> mfs = new ArrayList<>();
-        GaugeMetricFamily internalGaugeMetricFamily = new GaugeMetricFamily(
-                "http_connection_pool_internal",
-                "Metrics for internal pool",
-                Collections.singletonList("pool"));
-        PoolStats internalStats = null;
+        GaugeMetricFamily internalGaugeMetricFamily = getGaugeMetricFamily("internal");
 
-        ZimbraHttpConnectionManager internalHttpConnMgr = ZimbraHttpConnectionManager.getInternalHttpConnMgr();
-        if (internalHttpConnMgr != null && internalHttpConnMgr.getHttpConnMgr() instanceof PoolingHttpClientConnectionManager) {
-            internalStats = ((PoolingHttpClientConnectionManager) internalHttpConnMgr.getHttpConnMgr()).getTotalStats();
-        }
-
-        internalGaugeMetricFamily.addMetric(Collections.singletonList("available"), internalStats == null ? -1 :  internalStats.getAvailable());
-        internalGaugeMetricFamily.addMetric(Collections.singletonList("leased"),  internalStats == null ? -1 :  internalStats.getLeased());
-        internalGaugeMetricFamily.addMetric(Collections.singletonList("max"),  internalStats == null ? -1 :  internalStats.getMax());
-        internalGaugeMetricFamily.addMetric(Collections.singletonList("pending"),  internalStats == null ? -1 :  internalStats.getPending());
+        setMetrics(ZimbraHttpConnectionManager.getInternalHttpConnMgr(), internalGaugeMetricFamily);
         mfs.add(internalGaugeMetricFamily);
-        GaugeMetricFamily externalGaugeMetricFamily = new GaugeMetricFamily(
-                "http_connection_pool_external",
-                "Metrics for external pool",
-                Collections.singletonList("pool"));
+
+        GaugeMetricFamily externalGaugeMetricFamily = getGaugeMetricFamily("external");
+        setMetrics(ZimbraHttpConnectionManager.getExternalHttpConnMgr(), externalGaugeMetricFamily);
         mfs.add(externalGaugeMetricFamily);
-
-        PoolStats externalStats = null;
-        ZimbraHttpConnectionManager externalHttpConnMgr = ZimbraHttpConnectionManager.getExternalHttpConnMgr();
-        if (externalHttpConnMgr != null && externalHttpConnMgr.getHttpConnMgr() instanceof PoolingHttpClientConnectionManager) {
-            externalStats = ((PoolingHttpClientConnectionManager) externalHttpConnMgr.getHttpConnMgr()).getTotalStats();
-        }
-
-        externalGaugeMetricFamily.addMetric(Collections.singletonList("available"), externalStats == null ? -1 :  externalStats.getAvailable());
-        externalGaugeMetricFamily.addMetric(Collections.singletonList("leased"),  externalStats == null ? -1 :  externalStats.getLeased());
-        externalGaugeMetricFamily.addMetric(Collections.singletonList("max"),  externalStats == null ? -1 :  externalStats.getMax());
-        externalGaugeMetricFamily.addMetric(Collections.singletonList("pending"),  externalStats == null ? -1 :  externalStats.getPending());
-
         return mfs;
+    }
+
+    /**
+     * get Gauge Metric Family
+     */
+    GaugeMetricFamily getGaugeMetricFamily(String type) {
+        return new GaugeMetricFamily(
+                "http_connection_" + POOL + "_" + type,
+                "Metrics for " + type + " " + POOL,
+                Collections.singletonList(POOL));
+    }
+
+    /**
+     * set metrics
+     */
+    void setMetrics(ZimbraHttpConnectionManager zimbraHttpConnectionManager, GaugeMetricFamily gaugeMetricFamily) {
+        PoolStats poolStats = null;
+        if (zimbraHttpConnectionManager != null && zimbraHttpConnectionManager.getHttpConnMgr() instanceof PoolingHttpClientConnectionManager) {
+            poolStats = ((PoolingHttpClientConnectionManager) zimbraHttpConnectionManager.getHttpConnMgr()).getTotalStats();
+        }
+        if (poolStats == null) {
+            return;
+        }
+        gaugeMetricFamily.addMetric(Collections.singletonList("available"), poolStats.getAvailable());
+        gaugeMetricFamily.addMetric(Collections.singletonList("leased"), poolStats.getLeased());
+        gaugeMetricFamily.addMetric(Collections.singletonList("max"), poolStats.getMax());
+        gaugeMetricFamily.addMetric(Collections.singletonList("pending"), poolStats.getPending());
     }
 }
