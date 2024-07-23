6117a5e0b276e53f4e22f21540b2420bcd420a04
Mauro Bartolomeoli <maurobartolomeoli@gmail.com>
2024-07-23 15:22:33 +0200

    chore: added build for ubuntu-focal and fixed installation script

--- install_packages.sh
+++ install_packages.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
+OS=${2:-"ubuntu-jammy"}
 HOST=$1
-TMP_FOLDER="/tmp/$(date +%s)"
-DEB_PACKAGES_TO_INSTALL="carbonio-common-core*focal_amd64.deb"
 
-rsync -acz --progress --stats artifacts/${DEB_PACKAGES_TO_INSTALL} root@${HOST}:${TMP_FOLDER}
+TMP_FOLDER="/tmp/$(date +%s)"
+DEB_PACKAGES_TO_INSTALL="carbonio-mailbox*_amd64.deb"
+
+rsync -acz --progress --stats artifacts/${OS}/${DEB_PACKAGES_TO_INSTALL} root@${HOST}:${TMP_FOLDER}/
 ssh root@${HOST} "dpkg -i ${TMP_FOLDER}/*.deb"
 ssh -t root@${HOST} "pending-setups -a"
 
