6ee6ac5cc2a1c7a9d7699ef6517248984525a71e
Mauro Bartolomeoli <maurobartolomeoli@gmail.com>
2024-07-24 10:58:24 +0200

    chore: added mailbox restart to install script

--- install_packages.sh
+++ install_packages.sh
@@ -9,4 +9,12 @@ DEB_PACKAGES_TO_INSTALL="carbonio-mailbox*_amd64.deb"
 rsync -acz --progress --stats artifacts/${OS}/${DEB_PACKAGES_TO_INSTALL} root@${HOST}:${TMP_FOLDER}/
 ssh root@${HOST} "dpkg -i ${TMP_FOLDER}/*.deb"
 ssh -t root@${HOST} "pending-setups -a"
+echo ""
+ssh -q root@${HOST} "/bin/bash" <<EOF
+su - zextras --
+echo -n -e "\033[1;31m"
+zmmailboxdctl restart
+echo -n -e "\033[0m"
+exit
+EOF
 
