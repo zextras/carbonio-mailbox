9fee0f990c13908d01212c23896a6b4fa4e65362
Matteo Baglini <matteo.baglini@gmail.com>
2024-09-16 15:16:42 +0200

    feat: put all account's calendar inside all calendars group

--- store/src/main/java/com/zimbra/cs/service/mail/GetCalendarGroups.java
+++ store/src/main/java/com/zimbra/cs/service/mail/GetCalendarGroups.java
@@ -3,27 +3,36 @@ package com.zimbra.cs.service.mail;
 import com.zimbra.common.service.ServiceException;
 import com.zimbra.common.soap.Element;
 import com.zimbra.common.soap.MailConstants;
+import com.zimbra.cs.index.SortBy;
 import java.util.Map;
 
 public class GetCalendarGroups extends MailDocumentHandler {
+
+  private static final String ALL_CALENDARS_GROUP_ID = "a970bb9528c94c40bd51bfede60fcb31";
+  private static final String ALL_CALENDARS_GROUP_NAME = "All Calendars";
+
   @Override
   public Element handle(Element request, Map<String, Object> context) throws ServiceException {
     final var zsc = getZimbraSoapContext(context);
     final var account = getRequestedAccount(getZimbraSoapContext(context));
+    final var mbox = getRequestedMailbox(zsc);
+    final var octxt = getOperationContext(zsc, context);
 
     if (!canAccessAccount(zsc, account))
       throw ServiceException.PERM_DENIED("can not access account");
 
+    final var calendarFolders = mbox.getCalendarFolders(octxt, SortBy.NAME_ASC);
+
     final var response = zsc.createElement(MailConstants.GET_CALENDAR_GROUPS_RESPONSE);
 
     final var group1 = response.addNonUniqueElement("group");
-    // TODO: use real group id
-    group1.addAttribute("id", "123");
-    group1.addAttribute("name", "All Calendars");
+    group1.addAttribute("id", ALL_CALENDARS_GROUP_ID);
+    group1.addAttribute("name", ALL_CALENDARS_GROUP_NAME);
 
-    // TODO: load all calendars and use their ids
-    final var calendarId = group1.addNonUniqueElement("calendarId");
-    calendarId.setText("555");
+    for (final var calendarFolder : calendarFolders) {
+      final var calendarId = group1.addNonUniqueElement("calendarId");
+      calendarId.setText(calendarFolder.getFolderIdAsString());
+    }
 
     return response;
   }
