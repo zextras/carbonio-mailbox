5e6d7810129a1f5a957ae498d6d615b182c0ad29
drazen04 <hangtime23@hotmail.it>
2024-09-26 12:37:25 +0200

    feat: add getCalendarGroups.

--- store/src/main/java/com/zimbra/cs/service/mail/GetCalendarGroups.java
+++ store/src/main/java/com/zimbra/cs/service/mail/GetCalendarGroups.java
@@ -6,6 +6,8 @@ import com.zimbra.common.soap.MailConstants;
 import com.zimbra.cs.index.SortBy;
 import com.zimbra.cs.mailbox.Folder;
 import com.zimbra.soap.ZimbraSoapContext;
+
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
@@ -14,6 +16,15 @@ public class GetCalendarGroups extends MailDocumentHandler {
   // TODO - double: use UUID or a fixed string like "all-calendars-id"?
   private static final String ALL_CALENDARS_GROUP_ID = "a970bb9528c94c40bd51bfede60fcb31";
   private static final String ALL_CALENDARS_GROUP_NAME = "All calendars";
+
+  private static final String LIST_SEPARATOR = "#";
+  private static final String CALENDAR_IDS_SECTION_KEY = "calendarIds";
+  private static final String CALENDAR_IDS_METADATA_KEY = "cids";
+
+  private static final String GROUP_ELEMENT_NAME = "group";
+  private static final String ID_ELEMENT_NAME = "id";
+  private static final String NAME_ELEMENT_NAME = "name";
+  private static final String CALENDAR_ID_ELEMENT_NAME = "calendarId";
 
   @Override
   public Element handle(Element request, Map<String, Object> context) throws ServiceException {
@@ -27,38 +38,58 @@ public class GetCalendarGroups extends MailDocumentHandler {
     final var octxt = getOperationContext(zsc, context);
 
     final var calendars = mbox.getCalendarFolders(octxt, SortBy.NAME_ASC);
+    final var groups = mbox.getCalendarGroups(octxt, SortBy.NAME_ASC);
     // TODO - double: load all calendar groups from datastore
 
-    return buildResponse(zsc, calendars);
+    return buildResponse(zsc, calendars, groups);
   }
 
-  private static Element buildResponse(ZimbraSoapContext zsc, List<Folder> calendars) {
+  private static Element buildResponse(ZimbraSoapContext zsc, List<Folder> calendars, List<Folder> groups) throws ServiceException {
     final var response = zsc.createElement(MailConstants.GET_CALENDAR_GROUPS_RESPONSE);
 
     addAllCalendarsGroup(calendars, response);
     addFakeCalendarGroup(calendars, response);
+    addGroups(groups, response);
     // TODO - double: add other groups loaded from datastore
     return response;
   }
 
+  private static void addGroups(List<Folder> groups, Element response) throws ServiceException {
+    for (final var group : groups) {
+      final var groupElement = response.addNonUniqueElement(GROUP_ELEMENT_NAME);
+      groupElement.addAttribute(ID_ELEMENT_NAME, group.getId());
+      groupElement.addAttribute(NAME_ELEMENT_NAME, group.getName());
+      for (final var calendarId : decodeCustomMetadata(group)) {
+        final var calendarIdElement = groupElement.addNonUniqueElement(CALENDAR_ID_ELEMENT_NAME);
+        calendarIdElement.setText(calendarId);
+      }
+    }
+  }
+
   private static void addAllCalendarsGroup(List<Folder> calendars, Element response) {
-    final var allCalendarsGroup = response.addNonUniqueElement("group");
-    allCalendarsGroup.addAttribute("id", ALL_CALENDARS_GROUP_ID);
-    allCalendarsGroup.addAttribute("name", ALL_CALENDARS_GROUP_NAME);
+    final var allCalendarsGroup = response.addNonUniqueElement(GROUP_ELEMENT_NAME);
+    allCalendarsGroup.addAttribute(ID_ELEMENT_NAME, ALL_CALENDARS_GROUP_ID);
+    allCalendarsGroup.addAttribute(NAME_ELEMENT_NAME, ALL_CALENDARS_GROUP_NAME);
 
     for (final var calendarFolder : calendars) {
-      final var calendarId = allCalendarsGroup.addNonUniqueElement("calendarId");
+      final var calendarId = allCalendarsGroup.addNonUniqueElement(CALENDAR_ID_ELEMENT_NAME);
       calendarId.setText(calendarFolder.getFolderIdAsString());
     }
   }
 
   private static void addFakeCalendarGroup(List<Folder> calendars, Element response) {
-    final var allCalendarsGroup = response.addNonUniqueElement("group");
+    final var allCalendarsGroup = response.addNonUniqueElement(GROUP_ELEMENT_NAME);
     allCalendarsGroup.addAttribute("id", "aec527e27fd543ee88a1d72ebf38d63f");
     allCalendarsGroup.addAttribute("name", "Fake");
 
     final var calendarFolder = calendars.get(0);
-    final var calendarId = allCalendarsGroup.addNonUniqueElement("calendarId");
+    final var calendarId = allCalendarsGroup.addNonUniqueElement(CALENDAR_ID_ELEMENT_NAME);
     calendarId.setText(calendarFolder.getFolderIdAsString());
+  }
+
+  private static List<String> decodeCustomMetadata(Folder group) throws ServiceException {
+    final var encodedList =
+            group.getCustomData(CALENDAR_IDS_SECTION_KEY).get(CALENDAR_IDS_METADATA_KEY);
+    return Arrays.stream(encodedList.split(LIST_SEPARATOR)).toList();
   }
 }
