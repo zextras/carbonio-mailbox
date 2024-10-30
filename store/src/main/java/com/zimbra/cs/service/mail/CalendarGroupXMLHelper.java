567749dc80d15865cebbab6d6ca3c46064e54a63
drazen04 <hangtime23@hotmail.it>
2024-10-30 16:16:03 +0100

    refactor: create createAllCalendarElement in CalendarGroupXMLHelper

--- store/src/main/java/com/zimbra/cs/service/mail/CalendarGroupXMLHelper.java
+++ store/src/main/java/com/zimbra/cs/service/mail/CalendarGroupXMLHelper.java
@@ -6,6 +6,10 @@ import com.zimbra.cs.mailbox.Folder;
 import java.util.List;
 
 public class CalendarGroupXMLHelper {
+    // TODO - double: use UUID or a fixed string like "all-calendars-id"?
+    private static final String ALL_CALENDARS_GROUP_ID = "a970bb9528c94c40bd51bfede60fcb31";
+    private static final String ALL_CALENDARS_GROUP_NAME = "All calendars";
+
     private static final String GROUP_ELEMENT_NAME = "group";
     private static final String ID_ELEMENT_NAME = "id";
     private static final String NAME_ELEMENT_NAME = "name";
@@ -19,6 +23,13 @@ public class CalendarGroupXMLHelper {
         groupElement.addAttribute(ID_ELEMENT_NAME, String.valueOf(group.getId()));
         groupElement.addAttribute(NAME_ELEMENT_NAME, group.getName());
         return groupElement;
+    }
+
+    protected static Element createAllCalendarElement(Element response) {
+        final var allCalendarsGroup = response.addNonUniqueElement(GROUP_ELEMENT_NAME);
+        allCalendarsGroup.addAttribute(ID_ELEMENT_NAME, ALL_CALENDARS_GROUP_ID);
+        allCalendarsGroup.addAttribute(NAME_ELEMENT_NAME, ALL_CALENDARS_GROUP_NAME);
+        return allCalendarsGroup;
     }
 
     protected static void addCalendarIdsToGroupElement(Element groupElement, List<String> calendarIds) {
