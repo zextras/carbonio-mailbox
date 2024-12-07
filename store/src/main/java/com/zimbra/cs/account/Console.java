7e3eb2f6be6595a4cb26d2e5cf1d0a5698447596
Giovanni De Facci <giovanni.defacci@zextras.com>
2024-12-07 19:14:54 +0100

    refactor: extract Console and Category from ProvUtil

---
+++ store/src/main/java/com/zimbra/cs/account/Console.java
@@ -0,0 +1,63 @@
+package com.zimbra.cs.account;
+
+import com.google.common.base.Charsets;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintStream;
+
+public class Console {
+
+  final PrintStream stdOut;
+  final PrintStream stdError;
+
+  public Console(PrintStream stdOut, PrintStream stdError) {
+    this.stdOut = stdOut;
+    this.stdError = stdError;
+  }
+
+  public Console(OutputStream outputStream, OutputStream errorStream) {
+    this.stdOut = new PrintStream(outputStream);
+    this.stdError = new PrintStream(errorStream);
+  }
+
+  public void print(String data) {
+    this.stdOut.print(data);
+  }
+
+  public void println(Object obj) {
+    this.stdOut.println(obj);
+  }
+
+  public void println() {
+    this.stdOut.println();
+  }
+
+  public void printStacktrace(Exception exception) {
+    exception.printStackTrace(this.stdError);
+  }
+
+  public void printError(String text) {
+    PrintStream ps = this.stdError;
+    try {
+      BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(ps, Charsets.UTF_8));
+      writer.write(text + "\n");
+      writer.flush();
+    } catch (IOException e) {
+      ps.println(text);
+    }
+  }
+
+  public void printOutput(String text) {
+    try {
+      BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(this.stdOut, Charsets.UTF_8));
+      writer.write(text + "\n");
+      writer.flush();
+    } catch (IOException e) {
+      this.stdOut.println(text);
+    }
+  }
+
+}
