a166eceb7fd5606a65499ccb596273b86a6e4598
Davide Frison <davide.frison@zextras.com>
2025-01-08 14:54:26 +0100

    chore(WIP): done extracting filters

--- store/src/main/java/com/zextras/mailbox/MailboxAPIs.java
+++ store/src/main/java/com/zextras/mailbox/MailboxAPIs.java
@@ -8,8 +8,15 @@ package com.zextras.mailbox;
 
 import com.google.inject.servlet.GuiceFilter;
 import com.zextras.mailbox.servlet.GuiceMailboxServletConfig;
+import com.zimbra.common.filters.Base64Filter;
 import com.zimbra.cs.account.Config;
+import com.zimbra.cs.servlet.ContextPathBasedThreadPoolBalancerFilter;
+import com.zimbra.cs.servlet.CsrfFilter;
 import com.zimbra.cs.servlet.DoSFilter;
+import com.zimbra.cs.servlet.ETagHeaderFilter;
+import com.zimbra.cs.servlet.RequestStringFilter;
+import com.zimbra.cs.servlet.SetHeaderFilter;
+import com.zimbra.cs.servlet.SpnegoFilter;
 import com.zimbra.cs.servlet.ZimbraInvalidLoginFilter;
 import com.zimbra.cs.servlet.ZimbraQoSFilter;
 import java.util.EnumSet;
@@ -37,6 +44,36 @@ public class MailboxAPIs {
 
 		servletContextHandler.addFilter(new FilterHolder(ZimbraInvalidLoginFilter.class),"/*", EnumSet.of(DispatcherType.REQUEST));
 		servletContextHandler.addFilter(new FilterHolder(ZimbraQoSFilter.class),"/*", EnumSet.of(DispatcherType.REQUEST));
+
+		final FilterHolder contextPathBasedThreadPoolBalancerFilter = new FilterHolder(ContextPathBasedThreadPoolBalancerFilter.class);
+		contextPathBasedThreadPoolBalancerFilter.setInitParameter("suspendMs", "1000");
+		contextPathBasedThreadPoolBalancerFilter.setInitParameter("Rules", String.join(",", configuration.getHttpContextPathBasedThreadPoolBalancingFilterRules()));
+		servletContextHandler.addFilter(contextPathBasedThreadPoolBalancerFilter,"/*", EnumSet.of(DispatcherType.REQUEST));
+
+		servletContextHandler.addFilter(new FilterHolder(ETagHeaderFilter.class),"/*", EnumSet.of(DispatcherType.REQUEST));
+
+		final FilterHolder spnegoFilter = new FilterHolder(SpnegoFilter.class);
+		spnegoFilter.setInitParameter("passThruOnFailureUri", "/service/spnego");
+		spnegoFilter.setInitParameter("error401Page", "/spnego/error401.jsp");
+		servletContextHandler.addFilter(spnegoFilter,"/spnego/*", EnumSet.of(DispatcherType.REQUEST));
+
+		servletContextHandler.addFilter(new FilterHolder(SetHeaderFilter.class),"/*", EnumSet.of(DispatcherType.REQUEST));
+
+		final FilterHolder base64Filter = new FilterHolder(Base64Filter.class);
+		servletContextHandler.addFilter(base64Filter,"/user/*", EnumSet.of(DispatcherType.REQUEST));
+		servletContextHandler.addFilter(base64Filter,"/home/*", EnumSet.of(DispatcherType.REQUEST));
+
+		servletContextHandler.addFilter(new FilterHolder(RequestStringFilter.class),"/*", EnumSet.of(DispatcherType.REQUEST));
+		final FilterHolder csrfFilter = new FilterHolder(CsrfFilter.class);
+		csrfFilter.setInitParameter("csrf.req.check", "true");
+		csrfFilter.setInitParameter("allowed.referrer.host", "");
+		servletContextHandler.addFilter(csrfFilter,"/admin/soap/*", EnumSet.of(DispatcherType.REQUEST));
+		servletContextHandler.addFilter(csrfFilter,"/soap/*", EnumSet.of(DispatcherType.REQUEST));
+		servletContextHandler.addFilter(csrfFilter,"/user/*", EnumSet.of(DispatcherType.REQUEST));
+		servletContextHandler.addFilter(csrfFilter,"/home/*", EnumSet.of(DispatcherType.REQUEST));
+		servletContextHandler.addFilter(csrfFilter,"/upload/*", EnumSet.of(DispatcherType.REQUEST));
+		servletContextHandler.addFilter(csrfFilter,"/extension/*", EnumSet.of(DispatcherType.REQUEST));
+
 	}
 	private void addListeners(ServletContextHandler servletContextHandler) {
 		servletContextHandler.addEventListener(new GuiceMailboxServletConfig());
