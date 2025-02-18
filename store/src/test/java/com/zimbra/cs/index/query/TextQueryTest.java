ebb0f38dc2d8e8cd76396c96ae036f1234f70c78
Matteo Baglini <matteo.baglini@gmail.com>
2025-02-18 15:42:43 +0100

    chore: reformat

--- store/src/test/java/com/zimbra/cs/index/query/TextQueryTest.java
+++ store/src/test/java/com/zimbra/cs/index/query/TextQueryTest.java
@@ -17,6 +17,7 @@ import com.zimbra.common.soap.Element;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+
 import com.zimbra.cs.account.Account;
 import com.zimbra.cs.account.MockProvisioning;
 import com.zimbra.cs.account.Provisioning;
@@ -56,148 +57,148 @@ public final class TextQueryTest {
         MailboxTestUtil.clearData();
     }
 
- @Test
- void wildcardExpandedToNone() throws Exception {
-  Mailbox mbox = MailboxManager.getInstance().getMailboxByAccountId(MockProvisioning.DEFAULT_ACCOUNT_ID);
+    @Test
+    void wildcardExpandedToNone() throws Exception {
+        Mailbox mbox = MailboxManager.getInstance().getMailboxByAccountId(MockProvisioning.DEFAULT_ACCOUNT_ID);
 
-  ZimbraQueryResults results = mbox.index.search(new OperationContext(mbox), "none*",
-    EnumSet.of(MailItem.Type.MESSAGE), SortBy.NONE, 100);
-  assertFalse(results.hasNext());
+        ZimbraQueryResults results = mbox.index.search(new OperationContext(mbox), "none*",
+                EnumSet.of(MailItem.Type.MESSAGE), SortBy.NONE, 100);
+        assertFalse(results.hasNext());
 
-  results = mbox.index.search(new OperationContext(mbox), "from:none* AND subject:none*",
-    EnumSet.of(MailItem.Type.MESSAGE), SortBy.NONE, 100);
-  assertFalse(results.hasNext());
+        results = mbox.index.search(new OperationContext(mbox), "from:none* AND subject:none*",
+                EnumSet.of(MailItem.Type.MESSAGE), SortBy.NONE, 100);
+        assertFalse(results.hasNext());
 
-  results = mbox.index.search(new OperationContext(mbox), "from:none* OR subject:none*",
-    EnumSet.of(MailItem.Type.MESSAGE), SortBy.NONE, 100);
-  assertFalse(results.hasNext());
- }
+        results = mbox.index.search(new OperationContext(mbox), "from:none* OR subject:none*",
+                EnumSet.of(MailItem.Type.MESSAGE), SortBy.NONE, 100);
+        assertFalse(results.hasNext());
+    }
 
- @Test
- void sortByHasAttach() throws Exception {
-  Account acct = Provisioning.getInstance().getAccountById(MockProvisioning.DEFAULT_ACCOUNT_ID);
-  Mailbox mbox = MailboxManager.getInstance().getMailboxByAccount(acct);
+    @Test
+    void sortByHasAttach() throws Exception {
+        Account acct = Provisioning.getInstance().getAccountById(MockProvisioning.DEFAULT_ACCOUNT_ID);
+        Mailbox mbox = MailboxManager.getInstance().getMailboxByAccount(acct);
 
-  // setup: add a message
-  DeliveryOptions dopt = new DeliveryOptions().setFolderId(Mailbox.ID_FOLDER_INBOX).setFlags(Flag.BITMASK_UNREAD);
-  Message msg = mbox.addMessage(null, MailboxTestUtil.generateMessage("test subject"), dopt, null);
-  Message msgWithAttach = mbox.addMessage(null,
-    MailboxTestUtil.generateMessageWithAttachment("test subject has attach"), dopt, null);
+        // setup: add a message
+        DeliveryOptions dopt = new DeliveryOptions().setFolderId(Mailbox.ID_FOLDER_INBOX).setFlags(Flag.BITMASK_UNREAD);
+        Message msg = mbox.addMessage(null, MailboxTestUtil.generateMessage("test subject"), dopt, null);
+        Message msgWithAttach = mbox.addMessage(null,
+                MailboxTestUtil.generateMessageWithAttachment("test subject has attach"), dopt, null);
 
-  SearchResponse resp;
-  List<SearchHit> hits;
-  int msgId;
-  int msgWithAttachId;
-  SearchRequest sr = new SearchRequest();
-  sr.setSearchTypes("message");
-  sr.setQuery("test");
-  sr.setSortBy(SortBy.ATTACHMENT_ASC.toString());
-  resp = doSearch(sr, acct);
-  hits = resp.getSearchHits();
-  assertEquals(2, hits.size(), "Number of hits");
-  msgId = Integer.parseInt(hits.get(0).getId());
-  msgWithAttachId = Integer.parseInt(hits.get(1).getId());
-  assertEquals(msg.getId(), msgId, "correct hit ascending no attachments");
-  assertEquals(msgWithAttach.getId(), msgWithAttachId, "correct hit ascending has attachments");
+        SearchResponse resp;
+        List<SearchHit> hits;
+        int msgId;
+        int msgWithAttachId;
+        SearchRequest sr = new SearchRequest();
+        sr.setSearchTypes("message");
+        sr.setQuery("test");
+        sr.setSortBy(SortBy.ATTACHMENT_ASC.toString());
+        resp = doSearch(sr, acct);
+        hits = resp.getSearchHits();
+        assertEquals(2, hits.size(), "Number of hits");
+        msgId = Integer.parseInt(hits.get(0).getId());
+        msgWithAttachId = Integer.parseInt(hits.get(1).getId());
+        assertEquals(msg.getId(), msgId, "correct hit ascending no attachments");
+        assertEquals(msgWithAttach.getId(), msgWithAttachId, "correct hit ascending has attachments");
 
-  /* Check that we get them in the opposite order if we change the search direction */
-  sr.setSortBy(SortBy.ATTACHMENT_DESC.toString());
-  resp = doSearch(sr, acct);
-  hits = resp.getSearchHits();
-  assertEquals(2, hits.size(), "Number of hits");
-  msgId = Integer.parseInt(hits.get(1).getId());
-  msgWithAttachId = Integer.parseInt(hits.get(0).getId());
-  assertEquals(msg.getId(), msgId, "correct hit descending no attachments");
-  assertEquals(msgWithAttach.getId(), msgWithAttachId, "correct hit descending has attachments");
- }
+        /* Check that we get them in the opposite order if we change the search direction */
+        sr.setSortBy(SortBy.ATTACHMENT_DESC.toString());
+        resp = doSearch(sr, acct);
+        hits = resp.getSearchHits();
+        assertEquals(2, hits.size(), "Number of hits");
+        msgId = Integer.parseInt(hits.get(1).getId());
+        msgWithAttachId = Integer.parseInt(hits.get(0).getId());
+        assertEquals(msg.getId(), msgId, "correct hit descending no attachments");
+        assertEquals(msgWithAttach.getId(), msgWithAttachId, "correct hit descending has attachments");
+    }
 
- @Test
- void sortByIsFlagged() throws Exception {
-  Account acct = Provisioning.getInstance().getAccountById(MockProvisioning.DEFAULT_ACCOUNT_ID);
-  Mailbox mbox = MailboxManager.getInstance().getMailboxByAccount(acct);
+    @Test
+    void sortByIsFlagged() throws Exception {
+        Account acct = Provisioning.getInstance().getAccountById(MockProvisioning.DEFAULT_ACCOUNT_ID);
+        Mailbox mbox = MailboxManager.getInstance().getMailboxByAccount(acct);
 
-  // setup: add a message
-  DeliveryOptions dopt = new DeliveryOptions().setFolderId(Mailbox.ID_FOLDER_INBOX).setFlags(Flag.BITMASK_UNREAD);
-  Message msg = mbox.addMessage(null, MailboxTestUtil.generateMessage("test subject"), dopt, null);
-  dopt = new DeliveryOptions().setFolderId(Mailbox.ID_FOLDER_INBOX).setFlags(
-    Flag.BITMASK_UNREAD | Flag.BITMASK_FLAGGED);
-  Message msgWithFlag = mbox.addMessage(null, MailboxTestUtil.generateMessage("test subject flag"), dopt, null);
+        // setup: add a message
+        DeliveryOptions dopt = new DeliveryOptions().setFolderId(Mailbox.ID_FOLDER_INBOX).setFlags(Flag.BITMASK_UNREAD);
+        Message msg = mbox.addMessage(null, MailboxTestUtil.generateMessage("test subject"), dopt, null);
+        dopt = new DeliveryOptions().setFolderId(Mailbox.ID_FOLDER_INBOX).setFlags(
+                Flag.BITMASK_UNREAD | Flag.BITMASK_FLAGGED);
+        Message msgWithFlag = mbox.addMessage(null, MailboxTestUtil.generateMessage("test subject flag"), dopt, null);
 
-  SearchResponse resp;
-  List<SearchHit> hits;
-  int msgId;
-  int msgWithFlagId;
-  SearchRequest sr = new SearchRequest();
-  sr.setSearchTypes("message");
-  sr.setQuery("test");
-  sr.setSortBy(SortBy.FLAG_ASC.toString());
-  resp = doSearch(sr, acct);
-  hits = resp.getSearchHits();
-  assertEquals(2, hits.size(), "Number of hits");
-  msgId = Integer.parseInt(hits.get(0).getId());
-  msgWithFlagId = Integer.parseInt(hits.get(1).getId());
-  assertEquals(msg.getId(), msgId, "correct hit ascending unflagged");
-  assertEquals(msgWithFlag.getId(), msgWithFlagId, "correct hit ascending flagged");
+        SearchResponse resp;
+        List<SearchHit> hits;
+        int msgId;
+        int msgWithFlagId;
+        SearchRequest sr = new SearchRequest();
+        sr.setSearchTypes("message");
+        sr.setQuery("test");
+        sr.setSortBy(SortBy.FLAG_ASC.toString());
+        resp = doSearch(sr, acct);
+        hits = resp.getSearchHits();
+        assertEquals(2, hits.size(), "Number of hits");
+        msgId = Integer.parseInt(hits.get(0).getId());
+        msgWithFlagId = Integer.parseInt(hits.get(1).getId());
+        assertEquals(msg.getId(), msgId, "correct hit ascending unflagged");
+        assertEquals(msgWithFlag.getId(), msgWithFlagId, "correct hit ascending flagged");
 
-  /* Check that we get them in the opposite order if we change the search direction */
-  sr.setSortBy(SortBy.FLAG_DESC.toString());
-  resp = doSearch(sr, acct);
-  hits = resp.getSearchHits();
-  assertEquals(2, hits.size(), "Number of hits");
-  msgId = Integer.parseInt(hits.get(1).getId());
-  msgWithFlagId = Integer.parseInt(hits.get(0).getId());
-  assertEquals(msg.getId(), msgId, "correct hit descending unflagged");
-  assertEquals(msgWithFlag.getId(), msgWithFlagId, "correct hit descending flagged");
- }
+        /* Check that we get them in the opposite order if we change the search direction */
+        sr.setSortBy(SortBy.FLAG_DESC.toString());
+        resp = doSearch(sr, acct);
+        hits = resp.getSearchHits();
+        assertEquals(2, hits.size(), "Number of hits");
+        msgId = Integer.parseInt(hits.get(1).getId());
+        msgWithFlagId = Integer.parseInt(hits.get(0).getId());
+        assertEquals(msg.getId(), msgId, "correct hit descending unflagged");
+        assertEquals(msgWithFlag.getId(), msgWithFlagId, "correct hit descending flagged");
+    }
 
- @Test
- void sortByPriority() throws Exception {
-  Account acct = Provisioning.getInstance().getAccountById(MockProvisioning.DEFAULT_ACCOUNT_ID);
-  Mailbox mbox = MailboxManager.getInstance().getMailboxByAccount(acct);
+    @Test
+    void sortByPriority() throws Exception {
+        Account acct = Provisioning.getInstance().getAccountById(MockProvisioning.DEFAULT_ACCOUNT_ID);
+        Mailbox mbox = MailboxManager.getInstance().getMailboxByAccount(acct);
 
-  // setup: add a message
-  DeliveryOptions dopt = new DeliveryOptions().setFolderId(Mailbox.ID_FOLDER_INBOX).setFlags(Flag.BITMASK_UNREAD);
-  Message msg = mbox.addMessage(null, MailboxTestUtil.generateMessage("test subject"), dopt, null);
-  Message msgWithHighPri = mbox.addMessage(null,
-    MailboxTestUtil.generateHighPriorityMessage("test subject is HI-PRI"), dopt, null);
-  Message msgWithLowPri = mbox.addMessage(null,
-    MailboxTestUtil.generateLowPriorityMessage("test subject is LOW-PRI"), dopt, null);
+        // setup: add a message
+        DeliveryOptions dopt = new DeliveryOptions().setFolderId(Mailbox.ID_FOLDER_INBOX).setFlags(Flag.BITMASK_UNREAD);
+        Message msg = mbox.addMessage(null, MailboxTestUtil.generateMessage("test subject"), dopt, null);
+        Message msgWithHighPri = mbox.addMessage(null,
+                MailboxTestUtil.generateHighPriorityMessage("test subject is HI-PRI"), dopt, null);
+        Message msgWithLowPri = mbox.addMessage(null,
+                MailboxTestUtil.generateLowPriorityMessage("test subject is LOW-PRI"), dopt, null);
 
-  SearchResponse resp;
-  List<SearchHit> hits;
-  int msgId;
-  int msgWithLowPriId;
-  int msgWithHiPriId;
-  SearchRequest sr = new SearchRequest();
-  sr.setSearchTypes("message");
-  sr.setQuery("test");
-  sr.setSortBy(SortBy.PRIORITY_ASC.toString());
-  resp = doSearch(sr, acct);
-  hits = resp.getSearchHits();
-  assertEquals(3, hits.size(), "Number of hits");
-  msgId = Integer.parseInt(hits.get(1).getId());
-  msgWithHiPriId = Integer.parseInt(hits.get(2).getId());
-  msgWithLowPriId = Integer.parseInt(hits.get(0).getId());
-  assertEquals(msgWithHighPri.getId(), msgWithHiPriId, "correct hit ascending high");
-  assertEquals(msg.getId(), msgId, "correct hit ascending med");
-  assertEquals(msgWithLowPri.getId(), msgWithLowPriId, "correct hit ascending low");
+        SearchResponse resp;
+        List<SearchHit> hits;
+        int msgId;
+        int msgWithLowPriId;
+        int msgWithHiPriId;
+        SearchRequest sr = new SearchRequest();
+        sr.setSearchTypes("message");
+        sr.setQuery("test");
+        sr.setSortBy(SortBy.PRIORITY_ASC.toString());
+        resp = doSearch(sr, acct);
+        hits = resp.getSearchHits();
+        assertEquals(3, hits.size(), "Number of hits");
+        msgId = Integer.parseInt(hits.get(1).getId());
+        msgWithHiPriId = Integer.parseInt(hits.get(2).getId());
+        msgWithLowPriId = Integer.parseInt(hits.get(0).getId());
+        assertEquals(msgWithHighPri.getId(), msgWithHiPriId, "correct hit ascending high");
+        assertEquals(msg.getId(), msgId, "correct hit ascending med");
+        assertEquals(msgWithLowPri.getId(), msgWithLowPriId, "correct hit ascending low");
 
-  /* Check that we get them in the opposite order if we change the search direction */
-  sr.setSortBy(SortBy.PRIORITY_DESC.toString());
-  resp = doSearch(sr, acct);
-  hits = resp.getSearchHits();
-  assertEquals(3, hits.size(), "Number of hits");
-  msgId = Integer.parseInt(hits.get(1).getId());
-  msgWithHiPriId = Integer.parseInt(hits.get(0).getId());
-  msgWithLowPriId = Integer.parseInt(hits.get(2).getId());
-  assertEquals(msgWithHighPri.getId(), msgWithHiPriId, "correct hit descending high");
-  assertEquals(msg.getId(), msgId, "correct hit descending med");
-  assertEquals(msgWithLowPri.getId(), msgWithLowPriId, "correct hit descending low");
- }
+        /* Check that we get them in the opposite order if we change the search direction */
+        sr.setSortBy(SortBy.PRIORITY_DESC.toString());
+        resp = doSearch(sr, acct);
+        hits = resp.getSearchHits();
+        assertEquals(3, hits.size(), "Number of hits");
+        msgId = Integer.parseInt(hits.get(1).getId());
+        msgWithHiPriId = Integer.parseInt(hits.get(0).getId());
+        msgWithLowPriId = Integer.parseInt(hits.get(2).getId());
+        assertEquals(msgWithHighPri.getId(), msgWithHiPriId, "correct hit descending high");
+        assertEquals(msg.getId(), msgId, "correct hit descending med");
+        assertEquals(msgWithLowPri.getId(), msgWithLowPriId, "correct hit descending low");
+    }
 
     static SearchResponse doSearch(SearchRequest request, Account acct) throws Exception {
         Element response = new Search().handle(JaxbUtil.jaxbToElement(request, Element.XMLElement.mFactory),
-                                ServiceTestUtil.getRequestContext(acct));
+                ServiceTestUtil.getRequestContext(acct));
         SearchResponse resp = JaxbUtil.elementToJaxb(response, SearchResponse.class);
         return resp;
     }
