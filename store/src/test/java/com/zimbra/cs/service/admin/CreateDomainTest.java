75bc1dea9677056fab8003171e5a43e32c460e2c
drazen04 <hangtime23@hotmail.it>
2025-02-26 15:44:28 +0100

    test: assert that domain is set has not initialized after creation

--- store/src/test/java/com/zimbra/cs/service/admin/CreateDomainTest.java
+++ store/src/test/java/com/zimbra/cs/service/admin/CreateDomainTest.java
@@ -3,6 +3,7 @@ package com.zimbra.cs.service.admin;
 import com.zextras.mailbox.util.MailboxTestUtil;
 import com.zimbra.common.service.ServiceException;
 import com.zimbra.cs.account.AccountServiceException;
+import com.zimbra.cs.account.Domain;
 import com.zimbra.cs.account.Provisioning;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -11,8 +12,11 @@ import org.junit.jupiter.api.Test;
 import java.util.HashMap;
 import java.util.Map;
 
+import static com.zimbra.common.account.ZAttrProvisioning.A_carbonioIsInitializedForDelegation;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class CreateDomainTest {
 
@@ -37,5 +41,13 @@ class CreateDomainTest {
         final AccountServiceException actualException = assertThrows(AccountServiceException.class, () -> provisioning.createDomain("test.domain.com", extraAttr));
 
         assertEquals("zimbraDomainMaxAccounts value(-1) smaller than minimum allowed: 0", actualException.getMessage());
+    }
+
+    @Test
+    void created_domain_is_not_initialized() throws ServiceException {
+        Domain domain = provisioning.createDomain("test.domain.com", new HashMap<>());
+
+        assertTrue(domain.getAttrs(false).containsKey(A_carbonioIsInitializedForDelegation));
+        assertFalse(domain.getBooleanAttr(A_carbonioIsInitializedForDelegation, false));
     }
 }
