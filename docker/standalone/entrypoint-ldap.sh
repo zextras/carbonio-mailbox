b773039e399c599c9277e7f7e9415de421e5ee5c
Davide Frison <davide.frison@zextras.com>
2025-04-02 12:18:48 +0200

    chore: add initial entries and update admin password

--- docker/standalone/entrypoint-ldap.sh
+++ docker/standalone/entrypoint-ldap.sh
@@ -20,9 +20,19 @@ openssl req -x509 -newkey rsa:4096 -sha256 -days 3650 \
 
 sh /ldap-utils/zmldapschema
 cp -r /opt/zextras/common/etc/openldap/zimbra/config/* /opt/zextras/data/ldap/config/
-
-
+sh /ldap-utils/replace-olcRootPw
 mkdir -p /run/carbonio
 
 /opt/zextras/common/libexec/slapd -l LOCAL0 -h ldap://0.0.0.0:1389 \
--F /opt/zextras/data/ldap/config -d 256
+-F /opt/zextras/data/ldap/config -d 256 >> /tmp/openldap.log &
+
+echo "Waiting for OpenLDAP to start..."
+while ! netcat -z localhost 1389; do
+  sleep 0.5
+done
+
+echo "OpenLDAP started."
+
+sh /ldap-utils/add_initial_data
+
+tail -f /tmp/openldap.log
