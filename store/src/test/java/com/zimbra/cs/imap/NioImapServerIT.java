9e1a4fa21857001c1ae76b10b93f14b2e27b8787
Davide Frison <davide.frison@zextras.com>
2025-04-08 16:08:22 +0200

    test: NioImapServerIT test fetch messages

--- store/src/test/java/com/zimbra/cs/imap/NioImapServerIT.java
+++ store/src/test/java/com/zimbra/cs/imap/NioImapServerIT.java
@@ -1,15 +1,20 @@
 package com.zimbra.cs.imap;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.mock;
 
 import com.zextras.mailbox.util.MailboxTestUtil;
+import com.zextras.mailbox.util.MailboxTestUtil.AccountAction;
 import com.zextras.mailbox.util.MailboxTestUtil.AccountCreator;
 import com.zimbra.cs.account.Account;
 import com.zimbra.cs.account.Provisioning;
+import com.zimbra.cs.account.accesscontrol.RightManager;
+import com.zimbra.cs.mailbox.MailboxManager;
 import io.micrometer.core.instrument.MeterRegistry;
 import java.io.IOException;
+import java.util.Properties;
+import javax.mail.Session;
+import javax.mail.internet.MimeMessage;
 import org.apache.commons.net.imap.AuthenticatingIMAPClient;
 import org.apache.commons.net.imap.IMAPClient;
 import org.junit.jupiter.api.AfterAll;
@@ -21,6 +26,8 @@ class NioImapServerIT {
   private static IMAPClient imapClient;
   private static Account account;
   private static NioImapServer imapServer;
+	private static final String FIRST_MESSAGE_BODY = "This is the first message";
+  private static final String SECOND_MESSAGE_BODY = "This is the second message";
 
   @BeforeAll
   public static void setUp() throws Exception {
@@ -34,8 +41,21 @@ class NioImapServerIT {
     Provisioning provisioning = Provisioning.getInstance();
     provisioning.getConfig().setImapBindPort(7143);
     provisioning.getConfig().setImapCleartextLoginEnabled(true);
+
     AccountCreator.Factory accountFactory = new AccountCreator.Factory(provisioning);
     account = accountFactory.get().create();
+
+    AccountAction.Factory accountActionFactory = new AccountAction.Factory(MailboxManager.getInstance(), RightManager.getInstance());
+
+		MimeMessage firstMessage = new MimeMessage(Session.getInstance(new Properties()));
+    firstMessage.setSubject("Test email 1");
+    firstMessage.setText(FIRST_MESSAGE_BODY);
+    accountActionFactory.forAccount(account).saveMsgInInbox(firstMessage);
+
+		MimeMessage secondMessage = new MimeMessage(Session.getInstance(new Properties()));
+    secondMessage.setSubject("Test email 2");
+    secondMessage.setText(SECOND_MESSAGE_BODY);
+    accountActionFactory.forAccount(account).saveMsgInInbox(secondMessage);
 
     imapServer.start();
     imapClient.connect("localhost", 7143);
@@ -61,14 +81,45 @@ class NioImapServerIT {
     imapClient.list("", "*");
     var replyString = imapClient.getReplyString();
 
-    var expectedOutput =
+    var foldersOutput =
         "* LIST (\\HasNoChildren \\Drafts) \"/\" \"Drafts\"\r\n"
             + "* LIST (\\HasNoChildren) \"/\" \"INBOX\"\r\n"
             + "* LIST (\\NoInferiors \\Junk) \"/\" \"Junk\"\r\n"
             + "* LIST (\\HasNoChildren \\Sent) \"/\" \"Sent\"\r\n"
-            + "* LIST (\\HasNoChildren \\Trash) \"/\" \"Trash\"\r\n"
-            + "AAAB OK LIST completed\r\n";
+            + "* LIST (\\HasNoChildren \\Trash) \"/\" \"Trash\"\r\n";
 
-    assertEquals(expectedOutput, replyString, "List folders failed: " + replyString);
+    assertTrue(replyString.contains(foldersOutput), "List folders failed: " + replyString);
+    assertTrue(replyString.contains("OK LIST completed"), "List folders failed: " + replyString);
+  }
+
+  @Test
+  void shouldDisplayTheFirstMessage() throws IOException {
+    imapClient.login(account.getName(), account.getUserPassword());
+    imapClient.select("INBOX");
+    imapClient.fetch("1","(BODY[TEXT])");
+    var replyString = imapClient.getReplyString();
+
+    assertTrue(replyString.contains(FIRST_MESSAGE_BODY));
+  }
+
+  @Test
+  void shouldDisplayTheSecondMessage() throws IOException {
+    imapClient.login(account.getName(), account.getUserPassword());
+    imapClient.select("INBOX");
+    imapClient.fetch("2","(BODY[TEXT])");
+    var replyString = imapClient.getReplyString();
+
+    assertTrue(replyString.contains(SECOND_MESSAGE_BODY), replyString);
+  }
+
+  @Test
+  void shouldDisplayAllMessages() throws IOException {
+    imapClient.login(account.getName(), account.getUserPassword());
+    imapClient.select("INBOX");
+    imapClient.fetch("1:2","(BODY[TEXT])");
+    var replyString = imapClient.getReplyString();
+
+    assertTrue(replyString.contains(FIRST_MESSAGE_BODY), replyString);
+    assertTrue(replyString.contains(SECOND_MESSAGE_BODY), replyString);
   }
 }
