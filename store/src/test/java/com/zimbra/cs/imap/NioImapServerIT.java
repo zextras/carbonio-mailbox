b7f527660411790b81daf60800beb385ce07f959
Keshav Bhatt <keshavnrj@gmail.com>
2025-04-08 18:54:01 +0530

    test: add NioImapServer integration tests with folder listing validation

--- store/src/test/java/com/zimbra/cs/imap/NioImapServerIT.java
+++ store/src/test/java/com/zimbra/cs/imap/NioImapServerIT.java
@@ -1,11 +1,11 @@
 package com.zimbra.cs.imap;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.mock;
 
 import com.zextras.mailbox.util.MailboxTestUtil;
 import com.zextras.mailbox.util.MailboxTestUtil.AccountCreator;
-import com.zimbra.common.service.ServiceException;
 import com.zimbra.cs.account.Account;
 import com.zimbra.cs.account.Provisioning;
 import io.micrometer.core.instrument.MeterRegistry;
@@ -14,27 +14,18 @@ import org.apache.commons.net.imap.AuthenticatingIMAPClient;
 import org.apache.commons.net.imap.IMAPClient;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class NioImapServerIT {
 
-  private NioImapServer imapServer;
-  private IMAPClient imapClient;
-  private Account account;
+  private static IMAPClient imapClient;
+  private static Account account;
+  private static NioImapServer imapServer;
 
   @BeforeAll
-  public static void init() throws Exception {
+  public static void setUp() throws Exception {
     MailboxTestUtil.setUp();
-  }
 
-  @AfterAll
-  public static void tearDown() throws Exception {
-    MailboxTestUtil.tearDown();
-  }
-
-  @BeforeEach
-  public void setUp() throws ServiceException, IOException {
     ImapConfig config = new ImapConfig(false);
     MeterRegistry mockMeterRegistry = mock(MeterRegistry.class);
     imapServer = new NioImapServer(config, mockMeterRegistry);
@@ -50,11 +41,34 @@ class NioImapServerIT {
     imapClient.connect("localhost", 7143);
   }
 
+  @AfterAll
+  public static void tearDown() throws Exception {
+    imapServer.stop();
+    MailboxTestUtil.tearDown();
+  }
+
   @Test
   void shouldLogin() throws IOException {
     imapClient.login(account.getName(), account.getUserPassword());
     var replyString = imapClient.getReplyString();
 
     assertTrue(replyString.contains("OK"), "Login failed: " + replyString);
+  }
+
+  @Test
+  void shouldListFolders() throws IOException {
+    imapClient.login(account.getName(), account.getUserPassword());
+    imapClient.list("", "*");
+    var replyString = imapClient.getReplyString();
+
+    var expectedOutput =
+        "* LIST (\\HasNoChildren \\Drafts) \"/\" \"Drafts\"\r\n"
+            + "* LIST (\\HasNoChildren) \"/\" \"INBOX\"\r\n"
+            + "* LIST (\\NoInferiors \\Junk) \"/\" \"Junk\"\r\n"
+            + "* LIST (\\HasNoChildren \\Sent) \"/\" \"Sent\"\r\n"
+            + "* LIST (\\HasNoChildren \\Trash) \"/\" \"Trash\"\r\n"
+            + "AAAB OK LIST completed\r\n";
+
+    assertEquals(expectedOutput, replyString, "List folders failed: " + replyString);
   }
 }
