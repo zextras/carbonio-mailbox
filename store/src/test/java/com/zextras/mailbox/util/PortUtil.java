7c3529db1ec002cee6c30c58703fce23afa39ee0
Davide Frison <davide.frison@zextras.com>
2025-04-10 12:00:06 +0200

    test: remove description from PortUtil

--- store/src/test/java/com/zextras/mailbox/util/PortUtil.java
+++ store/src/test/java/com/zextras/mailbox/util/PortUtil.java
@@ -1,14 +1,7 @@
 package com.zextras.mailbox.util;
 
-import com.unboundid.ldap.listener.InMemoryDirectoryServer;
-import com.zimbra.cs.db.HSQLDB;
 import java.io.IOException;
 
-/**
- * This utility class allows easy setup for the Mailbox environment using {@link #setUp()} method.
- * To clean up th environment remember to call {@link #tearDown()}. It uses an {@link
- * InMemoryDirectoryServer} and an in memory database using {@link HSQLDB} as dependencies.
- */
 public class PortUtil {
   public static int findFreePort() {
     try (java.net.ServerSocket socket = new java.net.ServerSocket(0)) {
