74837847d167eab5ceff3d717656f9d3ed006c52
Davide Frison <davide.frison@zextras.com>
2025-04-14 18:24:23 +0200

    refactor: replace usages of DEFAULT_DOMAIN in SearchDirectoryTest

--- store/src/test/java/com/zimbra/cs/service/admin/SearchDirectoryTest.java
+++ store/src/test/java/com/zimbra/cs/service/admin/SearchDirectoryTest.java
@@ -1,5 +1,8 @@
 package com.zimbra.cs.service.admin;
 
+import static com.zimbra.common.soap.Element.parseXML;
+import static org.junit.jupiter.api.Assertions.*;
+
 import com.zextras.mailbox.soap.SoapTestSuite;
 import com.zextras.mailbox.util.MailboxTestUtil.AccountCreator;
 import com.zimbra.common.service.ServiceException;
@@ -10,6 +13,9 @@ import com.zimbra.soap.JaxbUtil;
 import com.zimbra.soap.admin.message.SearchDirectoryRequest;
 import com.zimbra.soap.admin.message.SearchDirectoryResponse;
 import com.zimbra.soap.admin.type.AccountInfo;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
 import org.apache.http.HttpResponse;
 import org.apache.http.HttpStatus;
 import org.apache.http.util.EntityUtils;
@@ -17,37 +23,28 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.List;
-
-import static com.zextras.mailbox.util.MailboxTestUtil.DEFAULT_DOMAIN;
-import static com.zimbra.common.soap.Element.parseXML;
-import static org.junit.jupiter.api.Assertions.*;
-
 @Tag("api")
 class SearchDirectoryTest extends SoapTestSuite {
   private static AccountCreator.Factory accountCreatorFactory;
-  private static Provisioning provisioning;
-  private static Account adminAccount;
+	private static Account adminAccount;
   private static Account secondAccount;
   private static Account firstAccount;
 
   @BeforeAll
   static void setUp() throws Exception {
-    provisioning = Provisioning.getInstance();
+		Provisioning provisioning = Provisioning.getInstance();
     provisioning.createDomain("different.com", new HashMap<>());
     accountCreatorFactory = new AccountCreator.Factory(provisioning);
-    adminAccount = newAccountOn(DEFAULT_DOMAIN).withUsername("admin.account").asGlobalAdmin().create();
-    secondAccount = newAccountOn(DEFAULT_DOMAIN).withUsername("second.account").create();
-    firstAccount = newAccountOn(DEFAULT_DOMAIN).withUsername("first.account").create();
+    adminAccount = newAccountOn(soapExtension.getDefaultDomain()).withUsername("admin.account").asGlobalAdmin().create();
+    secondAccount = newAccountOn(soapExtension.getDefaultDomain()).withUsername("second.account").create();
+    firstAccount = newAccountOn(soapExtension.getDefaultDomain()).withUsername("first.account").create();
   }
 
   @Test
   void searchAccountsInADomain() throws Exception {
     HttpResponse httpResponse = getSoapClient().newRequest()
         .setCaller(adminAccount)
-        .setSoapBody(searchAccountsByDomain(DEFAULT_DOMAIN))
+        .setSoapBody(searchAccountsByDomain(soapExtension.getDefaultDomain()))
         .execute();
 
     assertEquals(HttpStatus.SC_OK, httpResponse.getStatusLine().getStatusCode());
@@ -64,7 +61,7 @@ class SearchDirectoryTest extends SoapTestSuite {
   void searchAccountPaginationFirstPage() throws Exception {
     HttpResponse firstPageHttpResponse = getSoapClient().newRequest()
         .setCaller(adminAccount)
-        .setSoapBody(searchAccountsByDomain(DEFAULT_DOMAIN, 1, 0))
+        .setSoapBody(searchAccountsByDomain(soapExtension.getDefaultDomain(), 1, 0))
         .execute();
 
     assertEquals(HttpStatus.SC_OK, firstPageHttpResponse.getStatusLine().getStatusCode());
@@ -79,7 +76,7 @@ class SearchDirectoryTest extends SoapTestSuite {
   void searchAccountPaginationSecondPage() throws Exception {
     HttpResponse secondPageHttpResponse = getSoapClient().newRequest()
         .setCaller(adminAccount)
-        .setSoapBody(searchAccountsByDomain(DEFAULT_DOMAIN, 1, 1))
+        .setSoapBody(searchAccountsByDomain(soapExtension.getDefaultDomain(), 1, 1))
         .execute();
 
     assertEquals(HttpStatus.SC_OK, secondPageHttpResponse.getStatusLine().getStatusCode());
@@ -94,7 +91,7 @@ class SearchDirectoryTest extends SoapTestSuite {
   void searchAccountPaginationLastPage() throws Exception {
     final HttpResponse lastPageHttpResponse = getSoapClient().newRequest()
         .setCaller(adminAccount)
-        .setSoapBody(searchAccountsByDomain(DEFAULT_DOMAIN, 1, 2))
+        .setSoapBody(searchAccountsByDomain(soapExtension.getDefaultDomain(), 1, 2))
         .execute();
 
     assertEquals(HttpStatus.SC_OK, lastPageHttpResponse.getStatusLine().getStatusCode());
