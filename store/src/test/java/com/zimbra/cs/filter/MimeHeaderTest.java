5632e70ed1be9fc12f5eeb13bcedda375bcddbe3
Davide Frison <davide.frison@zextras.com>
2025-04-16 19:45:54 +0200

    fix: remove remaining usages of DEFAULT_ACCOUNT_ID pt.5

--- store/src/test/java/com/zimbra/cs/filter/MimeHeaderTest.java
+++ store/src/test/java/com/zimbra/cs/filter/MimeHeaderTest.java
@@ -5,19 +5,11 @@
 
 package com.zimbra.cs.filter;
 
-import java.util.HashMap;
-import java.util.List;
+import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import com.zextras.mailbox.util.AccountUtil;
 import com.zimbra.common.util.ArrayUtil;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.fail;
 import com.zimbra.cs.account.Account;
-import com.zimbra.cs.account.MockProvisioning;
-import com.zimbra.cs.account.Provisioning;
 import com.zimbra.cs.mailbox.DeliveryContext;
 import com.zimbra.cs.mailbox.Mailbox;
 import com.zimbra.cs.mailbox.MailboxManager;
@@ -26,6 +18,10 @@ import com.zimbra.cs.mailbox.Message;
 import com.zimbra.cs.mailbox.OperationContext;
 import com.zimbra.cs.mime.ParsedMessage;
 import com.zimbra.cs.service.util.ItemId;
+import java.util.List;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 public class MimeHeaderTest {
     private static String sampleMsg = "from: xyz@example.com\n"
@@ -68,17 +64,15 @@ public class MimeHeaderTest {
             + "\n"
             + "------=_Part_64_1822363563.1505482033554--\n";
 
-    @BeforeAll
-    public static void init() throws Exception {
-        MailboxTestUtil.initServer();
-        Provisioning prov = Provisioning.getInstance();
-        prov.createAccount("test@zimbra.com", "secret", new HashMap<String, Object>());
-    }
+  @BeforeAll
+  public static void init() throws Exception {
+    MailboxTestUtil.initServer();
+  }
 
-    @BeforeEach
-    public void setUp() throws Exception {
-        MailboxTestUtil.clearData();
-    }
+  @BeforeEach
+  public void setUp() throws Exception {
+    MailboxTestUtil.clearData();
+  }
 
  @Test
  void test() throws Exception {
@@ -110,8 +104,7 @@ public class MimeHeaderTest {
 
     private void doTest(String filterScript, String expected) throws Exception {
         try {
-            Account account = Provisioning.getInstance().getAccount(
-                    MockProvisioning.DEFAULT_ACCOUNT_ID);
+            Account account = AccountUtil.createAccount();
             RuleManager.clearCachedRules(account);
             Mailbox mbox = MailboxManager.getInstance().getMailboxByAccount(account);
 
--- store/src/test/java/com/zimbra/cs/index/query/ConvQueryTest.java
+++ store/src/test/java/com/zimbra/cs/index/query/ConvQueryTest.java
@@ -5,37 +5,33 @@
 
 package com.zimbra.cs.index.query;
 
-import java.util.HashMap;
+import static org.junit.jupiter.api.Assertions.*;
 
+import com.zextras.mailbox.util.AccountUtil;
+import com.zimbra.cs.account.Account;
+import com.zimbra.cs.mailbox.Mailbox;
+import com.zimbra.cs.mailbox.MailboxManager;
+import com.zimbra.cs.mailbox.MailboxTestUtil;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import com.zimbra.cs.account.MockProvisioning;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import com.zimbra.cs.account.Provisioning;
-import com.zimbra.cs.mailbox.Mailbox;
-import com.zimbra.cs.mailbox.MailboxManager;
-import com.zimbra.cs.mailbox.MailboxTestUtil;
-
 public final class ConvQueryTest {
 
-    @BeforeAll
-    public static void init() throws Exception {
-        MailboxTestUtil.initServer();
-        Provisioning prov = Provisioning.getInstance();
-        prov.createAccount("test@zimbra.com", "secret", new HashMap<String, Object>());
-    }
+  @BeforeAll
+  public static void init() throws Exception {
+    MailboxTestUtil.initServer();
+  }
 
-    @BeforeEach
-    public void setUp() throws Exception {
-        MailboxTestUtil.clearData();
-    }
+  @BeforeEach
+  public void setUp() throws Exception {
+    MailboxTestUtil.clearData();
+  }
 
  @Test
  void remoteConvId() throws Exception {
-  Mailbox mbox = MailboxManager.getInstance().getMailboxByAccountId(MockProvisioning.DEFAULT_ACCOUNT_ID);
+   final Account account = AccountUtil.createAccount();
+   Mailbox mbox = MailboxManager.getInstance().getMailboxByAccountId(account.getId());
   assertEquals("<DB[CONV:\"11111111-1111-1111-1111-111111111111:111\" ]>",
     ConvQuery.create(mbox, "11111111-1111-1111-1111-111111111111:111").compile(mbox, true).toString());
  }
