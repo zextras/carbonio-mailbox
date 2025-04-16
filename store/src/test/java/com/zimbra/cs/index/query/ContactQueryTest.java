c0c64caf8338f0f2448b8b63c781dee9d1db5698
Davide Frison <davide.frison@zextras.com>
2025-04-16 19:21:36 +0200

    fix: remove remaining usages of DEFAULT_ACCOUNT_ID
    - ItemQueryTest
    - ImportanceTestTest
    - ValueComparisonTest
    - AttachmentQueryTest
    - FlaggedTestTest
    - ParseMailboxIDTest
    - ContactQueryTest
    - MetadataTest

--- store/src/test/java/com/zimbra/cs/index/query/ContactQueryTest.java
+++ store/src/test/java/com/zimbra/cs/index/query/ContactQueryTest.java
@@ -5,21 +5,10 @@
 
 package com.zimbra.cs.index.query;
 
-import java.util.EnumSet;
-import java.util.HashMap;
-import java.util.Map;
-
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 import com.zimbra.common.mailbox.ContactConstants;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
-import com.zimbra.cs.account.MockProvisioning;
+import com.zimbra.cs.account.Account;
 import com.zimbra.cs.account.Provisioning;
 import com.zimbra.cs.index.SortBy;
 import com.zimbra.cs.index.ZimbraQueryResults;
@@ -30,6 +19,13 @@ import com.zimbra.cs.mailbox.MailboxManager;
 import com.zimbra.cs.mailbox.MailboxTestUtil;
 import com.zimbra.cs.mailbox.OperationContext;
 import com.zimbra.cs.mime.ParsedContact;
+import java.util.EnumSet;
+import java.util.HashMap;
+import java.util.Map;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
 
 /**
  * Unit test for {@link ContactQuery}.
@@ -38,26 +34,28 @@ import com.zimbra.cs.mime.ParsedContact;
  */
 public final class ContactQueryTest {
 
-    @BeforeAll
-    public static void init() throws Exception {
-        MailboxTestUtil.initServer();
-        Provisioning prov = Provisioning.getInstance();
-        prov.createAccount("test@zimbra.com", "secret", new HashMap<String, Object>());
-    }
+  private static Account account;
 
-    @BeforeEach
-    public void setUp() throws Exception {
-        MailboxTestUtil.clearData();
-    }
+  @BeforeAll
+  public static void init() throws Exception {
+    MailboxTestUtil.initServer();
+    Provisioning prov = Provisioning.getInstance();
+    account = prov.createAccount("test@zimbra.com", "secret", new HashMap<String, Object>());
+  }
+
+  @BeforeEach
+  public void setUp() throws Exception {
+    MailboxTestUtil.clearData();
+  }
 
  @Test
- void tokenize() throws Exception {
+ void tokenize() {
   assertEquals("Q(CONTACT:john,smith)", new ContactQuery("John Smith").toString());
  }
 
     @Disabled
     public void search() throws Exception {
-        Mailbox mbox = MailboxManager.getInstance().getMailboxByAccountId(MockProvisioning.DEFAULT_ACCOUNT_ID);
+        Mailbox mbox = MailboxManager.getInstance().getMailboxByAccountId(account.getId());
 
         Map<String, Object> fields = new HashMap<String, Object>();
         fields.put(ContactConstants.A_firstName, "Michael");
@@ -77,7 +75,7 @@ public final class ContactQueryTest {
 
  @Test
  void wildcard() throws Exception {
-  Mailbox mbox = MailboxManager.getInstance().getMailboxByAccountId(MockProvisioning.DEFAULT_ACCOUNT_ID);
+  Mailbox mbox = MailboxManager.getInstance().getMailboxByAccountId(account.getId());
 
   Map<String, Object> fields = new HashMap<String, Object>();
   fields.put(ContactConstants.A_firstName, "First*");
