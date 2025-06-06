5aa17272c353e22ca65f79c57db8cb3c1bb727df
Davide Frison <davide.frison@zextras.com>
2025-06-06 17:45:26 +0200

    test(ssdb): add get tests for all supported data types

--- store/src/test/java/com/zextras/mailbox/store/ephemeral/SSDBEphemeralStoreTest.java
+++ store/src/test/java/com/zextras/mailbox/store/ephemeral/SSDBEphemeralStoreTest.java
@@ -36,15 +36,16 @@ class SSDBEphemeralStoreTest {
 
 	private static EphemeralInput[] generateInput() {
 		return new EphemeralInput[]{
-				new EphemeralInput(new EphemeralKey("test1"), "value1"),
-				new EphemeralInput(new EphemeralKey("test2"), "value2"),
-				new EphemeralInput(new EphemeralKey("test3"), "value3")
+				new EphemeralInput(new EphemeralKey("testString"), "value1"),
+				new EphemeralInput(new EphemeralKey("testBoolean"), true),
+				new EphemeralInput(new EphemeralKey("testInteger"), 10),
+				new EphemeralInput(new EphemeralKey("testLong"), 10L)
 		};
 	}
 
 	@ParameterizedTest
 	@MethodSource ("generateInput")
-	void getString(EphemeralInput input) throws ServiceException {
+	void get(EphemeralInput input) throws ServiceException {
 		final SSDBEphemeralStore ssdbEphemeralStore = SSDBEphemeralStore.create(
 				redisContainer.getRedisURI());
 		final TestLocation testLocation = new TestLocation(new String[]{"ccc"});
@@ -52,22 +53,7 @@ class SSDBEphemeralStoreTest {
 
 		final EphemeralResult ephemeralResult = ssdbEphemeralStore.get(input.getEphemeralKey(),
 				testLocation);
-		Assertions.assertEquals(input.getValue(), ephemeralResult.getValue());
-	}
-
-	@Test
-	void getBoolean() throws ServiceException {
-		final SSDBEphemeralStore ssdbEphemeralStore = SSDBEphemeralStore.create(
-				redisContainer.getRedisURI());
-		final TestLocation testLocation = new TestLocation(new String[]{"ccc"});
-		final EphemeralKey key = new EphemeralKey("test");
-		final Boolean value = true;
-		final EphemeralInput input = new EphemeralInput(key, value);
-		ssdbEphemeralStore.set(input, testLocation);
-
-		final EphemeralResult ephemeralResult = ssdbEphemeralStore.get(key,
-				testLocation);
-		Assertions.assertEquals(value.toString(), ephemeralResult.getValue());
+		Assertions.assertEquals(input.getValue().toString(), ephemeralResult.getValue());
 	}
 
 	@Test
