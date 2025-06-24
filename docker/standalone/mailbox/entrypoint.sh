3625431c7a0c756d0310c03a1258268d5e3adc6e
Davide Frison <davide.frison@zextras.com>
2025-06-24 16:21:40 +0200

    chore: move localconfig under /opt/zextras/conf and add zmgsautil script to create GAL account

--- docker/standalone/mailbox/entrypoint.sh
+++ docker/standalone/mailbox/entrypoint.sh
@@ -5,19 +5,20 @@
 # SPDX-License-Identifier: AGPL-3.0-only
 #
 
-sed -i -e "s#LDAP_URL#${LDAP_URL}#g" /localconfig/localconfig.xml
-sed -i -e "s/LDAP_ROOT_PASSWORD/${LDAP_ROOT_PASSWORD}/g" /localconfig/localconfig.xml
-sed -i -e "s/LDAP_ADMIN_PASSWORD/${LDAP_ADMIN_PASSWORD}/g" /localconfig/localconfig.xml
-sed -i -e "s/MARIADB_ROOT_PASSWORD/${MARIADB_ROOT_PASSWORD}/g" /localconfig/localconfig.xml
-sed -i -e "s/MARIADB_URL/${MARIADB_URL}/g" /localconfig/localconfig.xml
-sed -i -e "s/MARIADB_PORT/${MARIADB_PORT}/g" /localconfig/localconfig.xml
-sed -i -e "s/SERVER_HOSTNAME/${HOSTNAME}/g" /localconfig/localconfig.xml
+LOCALCONFIG_FILE="/opt/zextras/conf/localconfig.xml"
+sed -i -e "s#LDAP_URL#${LDAP_URL}#g" "${LOCALCONFIG_FILE}"
+sed -i -e "s/LDAP_ROOT_PASSWORD/${LDAP_ROOT_PASSWORD}/g" "${LOCALCONFIG_FILE}"
+sed -i -e "s/LDAP_ADMIN_PASSWORD/${LDAP_ADMIN_PASSWORD}/g" "${LOCALCONFIG_FILE}"
+sed -i -e "s/MARIADB_ROOT_PASSWORD/${MARIADB_ROOT_PASSWORD}/g" "${LOCALCONFIG_FILE}"
+sed -i -e "s/MARIADB_URL/${MARIADB_URL}/g" "${LOCALCONFIG_FILE}"
+sed -i -e "s/MARIADB_PORT/${MARIADB_PORT}/g" "${LOCALCONFIG_FILE}"
+sed -i -e "s/SERVER_HOSTNAME/${HOSTNAME}/g" "${LOCALCONFIG_FILE}"
 
-SERVER_EXISTS=$(/usr/bin/zmprov -l gs "${HOSTNAME}" 2>&1)
+SERVER_EXISTS=$(zmprov -l gs "${HOSTNAME}" 2>&1)
 if [[ $SERVER_EXISTS == *"account.NO_SUCH_SERVER"* ]]; then
   echo "Creating server ${HOSTNAME}"
-  /usr/bin/zmprov -l cs "${HOSTNAME}" zimbraServiceInstalled mailbox zimbraServiceEnabled mailbox zimbraServiceEnabled service
+  zmprov -l cs "${HOSTNAME}" zimbraServiceInstalled mailbox zimbraServiceEnabled mailbox zimbraServiceEnabled service
   echo "Server ${HOSTNAME} created"
 fi
 
-java ${JAVA_ARGS}
+mailbox
--- docker/standalone/provisioning.sh
+++ docker/standalone/provisioning.sh
@@ -13,6 +13,7 @@ ca test@demo.zextras.io password
 ca admin@demo.zextras.io password zimbraIsAdminAccount TRUE
 EOF
 zmprov < /tmp/prov.ls"
+docker compose exec mailbox1 bash -c "zmgsautil createAccount -a galsync@demo.zextras.io -n InternalGAL --domain demo.zextras.io -t zimbra -f _InternalGAL -s mailbox1.demo.zextras.io"
 
 docker compose exec mailbox2 bash -c "> /tmp/prov2.ls && cat > /tmp/prov2.ls <<EOF
 cd demo2.zextras.io
@@ -20,5 +21,6 @@ ca test2@demo2.zextras.io password
 ca admin2@demo2.zextras.io password zimbraIsAdminAccount TRUE
 EOF
 zmprov < /tmp/prov2.ls"
+
 
 echo "Provisioning completed"
