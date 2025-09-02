16e71997d404cfa45c39366e8ece231a19517514
Davide Frison <davide.frison@zextras.com>
2025-09-02 19:29:22 +0200

    chore: define new yap build directory

--- build_packages.sh
+++ build_packages.sh
@@ -11,31 +11,34 @@ then
   echo "Please provide an OS as argument: (ubuntu-jammy, rocky-8)"
   exit 1
 fi
+# NOTE: this folder is referenced also in PKGBUILDs
+# yap is not able to retrieve files outside from PKGBUILD directories,
+# so build files must be placed in a known directory
 
+YAP_BUILD_PATH="/tmp/mailbox_build"
 echo "Building for OS: $OS"
-
 if [[ $OS == "ubuntu-jammy" ]]
 then
   docker run -it --rm \
     --entrypoint=yap \
     -v $(pwd)/artifacts/ubuntu-jammy:/artifacts \
-    -v $(pwd):/tmp/staging \
-    docker.io/m0rf30/yap-ubuntu-jammy:1.8 \
-    build ubuntu-jammy /tmp/staging/packages
+    -v $(pwd):"${YAP_BUILD_PATH}" \
+    --entrypoint /bin/bash \
+    docker.io/m0rf30/yap-ubuntu-jammy:1.8
 elif [[ $OS == "ubuntu-focal" ]]
 then
   docker run -it --rm \
     --entrypoint=yap \
     -v $(pwd)/artifacts/ubuntu-focal:/artifacts \
-    -v $(pwd):/tmp/staging \
+    -v $(pwd):"${YAP_BUILD_PATH}" \
     docker.io/m0rf30/yap-ubuntu-focal:1.8 \
-    build ubuntu-focal /tmp/staging/packages
+    build ubuntu-focal "${YAP_BUILD_PATH}"/packages
 elif [[ $OS == "rocky-8" ]]
 then
   docker run -it --rm \
     --entrypoint=yap \
     -v $(pwd)/artifacts/rocky-8:/artifacts \
-    -v $(pwd):/tmp/staging \
+    -v $(pwd):"${YAP_BUILD_PATH}" \
     docker.io/m0rf30/yap-rocky-8:1.10 \
-    build rocky-8 /tmp/staging/packages
+    build rocky-8 "${YAP_BUILD_PATH}"/packages
 fi
