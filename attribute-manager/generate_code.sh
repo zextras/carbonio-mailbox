2572c3c81b07d02cd5a05d429e417ab48abb8492
Davide Frison <davide.frison@zextras.com>
2025-09-04 15:32:16 +0200

    feat: added ldap maven module with ZAttrProvisioning generation

--- attribute-manager/generate_code.sh
+++ attribute-manager/generate_code.sh
@@ -6,3 +6,4 @@
 #
 
 mvn compile exec:java -Dexec.args="-a generateGetters -c account -i ../store/src/main/resources/conf/attrs -r a/ZAttrAccount.java"
+mvn compile exec:java -Dexec.args="-a generateProvisioning -i ../store/src/main/resources/conf/attrs -r a/ZAttrProvisioning.java"
