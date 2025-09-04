48d1354e732b57f72e6203d87f98271a03f327b1
Davide Frison <davide.frison@zextras.com>
2025-09-04 09:56:44 +0200

    WIP: extract attribute-manager module
    - try to separate the attribute generator from the build and cyclic dependency from generated classes

---
+++ attribute-manager/src/main/java/com/zimbra/cs/account/AttributeFlag.java
@@ -0,0 +1,20 @@
+// SPDX-FileCopyrightText: 2022 Synacor, Inc.
+// SPDX-FileCopyrightText: 2022 Zextras <https://www.zextras.com>
+//
+// SPDX-License-Identifier: GPL-2.0-only
+
+package com.zimbra.cs.account;
+
+public enum AttributeFlag {
+  accountInfo,
+  accountInherited,
+  accountCosDomainInherited,
+  domainAdminModifiable,
+  domainInfo,
+  domainInherited,
+  serverInherited,
+  idn,
+  ephemeral,
+  dynamic,
+  expirable
+}
