dfbb23e9403ce80b835a8278faf301c0fe9ea799
Davide Frison <davide.frison@zextras.com>
2025-09-16 11:44:44 +0200

    test: update AttributeManagerTest - does not depend on filesystem

--- attribute-manager/src/test/java/com/zimbra/cs/account/AttributeManagerTest.java
+++ attribute-manager/src/test/java/com/zimbra/cs/account/AttributeManagerTest.java
@@ -1,13 +1,11 @@
 package com.zimbra.cs.account;
 
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
-import com.zimbra.common.localconfig.LC;
 import com.zimbra.common.service.ServiceException;
 import java.util.Map;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class AttributeManagerTest {
@@ -17,29 +15,13 @@ class AttributeManagerTest {
   @AfterEach
   void cleanup() {
     AttributeManager.destroy();
-    LC.zimbra_attrs_directory.setDefault(LC.DEFAULT_ATTRS_DIRECTORY);
   }
 
   @Test
-  void shouldLoadAllAttributesWhenUsingSingletonAndZimbraAttrsDirectoryIsSet()
-      throws ServiceException {
-    LC.zimbra_attrs_directory.setDefault(BASE_ATTRIBUTES_DIRECTORY_PATH);
-
+  void shouldLoadAllAttributesWhenUsingSingleton() throws ServiceException {
     final AttributeManager attributeManager = AttributeManager.getInstance();
 
     assertLoadedAllAttributes(attributeManager);
-  }
-
-  @Test
-  void shouldThrowExceptionWhenTheFileSystemPathDoesntExist() {
-    LC.zimbra_attrs_directory.setDefault("src/main/resources/conf/non-existing-directory");
-
-    final ServiceException serviceException = Assertions.assertThrows(ServiceException.class,
-        AttributeManager::getInstance);
-
-    Assertions.assertEquals(
-        "system failure: attrs directory does not exists: src/main/resources/conf/non-existing-directory",
-        serviceException.getMessage());
   }
 
   @Test
