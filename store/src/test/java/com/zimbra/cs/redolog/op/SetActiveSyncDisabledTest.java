1a841543b479a3922187ffb36b520b92f948edcd
Davide Frison <davide.frison@zextras.com>
2025-09-22 09:46:01 +0200

    test: SetActiveSyncDisabledTest

--- store/src/test/java/com/zimbra/cs/redolog/op/SetActiveSyncDisabledTest.java
+++ store/src/test/java/com/zimbra/cs/redolog/op/SetActiveSyncDisabledTest.java
@@ -7,66 +7,55 @@ package com.zimbra.cs.redolog.op;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import com.zextras.mailbox.util.AccountUtil;
+import com.zextras.mailbox.MailboxTestSuite;
 import com.zimbra.cs.account.Account;
 import com.zimbra.cs.mailbox.Folder;
 import com.zimbra.cs.mailbox.MailItem;
 import com.zimbra.cs.mailbox.Mailbox;
 import com.zimbra.cs.mailbox.MailboxManager;
-import com.zimbra.cs.mailbox.MailboxTestUtil;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-public class SetActiveSyncDisabledTest {
+public class SetActiveSyncDisabledTest extends MailboxTestSuite {
 
-    @BeforeAll
-    public static void init() throws Exception {
-        MailboxTestUtil.initServer();
-    }
+	/**
+	 * Verifies serializing, de-serializing, and replaying for folder.
+	 */
+	@Test
+	void setDisableActiveSyncUserFolder() throws Exception {
+		final Account account = createAccount().create();
+		Mailbox mbox = MailboxManager.getInstance().getMailboxByAccountId(account.getId());
 
-    @BeforeEach
-    public void setUp() throws Exception {
-        MailboxTestUtil.clearData();
-    }
+		// Create folder.
+		Folder folder = mbox.createFolder(null, "/test",
+				new Folder.FolderOptions().setDefaultView(MailItem.Type.MESSAGE));
+		assertFalse(folder.isActiveSyncDisabled());
 
- /**
-  * Verifies serializing, de-serializing, and replaying for folder.
-  */
- @Test
- void setDisableActiveSyncUserFolder() throws Exception {
-   final Account account = AccountUtil.createAccount();
-   Mailbox mbox = MailboxManager.getInstance().getMailboxByAccountId(account.getId());
+		// Create RedoableOp.
+		SetActiveSyncDisabled redoPlayer = new SetActiveSyncDisabled(mbox.getId(), folder.getId(),
+				true);
 
-  // Create folder.
-  Folder folder = mbox.createFolder(null, "/test", new Folder.FolderOptions().setDefaultView(MailItem.Type.MESSAGE));
-  assertFalse(folder.isActiveSyncDisabled());
+		// Serialize, deserialize, and redo.
+		byte[] data = redoPlayer.testSerialize();
+		redoPlayer = new SetActiveSyncDisabled();
+		redoPlayer.setMailboxId(mbox.getId());
+		redoPlayer.testDeserialize(data);
+		redoPlayer.redo();
+		folder = mbox.getFolderById(null, folder.getId());
+		assertTrue(folder.isActiveSyncDisabled());
+	}
 
-  // Create RedoableOp.
-  SetActiveSyncDisabled redoPlayer = new SetActiveSyncDisabled(mbox.getId(), folder.getId(), true);
+	@Test
+	void setDisableActiveSyncSystemFolder() throws Exception {
+		final Account account = createAccount().create();
+		Mailbox mbox = MailboxManager.getInstance().getMailboxByAccountId(account.getId());
+		Folder folder = mbox.getFolderById(null, Mailbox.ID_FOLDER_INBOX);
+		assertFalse(folder.isActiveSyncDisabled());
 
-  // Serialize, deserialize, and redo.
-  byte[] data = redoPlayer.testSerialize();
-  redoPlayer = new SetActiveSyncDisabled();
-  redoPlayer.setMailboxId(mbox.getId());
-  redoPlayer.testDeserialize(data);
-  redoPlayer.redo();
-  folder = mbox.getFolderById(null, folder.getId());
-  assertTrue(folder.isActiveSyncDisabled());
- }
+		//try setting disableActiveSync to true!!
+		folder.setActiveSyncDisabled(true);
 
- @Test
- void setDisableActiveSyncSystemFolder() throws Exception {
-   final Account account = AccountUtil.createAccount();
-   Mailbox mbox = MailboxManager.getInstance().getMailboxByAccountId(account.getId());
-  Folder folder = mbox.getFolderById(null, Mailbox.ID_FOLDER_INBOX);
-  assertFalse(folder.isActiveSyncDisabled());
-
-  //try setting disableActiveSync to true!!
-  folder.setActiveSyncDisabled(true);
-
-  //cannot disable activesync for system folders!!
-  assertFalse(folder.isActiveSyncDisabled());
- }
+		//cannot disable activesync for system folders!!
+		assertFalse(folder.isActiveSyncDisabled());
+	}
 
 }
