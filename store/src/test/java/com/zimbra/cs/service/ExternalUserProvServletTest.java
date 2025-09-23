1e0d10438209af0003c977d9607e640647c0f8d6
Keshav Bhatt <keshavnrj@gmail.com>
2025-09-23 15:16:17 +0530

    refactor: update ExternalUserProvServletTest to extend MailboxTestSuite and improve test setup

--- store/src/test/java/com/zimbra/cs/service/ExternalUserProvServletTest.java
+++ store/src/test/java/com/zimbra/cs/service/ExternalUserProvServletTest.java
@@ -7,79 +7,72 @@ package com.zimbra.cs.service;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import com.google.common.collect.Maps;
-import com.zimbra.common.account.Key;
+import com.zextras.mailbox.MailboxTestSuite;
 import com.zimbra.common.account.ZAttrProvisioning.FeatureAddressVerificationStatus;
 import com.zimbra.cs.account.Account;
 import com.zimbra.cs.account.Provisioning;
-import com.zimbra.cs.mailbox.MailboxTestUtil;
-import java.lang.reflect.Method;
 import java.util.HashMap;
-import java.util.Map;
-import java.util.Optional;
 import javax.servlet.http.HttpServletRequest;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInfo;
 
-public class ExternalUserProvServletTest {
-  public String testName;
+public class ExternalUserProvServletTest extends MailboxTestSuite {
 
-  @BeforeAll
-  public static void init() throws Exception {
-    MailboxTestUtil.initServer();
-  }
+  private Account testAccount;
 
   @BeforeEach
-  public void before(TestInfo testInfo) throws Exception {
-    Optional<Method> testMethod = testInfo.getTestMethod();
-    if (testMethod.isPresent()) {
-      this.testName = testMethod.get().getName();
-    }
-    System.out.println(testName);
-    Provisioning prov = Provisioning.getInstance();
+  public void setUp() throws Exception {
+    clearData();
+    initData();
 
-    Map<String, Object> attrs = Maps.newHashMap();
-    attrs = Maps.newHashMap();
-    prov.createAccount("test@zimbra.com", "secret", attrs);
+    testAccount =
+        createAccount()
+            .withUsername("testuser")
+            .withDomain(DEFAULT_DOMAIN_NAME)
+            .withPassword("secret")
+            .create();
   }
 
   @Test
   void testHandleAddressVerificationExpired() throws Exception {
-    Account acct1 = Provisioning.getInstance().get(Key.AccountBy.name, "test@zimbra.com");
-    HashMap<String, String> headers = new HashMap<String, String>();
+    HashMap<String, String> headers = new HashMap<>();
     HttpServletRequest req =
         new MockHttpServletRequest(null, null, null, 123, "127.0.0.1", headers);
     MockHttpServletResponse resp = new MockHttpServletResponse();
+
     ExternalUserProvServlet servlet = new ExternalUserProvServlet();
-    servlet.handleAddressVerification(req, resp, acct1.getId(), "test2@zimbra.com", true);
-    assertNull(acct1.getPrefMailForwardingAddress());
+
+    servlet.handleAddressVerification(
+        req, resp, testAccount.getId(), "test2@" + DEFAULT_DOMAIN_NAME, true);
+
+    testAccount = Provisioning.getInstance().getAccountById(testAccount.getId());
+
+    assertNull(testAccount.getPrefMailForwardingAddress());
+
     assertEquals(
-        FeatureAddressVerificationStatus.expired, acct1.getFeatureAddressVerificationStatus());
+        FeatureAddressVerificationStatus.expired,
+        testAccount.getFeatureAddressVerificationStatus());
   }
 
   @Test
   void testHandleAddressVerificationSuccess() throws Exception {
-    Account acct1 = Provisioning.getInstance().get(Key.AccountBy.name, "test@zimbra.com");
-    HashMap<String, String> headers = new HashMap<String, String>();
+    HashMap<String, String> headers = new HashMap<>();
     HttpServletRequest req =
         new MockHttpServletRequest(null, null, null, 123, "127.0.0.1", headers);
     MockHttpServletResponse resp = new MockHttpServletResponse();
-    ExternalUserProvServlet servlet = new ExternalUserProvServlet();
-    servlet.handleAddressVerification(req, resp, acct1.getId(), "test2@zimbra.com", false);
-    assertEquals("test2@zimbra.com", acct1.getPrefMailForwardingAddress());
-    assertEquals(
-        FeatureAddressVerificationStatus.verified, acct1.getFeatureAddressVerificationStatus());
-  }
 
-  @AfterEach
-  public void tearDown() {
-    try {
-      MailboxTestUtil.clearData();
-    } catch (Exception e) {
-      e.printStackTrace();
-    }
+    ExternalUserProvServlet servlet = new ExternalUserProvServlet();
+
+    String forwardingEmail = "test2@" + DEFAULT_DOMAIN_NAME;
+
+    servlet.handleAddressVerification(req, resp, testAccount.getId(), forwardingEmail, false);
+
+    testAccount = Provisioning.getInstance().getAccountById(testAccount.getId());
+
+    assertEquals(forwardingEmail, testAccount.getPrefMailForwardingAddress());
+
+    assertEquals(
+        FeatureAddressVerificationStatus.verified,
+        testAccount.getFeatureAddressVerificationStatus());
   }
 }
