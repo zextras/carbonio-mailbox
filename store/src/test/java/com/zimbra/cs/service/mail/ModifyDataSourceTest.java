fb2265f93106d2871475a839f2af5e638f1824a3
Keshav Bhatt <keshavnrj@gmail.com>
2025-09-23 15:03:36 +0530

    refactor: extend ModifyDataSourceTest to inherit from MailboxTestSuite for improved test structure

--- store/src/test/java/com/zimbra/cs/service/mail/ModifyDataSourceTest.java
+++ store/src/test/java/com/zimbra/cs/service/mail/ModifyDataSourceTest.java
@@ -6,16 +6,9 @@
 package com.zimbra.cs.service.mail;
 
 import static org.junit.jupiter.api.Assertions.*;
-import java.lang.reflect.Method;
-import java.util.Map;
-import java.util.Optional;
-
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInfo;
 
 import com.google.common.collect.Maps;
+import com.zextras.mailbox.MailboxTestSuite;
 import com.zimbra.common.account.Key;
 import com.zimbra.common.service.ServiceException;
 import com.zimbra.common.soap.Element;
@@ -23,95 +16,81 @@ import com.zimbra.common.soap.MailConstants;
 import com.zimbra.cs.account.Account;
 import com.zimbra.cs.account.MockProvisioning;
 import com.zimbra.cs.account.Provisioning;
-
 import com.zimbra.cs.ldap.LdapConstants;
-import com.zimbra.cs.mailbox.MailboxTestUtil;
-import com.zimbra.cs.service.mail.ModifyDataSource;
 import com.zimbra.soap.type.DataSource.ConnectionType;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
-public class ModifyDataSourceTest {
+public class ModifyDataSourceTest extends MailboxTestSuite {
 
-    
-    public static String testInfo;
+  public static String testInfo;
 
-    private static String NAME_PREFIX;
+  private static String NAME_PREFIX;
 
-    @BeforeAll
-    public static void init() throws Exception {
-        MailboxTestUtil.initServer();
-        MockProvisioning prov = new MockProvisioning();
-        Provisioning.setInstance(prov);
+  @BeforeAll
+  public static void init() throws Exception {
+    MockProvisioning prov = new MockProvisioning();
+    Provisioning.setInstance(prov);
+    prov.createDomain("zimbra.com", Maps.newHashMap());
+  }
 
-        Map<String, Object> attrs = Maps.newHashMap();
-        prov.createDomain("zimbra.com", attrs);
+  @BeforeEach
+  public void setUp() throws Exception {
+    NAME_PREFIX =
+        String.format("%s-%s", ModifyDataSourceTest.class.getSimpleName(), testInfo).toLowerCase();
+  }
 
-        attrs = Maps.newHashMap();
-        prov.createAccount("test@zimbra.com", "secret", attrs);
+  @Test
+  void testValidateDataSourceEmail() {
+    try {
+      Account acct = createAccount().create();
+
+      Element request = new Element.XMLElement(MailConstants.E_CREATE_DATA_SOURCE_REQUEST);
+
+      Element ds1 = request.addUniqueElement(MailConstants.E_DS_POP3);
+
+      ds1.addAttribute(MailConstants.A_NAME, NAME_PREFIX + " testDataSourceEmailAddress1");
+      ds1.addAttribute(MailConstants.A_DS_IS_ENABLED, LdapConstants.LDAP_FALSE);
+      ds1.addAttribute(MailConstants.A_DS_HOST, "testhost.com");
+      ds1.addAttribute(MailConstants.A_DS_PORT, "0");
+      ds1.addAttribute(MailConstants.A_DS_USERNAME, "testuser1");
+      ds1.addAttribute(MailConstants.A_DS_EMAIL_ADDRESS, "testuser2@testhost.com");
+      ds1.addAttribute(MailConstants.A_DS_PASSWORD, "testpass");
+      ds1.addAttribute(MailConstants.A_FOLDER, "1");
+      ds1.addAttribute(MailConstants.A_DS_CONNECTION_TYPE, ConnectionType.cleartext.toString());
+
+      new CreateDataSource().handle(request, ServiceTestUtil.getRequestContext(acct));
+
+      Element request2 = new Element.XMLElement(MailConstants.E_CREATE_DATA_SOURCE_REQUEST);
+
+      Element ds2 = request2.addUniqueElement(MailConstants.E_DS_POP3);
+
+      ds2.addAttribute(MailConstants.A_NAME, NAME_PREFIX + " testDataSourceEmailAddress2");
+      ds2.addAttribute(MailConstants.A_DS_IS_ENABLED, LdapConstants.LDAP_FALSE);
+      ds2.addAttribute(MailConstants.A_DS_HOST, "testhost.com");
+      ds2.addAttribute(MailConstants.A_DS_PORT, "0");
+      ds2.addAttribute(MailConstants.A_DS_USERNAME, "testuser2");
+      ds2.addAttribute(MailConstants.A_DS_EMAIL_ADDRESS, "testuser3@testhost.com");
+      ds2.addAttribute(MailConstants.A_DS_PASSWORD, "testpass");
+      ds2.addAttribute(MailConstants.A_FOLDER, "1");
+      ds2.addAttribute(MailConstants.A_DS_CONNECTION_TYPE, ConnectionType.cleartext.toString());
+
+      new CreateDataSource().handle(request2, ServiceTestUtil.getRequestContext(acct));
+
+      // try to modify data source 1 to have an existing data source email address.
+      ds1.addAttribute(MailConstants.A_DS_EMAIL_ADDRESS, "testuser3@testhost.com");
+
+      try {
+        ModifyDataSource.validateDataSourceEmail(acct, ds1);
+        fail("datasource email validation failed");
+      } catch (ServiceException e) {
+        String expected = "data source already exists: testuser3@testhost.com";
+        assertTrue(e.getMessage().indexOf(expected) != -1);
+        assertNotNull(e);
+      }
+    } catch (Exception e) {
+      fail("No exception should be thrown" + e);
     }
-
- @BeforeEach
- public void setUp(TestInfo testInfo) throws Exception {
-  Optional<Method> testMethod = testInfo.getTestMethod();
-  if (testMethod.isPresent()) {
-   this.testInfo = testMethod.get().getName();
   }
-  NAME_PREFIX = String.format("%s-%s", ModifyDataSourceTest.class.getSimpleName(), testInfo).toLowerCase();
-  MailboxTestUtil.clearData();
- }
-
-
- @Test
- void testValidateDataSourceEmail() {
-  try {
-   Account acct = null;
-   acct = Provisioning.getInstance().get(Key.AccountBy.name, "test@zimbra.com");
-
-   Element request = new Element.XMLElement(MailConstants.E_CREATE_DATA_SOURCE_REQUEST);
-
-   Element ds1 = request.addUniqueElement(MailConstants.E_DS_POP3);
-
-   ds1.addAttribute(MailConstants.A_NAME, NAME_PREFIX + " testDataSourceEmailAddress1");
-   ds1.addAttribute(MailConstants.A_DS_IS_ENABLED, LdapConstants.LDAP_FALSE);
-   ds1.addAttribute(MailConstants.A_DS_HOST, "testhost.com");
-   ds1.addAttribute(MailConstants.A_DS_PORT, "0");
-   ds1.addAttribute(MailConstants.A_DS_USERNAME, "testuser1");
-   ds1.addAttribute(MailConstants.A_DS_EMAIL_ADDRESS, "testuser2@testhost.com");
-   ds1.addAttribute(MailConstants.A_DS_PASSWORD, "testpass");
-   ds1.addAttribute(MailConstants.A_FOLDER, "1");
-   ds1.addAttribute(MailConstants.A_DS_CONNECTION_TYPE, ConnectionType.cleartext.toString());
-
-   new CreateDataSource().handle(request, ServiceTestUtil.getRequestContext(acct));
-
-   Element request2 = new Element.XMLElement(MailConstants.E_CREATE_DATA_SOURCE_REQUEST);
-
-   Element ds2 = request2.addUniqueElement(MailConstants.E_DS_POP3);
-
-   ds2.addAttribute(MailConstants.A_NAME, NAME_PREFIX + " testDataSourceEmailAddress2");
-   ds2.addAttribute(MailConstants.A_DS_IS_ENABLED, LdapConstants.LDAP_FALSE);
-   ds2.addAttribute(MailConstants.A_DS_HOST, "testhost.com");
-   ds2.addAttribute(MailConstants.A_DS_PORT, "0");
-   ds2.addAttribute(MailConstants.A_DS_USERNAME, "testuser2");
-   ds2.addAttribute(MailConstants.A_DS_EMAIL_ADDRESS, "testuser3@testhost.com");
-   ds2.addAttribute(MailConstants.A_DS_PASSWORD, "testpass");
-   ds2.addAttribute(MailConstants.A_FOLDER, "1");
-   ds2.addAttribute(MailConstants.A_DS_CONNECTION_TYPE, ConnectionType.cleartext.toString());
-
-   new CreateDataSource().handle(request2, ServiceTestUtil.getRequestContext(acct));
-
-   // try to modify data source 1 to have an existing data source email address.
-   ds1.addAttribute(MailConstants.A_DS_EMAIL_ADDRESS, "testuser3@testhost.com");
-
-   try {
-    ModifyDataSource.validateDataSourceEmail(acct, ds1);
-    fail("datasource email validation failed");
-   } catch (ServiceException e) {
-    String expected = "data source already exists: testuser3@testhost.com";
-    assertTrue(e.getMessage().indexOf(expected)  != -1);
-    assertNotNull(e);
-   }
-  } catch (Exception e) {
-   fail("No exception should be thrown" + e);
-  }
- }
-
 }
