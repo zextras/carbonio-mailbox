891f833e167df0c4402a3439b5162573ecae4249
Davide Frison <davide.frison@zextras.com>
2025-10-01 19:11:26 +0200

    refactor: get rid of FirstServlet and move initialization in Mailbox

--- store/src/main/java/com/zextras/mailbox/Mailbox.java
+++ store/src/main/java/com/zextras/mailbox/Mailbox.java
@@ -5,10 +5,22 @@
 package com.zextras.mailbox;
 
 import com.zextras.mailbox.MailboxServer.Builder;
+import com.zimbra.common.localconfig.LC;
+import com.zimbra.common.net.SocketFactories;
 import com.zimbra.common.service.ServiceException;
 import com.zimbra.common.util.ZimbraLog;
 import com.zimbra.cs.account.Provisioning;
 import com.zimbra.cs.util.Zimbra;
+import com.zimbra.znative.IO;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.Timer;
+import java.util.TimerTask;
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.CommandLineParser;
 import org.apache.commons.cli.GnuParser;
@@ -19,60 +31,116 @@ import org.eclipse.jetty.util.component.LifeCycle;
 
 public class Mailbox {
 
-  private static final String LOCALCONFIG = "localconfig";
-  private static final String DRYRUN = "dryRun";
+	private static final String LOCALCONFIG = "localconfig";
+	private static final String DRYRUN = "dryRun";
 
-  public static void main(String[] args) throws Exception {
-    Options options = getOptions();
-    CommandLineParser parser = new GnuParser();
-    CommandLine commandLine = parser.parse(options, args);
+	public static void main(String[] args) throws Exception {
+		Options options = getOptions();
+		CommandLineParser parser = new GnuParser();
+		CommandLine commandLine = parser.parse(options, args);
 
-    if (commandLine.hasOption(LOCALCONFIG)) {
-      System.setProperty("zimbra.config", commandLine.getOptionValue(LOCALCONFIG));
-    }
+		if (commandLine.hasOption(LOCALCONFIG)) {
+			System.setProperty("zimbra.config", commandLine.getOptionValue(LOCALCONFIG));
+		}
 
-    Builder mailboxServerBuilder = new Builder(Provisioning.getInstance().getConfig(), Provisioning.getInstance().getLocalServer());
+		Builder mailboxServerBuilder = new Builder(Provisioning.getInstance().getConfig(),
+				Provisioning.getInstance().getLocalServer());
 
-    final Server server = mailboxServerBuilder.build();
+		final Server server = mailboxServerBuilder.build();
 
-    if (commandLine.hasOption(DRYRUN)) {
-      return;
-    }
+		if (commandLine.hasOption(DRYRUN)) {
+			return;
+		}
 
-    server.start();
-    server.addLifeCycleListener(new LifeCycle.Listener() {
-      @Override
-      public void lifeCycleStopping(LifeCycle event) {
+    initDependencies(); // old FirstServlet#init
+    Zimbra.startup();
+
+		server.addLifeCycleListener(new LifeCycle.Listener() {
+			@Override
+			public void lifeCycleStopped(LifeCycle event) {
 				try {
 					Zimbra.shutdown(); // clean shutdown of pools, timers, etc.
 				} catch (ServiceException e) {
-          ZimbraLog.misc.error("Failed to shutdown connections", e);
+					ZimbraLog.misc.error("Failed to shutdown connections", e);
 				}
 			}
-      @Override public void lifeCycleStarted(LifeCycle event) {
-        try {
-          Zimbra.startup();
-          ZimbraLog.misc.info("Mailbox dependencies started.");
-        } catch (OutOfMemoryError e) {
-          Zimbra.halt("out of memory", e);
-        }
-      }
-    });
-    server.start();
-    server.join();
-  }
+		});
+		server.start();
+		server.join();
+	}
 
-  private static Options getOptions() {
-    Options options = new Options();
+	private static Options getOptions() {
+		Options options = new Options();
 
-    Option dryRun =  new Option(DRYRUN,true, "If set does not start the server");
-    dryRun.setRequired(false);
-    options.addOption(dryRun);
+		Option dryRun = new Option(DRYRUN, true, "If set does not start the server");
+		dryRun.setRequired(false);
+		options.addOption(dryRun);
 
-    return options;
-  }
+		return options;
+	}
 
 
+	private static void initDependencies() {
+		try {
+			System.setProperty("javax.net.ssl.keyStore", LC.mailboxd_keystore.value());
+			System.setProperty("javax.net.ssl.keyStorePassword", LC.mailboxd_keystore_password.value());
+			System.setProperty("javax.net.ssl.trustStorePassword",
+					LC.mailboxd_truststore_password.value());
 
+			SocketFactories.registerProtocolsServer();
+			setupOutputRotation();
+		} catch (Throwable t) {
+			System.err.println("PrivilegedServlet init failed");
+			t.printStackTrace(System.err);
+			Runtime.getRuntime().halt(1);
+		}
+	}
+
+	private static Timer sOutputRotationTimer;
+
+	private static void doOutputRotation() {
+		Date now = new Date();
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMddHHmm");
+		String suffix = sdf.format(now);
+		String current = LC.mailboxd_output_file.value();
+		String rotateTo = current + "." + suffix;
+		try {
+			new File(current).renameTo(new File(rotateTo));
+			IO.setStdoutStderrTo(current);
+		} catch (IOException ioe) {
+			System.err.println("WARN: rotate stdout stderr failed: " + ioe);
+			ioe.printStackTrace();
+		}
+	}
+
+	private static void setupOutputRotation()
+			throws SecurityException {
+		long configMillis = LC.mailboxd_output_rotate_interval.intValue() * 1000;
+
+    if (configMillis <= 0) {
+      return;
+    }
+		sOutputRotationTimer = new Timer("Timer-OutputRotation");
+		GregorianCalendar now = new GregorianCalendar();
+		long millisSinceEpoch = now.getTimeInMillis();
+		long dstOffset = now.get(Calendar.DST_OFFSET);
+		long zoneOffset = now.get(Calendar.ZONE_OFFSET);
+		long millisSinceEpochLocal = millisSinceEpoch + dstOffset + zoneOffset;
+		long firstRotateInMillis = configMillis - (millisSinceEpochLocal % configMillis);
+		TimerTask tt = new TimerTask() {
+			public void run() {
+				try {
+					doOutputRotation();
+				} catch (Throwable e) {
+          if (e instanceof OutOfMemoryError) {
+            Zimbra.halt("Caught out of memory error", e);
+          }
+					System.err.println("WARN: Caught exception in FirstServlet timer " + e);
+					e.printStackTrace();
+				}
+			}
+		};
+		sOutputRotationTimer.scheduleAtFixedRate(tt, firstRotateInMillis, configMillis);
+	}
 
 }
