580d0a2fc686c36c9b0773104434f462810bfe87
Davide Frison <davide.frison@zextras.com>
2025-10-02 12:21:08 +0200

    chore: disable MailboxServerAPITest to see if it causes lock due to shutdown/startup

--- store/src/test/java/com/zextras/mailbox/server/MailboxServerAPITest.java
+++ store/src/test/java/com/zextras/mailbox/server/MailboxServerAPITest.java
@@ -18,11 +18,13 @@ import java.nio.file.Files;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.io.TempDir;
 
 @Tag("api")
+@Disabled
 class MailboxServerAPITest {
 
 	private static MailboxServer mailboxServer;
