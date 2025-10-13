c9447d1b6fe9eb80bf126c8920ba71ec6903fb46
Davide Frison <davide.frison@zextras.com>
2025-10-13 19:19:01 +0200

    test: completely remove easymock

--- store/src/test/java/com/zimbra/cs/redolog/CommitIdTest.java
+++ store/src/test/java/com/zimbra/cs/redolog/CommitIdTest.java
@@ -5,64 +5,58 @@
 package com.zimbra.cs.redolog;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
 
 import com.zimbra.common.service.ServiceException;
-import com.zimbra.cs.redolog.CommitId;
-import com.zimbra.cs.redolog.TransactionId;
 import com.zimbra.cs.redolog.op.CommitTxn;
-
-import org.easymock.EasyMock;
-import org.easymock.EasyMockSupport;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
-public class CommitIdTest extends EasyMockSupport {
+public class CommitIdTest {
     private CommitId id;
     private CommitTxn commitTxn;
 
-    @BeforeEach
-    public void setUp() {
-        commitTxn = createMock(CommitTxn.class);
-        EasyMock.expect(commitTxn.getTimestamp()).andReturn((long)1);
-        EasyMock.expect(commitTxn.getTransactionId())
-            .andReturn(new TransactionId(2, 3));
-    }
+  @BeforeEach
+  public void setUp() {
+    commitTxn = Mockito.mock(CommitTxn.class);
+    when(commitTxn.getTimestamp()).thenReturn(1L);
+    when(commitTxn.getTransactionId()).thenReturn(new TransactionId(2, 3));
+  }
 
- @Test
- void id() {
-  replayAll();
-  id = new CommitId(4, commitTxn);
-  assertEquals(4, id.getRedoSeq(), "Sequence != 4");
-  assertEquals("4-1-2-3", id.encodeToString(), "encodeToString != 4-1-2-3");
- }
+  @Test
+  void id() {
+    id = new CommitId(4, commitTxn);
+    assertEquals(4, id.getRedoSeq(), "Sequence != 4");
+    assertEquals("4-1-2-3", id.encodeToString(), "encodeToString != 4-1-2-3");
+  }
 
- @Test
- void matches() {
-  EasyMock.expect(commitTxn.getTimestamp()).andReturn((long) 1);
-  EasyMock.expect(commitTxn.getTransactionId())
-    .andReturn(new TransactionId(2, 3));
-  // Change timestamp for the next try.
-  EasyMock.expect(commitTxn.getTimestamp()).andReturn((long) 5);
-  EasyMock.expect(commitTxn.getTransactionId())
-    .andReturn(new TransactionId(2, 3));
-  replayAll();
-  id = new CommitId(4, commitTxn);
-  assertTrue(id.matches(commitTxn));
+  @Test
+  void matches() {
+    // First call to matches()
+    when(commitTxn.getTimestamp()).thenReturn(1L);
+    when(commitTxn.getTransactionId()).thenReturn(new TransactionId(2, 3));
 
-  assertFalse(id.matches(commitTxn));
- }
+    id = new CommitId(4, commitTxn);
+    assertTrue(id.matches(commitTxn));
 
- @Test
- void encodeDecode() throws Exception {
-  replayAll();
-  id = new CommitId(4, commitTxn);
-  assertEquals(id, CommitId.decodeFromString(id.encodeToString()), "id mismatch on decode");
- }
+    // Second call to matches() with different timestamp
+    when(commitTxn.getTimestamp()).thenReturn(5L);
+    when(commitTxn.getTransactionId()).thenReturn(new TransactionId(2, 3));
 
- @Test
- void decodeJunk() throws Exception {
-  assertThrows(ServiceException.class, () -> {
-   CommitId.decodeFromString("not-a-Commit-Id");
-  });
- }
+    assertFalse(id.matches(commitTxn));
+  }
+
+  @Test
+  void encodeDecode() throws Exception {
+    id = new CommitId(4, commitTxn);
+    assertEquals(id, CommitId.decodeFromString(id.encodeToString()), "id mismatch on decode");
+  }
+
+  @Test
+  void decodeJunk() throws Exception {
+    assertThrows(ServiceException.class, () -> {
+      CommitId.decodeFromString("not-a-Commit-Id");
+    });
+  }
 }
