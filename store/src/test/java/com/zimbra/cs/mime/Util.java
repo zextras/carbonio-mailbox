602efad10623473c0cd1a3617af8b13aacc7a9cd
Keshav Bhatt <keshavnrj@gmail.com>
2025-10-17 17:10:19 +0530

    chore: refactor invite generation to use text block for improved readability
    - remove unused imports

--- store/src/test/java/com/zimbra/cs/mime/Util.java
+++ store/src/test/java/com/zimbra/cs/mime/Util.java
@@ -1,32 +1,24 @@
 package com.zimbra.cs.mime;
 
-
-import javax.mail.Message;
-import javax.mail.MessagingException;
-import javax.mail.Session;
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeBodyPart;
-import javax.mail.internet.MimeMessage;
-import javax.mail.internet.MimeMultipart;
-
 public class Util {
 
 	public static String generateInvite() {
-		return "BEGIN:VCALENDAR\r\n" +
-						"METHOD:REQUEST\r\n" +
-						"PRODID:-//Example Corp//EN\r\n" +
-						"VERSION:2.0\r\n" +
-						"BEGIN:VEVENT\r\n" +
-						"UID:12345678@example.com\r\n" +
-						"DTSTAMP:20251017T120000Z\r\n" +
-						"DTSTART:20251018T140000Z\r\n" +
-						"DTEND:20251018T150000Z\r\n" +
-						"SUMMARY:Team Meeting\r\n" +
-						"DESCRIPTION:Discuss project status\r\n" +
-						"LOCATION:Conference Room A\r\n" +
-						"ORGANIZER:mailto:organizer@example.com\r\n" +
-						"ATTENDEE;CN=Attendee;RSVP=TRUE:mailto:attendee@example.com\r\n" +
-						"END:VEVENT\r\n" +
-						"END:VCALENDAR";
+		return """
+        BEGIN:VCALENDAR\r
+        METHOD:REQUEST\r
+        PRODID:-//Example Corp//EN\r
+        VERSION:2.0\r
+        BEGIN:VEVENT\r
+        UID:12345678@example.com\r
+        DTSTAMP:20251017T120000Z\r
+        DTSTART:20251018T140000Z\r
+        DTEND:20251018T150000Z\r
+        SUMMARY:Team Meeting\r
+        DESCRIPTION:Discuss project status\r
+        LOCATION:Conference Room A\r
+        ORGANIZER:mailto:organizer@example.com\r
+        ATTENDEE;CN=Attendee;RSVP=TRUE:mailto:attendee@example.com\r
+        END:VEVENT\r
+        END:VCALENDAR""";
 	}
 }
