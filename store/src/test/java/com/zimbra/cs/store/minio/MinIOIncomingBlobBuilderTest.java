bad72b2fd782ebe3767df3ff26601cd9feb12365
Davide Frison <davide.frison@zextras.com>
2025-11-04 09:48:22 +0100

    test: add tests for MinIOIncomingBlobBuilder

---
+++ store/src/test/java/com/zimbra/cs/store/minio/MinIOIncomingBlobBuilderTest.java
@@ -0,0 +1,44 @@
+package com.zimbra.cs.store.minio;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import com.zimbra.common.service.ServiceException;
+import com.zimbra.cs.store.BlobBuilder;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import org.hsqldb.lib.StringInputStream;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class MinIOIncomingBlobBuilderTest {
+
+	@Test
+	void shouldReturnBuilderInSetters() throws IOException {
+		final MinIOIncomingBlobBuilder builder = new MinIOIncomingBlobBuilder("test");
+		final BlobBuilder<MinioBlob> builderInstance = builder.append("aaa".getBytes())
+				.append(new ByteArrayInputStream("bbb".getBytes()))
+				.append(new StringInputStream("ccc"))
+				.disableDigest(true);
+		Assertions.assertNotNull(builderInstance);
+	}
+
+	@Test
+	void shouldBuildABlobWithGivenKeyAndData() throws IOException, ServiceException {
+		final MinIOIncomingBlobBuilder builder = new MinIOIncomingBlobBuilder("test");
+		final MinioBlob blob = builder.append("aaa".getBytes()).finish();
+
+		Assertions.assertEquals("test",blob.getKey());
+		Assertions.assertEquals("aaa", new String(blob.getContent()));
+	}
+
+	@Test
+	void shouldOverrideContentWhenAppending() throws IOException, ServiceException {
+		final MinIOIncomingBlobBuilder builder = new MinIOIncomingBlobBuilder("test");
+		final MinioBlob blob = builder.append("aaa".getBytes())
+				.append(new ByteArrayInputStream("bbb".getBytes()))
+				.finish();
+
+		Assertions.assertEquals("bbb", new String(blob.getContent()));
+	}
+
+}
