9c55539c66a4c04ca99954871182247d962df83c
Keshav Bhatt <keshavnrj@gmail.com>
2026-01-14 17:27:43 +0530

    feat: add method to extract session ID from SOAP response header

--- store/src/test/java/com/zextras/mailbox/soap/SoapUtils.java
+++ store/src/test/java/com/zextras/mailbox/soap/SoapUtils.java
@@ -6,6 +6,7 @@ package com.zextras.mailbox.soap;
 
 import com.zextras.mailbox.util.SoapClient.SoapResponse;
 import com.zimbra.common.soap.Element;
+import com.zimbra.common.soap.HeaderConstants;
 import com.zimbra.soap.JaxbUtil;
 import org.apache.http.HttpResponse;
 import org.apache.http.util.EntityUtils;
@@ -24,5 +25,33 @@ public class SoapUtils {
   public static <T> T getSoapResponse(String response, String bodyKey, Class<T> expected) throws Exception {
     return JaxbUtil.elementToJaxb(Element.parseXML(response).getElement("Body").getElement(
         bodyKey), expected);
+  }
+
+  /**
+   * Extracts the session ID from a SOAP response header.
+   *
+   * @param response the SOAP response
+   * @return the session ID, or null if not present
+   * @throws Exception if parsing fails
+   */
+  public static String getSessionId(SoapResponse response) throws Exception {
+    Element envelope = Element.parseXML(response.body());
+    Element header = envelope.getOptionalElement("Header");
+    if (header != null) {
+      Element context = header.getOptionalElement(HeaderConstants.CONTEXT);
+      if (context != null) {
+        Element session = context.getOptionalElement(HeaderConstants.E_SESSION);
+        if (session != null) {
+          // Try to get from 'id' attribute first
+          String sessionId = session.getAttribute(HeaderConstants.A_ID, null);
+          if (sessionId == null) {
+            // Try to get from element text
+            sessionId = session.getTextTrim();
+          }
+          return sessionId;
+        }
+      }
+    }
+    return null;
   }
 }
