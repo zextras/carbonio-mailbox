0884927655648c5525574cc09d7ee62997175b29
Keshav Bhatt <keshavnrj@gmail.com>
2026-02-23 17:59:41 +0530

    test: add test for immutability of archive folder

--- store/src/test/java/com/zimbra/cs/mailbox/AddArchiveFolderUtilTest.java
+++ store/src/test/java/com/zimbra/cs/mailbox/AddArchiveFolderUtilTest.java
@@ -3,8 +3,10 @@ package com.zimbra.cs.mailbox;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import com.zextras.mailbox.MailboxTestSuite;
+import com.zimbra.common.service.ServiceException;
 import com.zimbra.cs.account.Account;
 import com.zimbra.cs.account.Provisioning;
 import org.junit.jupiter.api.BeforeEach;
@@ -46,6 +48,22 @@ class AddArchiveFolderUtilTest extends MailboxTestSuite {
     Folder archiveFolder = mailbox.getFolderById(null, Mailbox.ID_FOLDER_ARCHIVE);
     assertNotNull(archiveFolder, "Archive folder should exist after account creation");
     assertEquals("Archive", archiveFolder.getName(), "Archive folder should have correct name");
+  }
+
+  @Test
+  void archiveFolderIsImmutableAndCannotBeDeleted() throws Exception {
+    Account account = Provisioning.getInstance().getAccountByName(testAccount.getName());
+
+    Mailbox mailbox = MailboxManager.getInstance().getMailboxByAccount(account);
+    Folder archiveFolder = mailbox.getFolderById(null, Mailbox.ID_FOLDER_ARCHIVE);
+
+    final ServiceException serviceException =
+        assertThrows(
+            ServiceException.class,
+            () -> mailbox.deleteFolder(null, String.valueOf(archiveFolder.getId())),
+            "Should not be able to delete the archive folder");
+
+    assertEquals("cannot modify immutable object: 20", serviceException.getMessage());
   }
 
   @Test
