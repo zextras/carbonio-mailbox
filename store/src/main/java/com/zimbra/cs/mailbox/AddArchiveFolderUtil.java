32b8d406721cffb8c88c384b87e940a47d96dff4
Keshav Bhatt <keshavnrj@gmail.com>
2026-02-23 15:46:54 +0530

    Revert "chore: remove AddArchiveFolderUtil and usages"
    This reverts commit 7fbdb1b62a443a88515c0505cefb4599b617ea05.

---
+++ store/src/main/java/com/zimbra/cs/mailbox/AddArchiveFolderUtil.java
@@ -0,0 +1,102 @@
+// SPDX-FileCopyrightText: 2026 Zextras <https://www.zextras.com>
+//
+// SPDX-License-Identifier: GPL-2.0-only
+
+package com.zimbra.cs.mailbox;
+
+import static com.zimbra.cs.mailbox.Mailbox.ID_FOLDER_ARCHIVE;
+
+import com.zimbra.common.service.ServiceException;
+import com.zimbra.common.util.UUIDUtil;
+import com.zimbra.common.util.ZimbraLog;
+import com.zimbra.cs.account.Account;
+import com.zimbra.cs.account.Provisioning;
+import com.zimbra.cs.mailbox.MailItem.Type;
+
+public class AddArchiveFolderUtil {
+
+  private static final byte ARCHIVE_SYSTEM_FOLDER_ATTRIBUTES = Folder.FOLDER_IS_IMMUTABLE;
+  private static final String ARCHIVE_FOLDER_NAME = "Archive";
+
+  private AddArchiveFolderUtil() {
+    // utility class, do not instantiate
+  }
+
+  public static void addArchiveFolderToAccount(String accountId, Provisioning provisioning)
+      throws ServiceException {
+
+    Account account = provisioning.getAccountById(accountId);
+    if (account == null) {
+      ZimbraLog.mailbox.info("Account not found: %s", accountId);
+      return;
+    }
+
+    Mailbox mailbox = MailboxManager.getInstance().getMailboxByAccount(account);
+    if (mailbox == null) {
+      ZimbraLog.mailbox.info("Mailbox not found for account: %s", accountId);
+      return;
+    }
+
+    if (archiveSystemFolderExistsIn(mailbox)) {
+      ZimbraLog.mailbox.info("Archive system folder already exists for account: %s", accountId);
+      return;
+    }
+
+    createArchiveSystemFolder(mailbox, accountId);
+  }
+
+  private static boolean archiveSystemFolderExistsIn(Mailbox mailbox) {
+    try {
+      mailbox.getFolderById(null, Mailbox.ID_FOLDER_ARCHIVE);
+      return true;
+    } catch (ServiceException e) {
+      return false;
+    }
+  }
+
+  private static void createArchiveSystemFolder(Mailbox mailbox, String accountId)
+      throws ServiceException {
+
+    mailbox.lock(true);
+
+    try {
+      mailbox.beginTransaction("createArchiveFolder", null);
+      try {
+        Folder userRoot = mailbox.getFolderById(null, Mailbox.ID_FOLDER_USER_ROOT);
+        if (userRoot == null) {
+          throw ServiceException.FAILURE(
+              "USER_ROOT folder not found in mailbox for account: " + accountId, null);
+        }
+
+        createArchiveFolder(mailbox, userRoot, ARCHIVE_SYSTEM_FOLDER_ATTRIBUTES);
+
+        mailbox.endTransaction(true);
+        ZimbraLog.mailbox.info("Created Archive system folder for account %s", accountId);
+
+      } catch (ServiceException e) {
+        mailbox.endTransaction(false);
+        throw e;
+      }
+    } finally {
+      mailbox.unlock();
+    }
+  }
+
+  public static void createArchiveFolder(
+      Mailbox mailbox, Folder userRoot, byte systemFolderAttributes) throws ServiceException {
+
+    Folder.create(
+        ID_FOLDER_ARCHIVE,
+        UUIDUtil.generateUUID(),
+        mailbox,
+        userRoot,
+        ARCHIVE_FOLDER_NAME,
+        systemFolderAttributes,
+        Type.MESSAGE,
+        0,
+        MailItem.DEFAULT_COLOR_RGB,
+        null,
+        null,
+        null);
+  }
+}
