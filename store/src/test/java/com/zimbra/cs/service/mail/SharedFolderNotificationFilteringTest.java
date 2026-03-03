baae5201a387131aaf49fe3f4eff09556306ad8f
Keshav Bhatt <keshavnrj@gmail.com>
2026-03-03 14:43:31 +0530

    test: add tests to filter notifications for name and color changes on group-shared folders

--- store/src/test/java/com/zimbra/cs/service/mail/SharedFolderNotificationFilteringTest.java
+++ store/src/test/java/com/zimbra/cs/service/mail/SharedFolderNotificationFilteringTest.java
@@ -7,6 +7,7 @@ import com.zimbra.common.service.ServiceException;
 import com.zimbra.common.soap.Element;
 import com.zimbra.common.soap.MailConstants;
 import com.zimbra.cs.account.Account;
+import com.zimbra.cs.account.Group;
 import com.zimbra.cs.index.SortBy;
 import com.zimbra.cs.mailbox.Folder;
 import com.zimbra.cs.mailbox.MailItem.Type;
@@ -16,6 +17,8 @@ import com.zimbra.soap.mail.message.FolderActionRequest;
 import com.zimbra.soap.mail.type.ActionGrantSelector;
 import com.zimbra.soap.mail.type.FolderActionSelector;
 import com.zimbra.soap.mail.type.NewMountpointSpec;
+import java.util.HashMap;
+import java.util.UUID;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Tag;
@@ -32,7 +35,7 @@ class SharedFolderNotificationFilteringTest extends SoapTestSuite {
   private static Folder getFirstCalendar(Account user) throws ServiceException {
     var mailbox = mailboxManager.getMailboxByAccount(user);
     var calendarFolders = mailbox.getCalendarFolders(null, SortBy.DATE_DESC);
-    return calendarFolders.get(0);
+    return calendarFolders.getFirst();
   }
 
   @BeforeEach
@@ -198,6 +201,64 @@ class SharedFolderNotificationFilteringTest extends SoapTestSuite {
         "Multiple restricted changes (color, name) on shared folders should be filtered out");
   }
 
+  /**
+   * Tests that name changes on a calendar folder shared with a distribution list (group) are
+   * filtered out from notifications for a member of that group.
+   */
+  @Test
+  void nameChangeOnGroupSharedCalendarFolderIsFilteredOut() throws Exception {
+    Group group = createDistributionListWithMember(granteeAccount);
+
+    var userACalendar = getFirstCalendar(ownerAccount);
+    shareFolderWithGroup(ownerAccount, group, userACalendar.getId());
+    createMountpoint(granteeAccount, userACalendar, "group shared calendar", "appointment");
+
+    String sessionId = createSessionForGrantee();
+    acknowledgeRefresh(sessionId);
+
+    // Owner renames the folder
+    var renameAction = new FolderActionSelector(userACalendar.getFolderIdAsString(), "rename");
+    renameAction.setName("Renamed Group Calendar");
+    getSoapClient().executeSoap(ownerAccount, new FolderActionRequest(renameAction));
+
+    SoapResponse response = checkForNotifications(sessionId);
+
+    Assertions.assertFalse(
+        response.body().contains("<notify"),
+        "Name changes on group-shared calendar folders should be filtered out from notifications");
+  }
+
+  /**
+   * Tests that color changes on a mail folder shared with a distribution list (group) are filtered
+   * out from notifications for a member of that group.
+   */
+  @Test
+  void colorChangeOnGroupSharedMailFolderIsFilteredOut() throws Exception {
+    Group group = createDistributionListWithMember(granteeAccount);
+
+    var mailbox = mailboxManager.getMailboxByAccount(ownerAccount);
+    var mailFolder =
+        mailbox.createFolder(
+            null, "GroupSharedMail", new Folder.FolderOptions().setDefaultView(Type.MESSAGE));
+
+    shareFolderWithGroup(ownerAccount, group, mailFolder.getId());
+    createMountpoint(granteeAccount, mailFolder, "Group Shared Mail", Type.MESSAGE.toString());
+
+    String sessionId = createSessionForGrantee();
+    acknowledgeRefresh(sessionId);
+
+    // Owner changes folder color
+    var colorAction = new FolderActionSelector(String.valueOf(mailFolder.getId()), "color");
+    colorAction.setColor((byte) 4);
+    getSoapClient().executeSoap(ownerAccount, new FolderActionRequest(colorAction));
+
+    SoapResponse response = checkForNotifications(sessionId);
+
+    Assertions.assertFalse(
+        response.body().contains("<notify"),
+        "Color changes on group-shared mail folders should be filtered out from notifications");
+  }
+
   // Helper methods
 
   private String createSessionForGrantee() throws Exception {
@@ -249,5 +310,27 @@ class SharedFolderNotificationFilteringTest extends SoapTestSuite {
     newMountpointSpec.setOwnerId(sharedFolder.getAccountId());
     newMountpointSpec.setFolderId("1");
     getSoapClient().executeSoap(onAccount, new CreateMountpointRequest(newMountpointSpec));
+  }
+
+  /**
+   * Creates a static distribution list on the default domain and adds {@code member} to it.
+   *
+   * @return the created {@link Group}
+   */
+  private Group createDistributionListWithMember(Account member) throws Exception {
+    String listAddress = "dl-" + UUID.randomUUID() + "@" + getDefaultDomainName();
+    Group group = getProvisioning().createGroup(listAddress, new HashMap<>(), false);
+    getProvisioning().addGroupMembers(group, new String[] {member.getName()});
+    return group;
+  }
+
+  private void shareFolderWithGroup(Account owner, Group group, int folderId) throws Exception {
+    var grantRequest = new FolderActionSelector(String.valueOf(folderId), "grant");
+    var grant = new ActionGrantSelector("rwidx", "grp");
+    grant.setZimbraId(group.getId());
+    grant.setDisplayName(group.getName());
+    grant.setPassword("");
+    grantRequest.setGrant(grant);
+    getSoapClient().executeSoap(owner, new FolderActionRequest(grantRequest));
   }
 }
