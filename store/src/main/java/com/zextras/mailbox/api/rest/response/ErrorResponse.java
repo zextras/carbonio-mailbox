0b9782fa8ac5575e86079c17454de7ad0ee33868
drazen04 <hangtime23@hotmail.it>
2026-03-09 16:52:08 +0100

    doc: add header licence

--- store/src/main/java/com/zextras/mailbox/api/rest/response/ErrorResponse.java
+++ store/src/main/java/com/zextras/mailbox/api/rest/response/ErrorResponse.java
@@ -1,3 +1,9 @@
+/*
+ * SPDX-FileCopyrightText: 2026 Zextras <https://www.zextras.com>
+ *
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
+
 package com.zextras.mailbox.api.rest.response;
 
 public record ErrorResponse(String error) {
