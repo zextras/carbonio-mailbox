5d6b0b934de4e7c7c22ef57a2da45134e5af16a0
Davide Frison <davide.frison@zextras.com>
2026-03-18 11:19:34 +0100

    Revert "test: ContactAutoCompleteTest flush index instead of thread sleep"
    This reverts commit 703b2d7f4324596b22a21d8cc38a460febcde86f.

--- store/src/test/java/com/zimbra/cs/mailbox/ContactAutoCompleteTest.java
+++ store/src/test/java/com/zimbra/cs/mailbox/ContactAutoCompleteTest.java
@@ -22,6 +22,7 @@ import java.util.Map;
 import java.util.UUID;
 import javax.mail.internet.InternetAddress;
 import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 
@@ -31,6 +32,7 @@ import org.junit.jupiter.api.Test;
  * @author ysasaki
  */
 
+@Tag("flaky")
 class ContactAutoCompleteTest extends MailboxTestSuite {
 
   @Test
@@ -98,8 +100,8 @@ class ContactAutoCompleteTest extends MailboxTestSuite {
     fields.put(ContactConstants.A_lastName, "subject: from:");
     fields.put(ContactConstants.A_email, getRandomMail());
     mbox.createContact(null, new ParsedContact(fields), Mailbox.ID_FOLDER_CONTACTS, null);
-    MailboxTestUtil.index(mbox);
 
+    Thread.sleep(500);
     ContactAutoComplete autocomplete = new ContactAutoComplete(mbox.getAccount(),
         new OperationContext(mbox));
     assertEquals(1, autocomplete.query("not", null, 100).entries.size());
@@ -118,8 +120,8 @@ class ContactAutoCompleteTest extends MailboxTestSuite {
     fields.put(ContactConstants.A_lastName, "test.server-vmware - dash");
     fields.put(ContactConstants.A_email, getRandomMail());
     mbox.createContact(null, new ParsedContact(fields), Mailbox.ID_FOLDER_CONTACTS, null);
-    MailboxTestUtil.index(mbox);
 
+    Thread.sleep(500);
     ContactAutoComplete autocomplete = new ContactAutoComplete(mbox.getAccount(),
         new OperationContext(mbox));
     assertEquals(1, autocomplete.query("conf -", null, 100).entries.size());
