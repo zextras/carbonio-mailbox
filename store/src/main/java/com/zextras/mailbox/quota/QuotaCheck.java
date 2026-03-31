95e5f75248623c1ba87292a607a606f524a29466
Davide Frison <36330277+frisonisland@users.noreply.github.com>
2026-03-31 17:35:10 +0200

    feat: [CO-3400] mailbox check quota (#970)
    * refactor: rename variables and add comments based on analysis
    
    - add md file explaining current behavior
    
    * feat: implement quota checking mechanism for mailbox operations
    
    * refactor: add new hook apis
    
    * chore: rename for clarity
    
    * refactor: switch to service exception and move old implementation in QuotaHookSingleton
    
    * refactor: switch to service exception and move old implementation in QuotaHookSingleton (checkQuota)
    
    * refactor: rename QuotaHook methods to better describe its use cases
    
    * refactor: rename QuotaHook.java and remove unused files and classes
    
    * chore: revert Conversation
    
    * test: refactor SendMsgIT
    
    * test: add tests on quota check when sending message
    
    * test: test on overquota savedraft
    
    * test: rename tests to avoid double when
    
    * test: refactor set attributes usage
    
    * test: delete message when overquota
    
    * refactor: add service exception to mailbox
    
    ---------
    
    Co-authored-by: Luca Stauble <luca.stauble@gmail.com>
    Co-authored-by: drazen04 <hangtime23@hotmail.it>

---
+++ store/src/main/java/com/zextras/mailbox/quota/QuotaCheck.java
@@ -0,0 +1,14 @@
+package com.zextras.mailbox.quota;
+
+import com.zimbra.common.service.ServiceException;
+import com.zimbra.cs.account.Account;
+
+public interface QuotaCheck {
+
+    void onSendMessage(Account acct) throws ServiceException;
+
+    void onAddMessage(Account acct, long newTotalMailboxUsage) throws ServiceException;
+
+    void onDeleteMessage(Account acct, long size) throws ServiceException;
+}
+
