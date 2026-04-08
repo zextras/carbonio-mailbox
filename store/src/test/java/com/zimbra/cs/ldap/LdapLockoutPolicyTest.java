8e958dcd5e295c64d4de4481ddbe1d895f8f44df
Davide Frison <davide.frison@zextras.com>
2026-04-08 10:14:28 +0200

    chore: cleanup LdapLockoutPolicyTest.java

--- store/src/test/java/com/zimbra/cs/ldap/LdapLockoutPolicyTest.java
+++ store/src/test/java/com/zimbra/cs/ldap/LdapLockoutPolicyTest.java
@@ -7,10 +7,8 @@ package com.zimbra.cs.ldap;
 import static org.junit.jupiter.api.Assertions.*;
 
 import com.zextras.mailbox.MailboxTestSuite;
-import com.zimbra.cs.account.Account;
 import com.zimbra.cs.account.Provisioning;
 import com.zimbra.cs.account.ldap.LdapLockoutPolicy;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
@@ -27,25 +25,26 @@ public class LdapLockoutPolicyTest extends MailboxTestSuite {
 		acct.setPasswordLockoutEnabled(true);
 		acct.setPasswordLockoutMaxFailures(2);
 		acct.setPasswordLockoutFailureLifetime("120s");
+		final Provisioning provisioning = Provisioning.getInstance();
 
 		// First failure
-		LdapLockoutPolicy lockoutPolicy = new LdapLockoutPolicy(Provisioning.getInstance(), acct);
+		LdapLockoutPolicy lockoutPolicy = new LdapLockoutPolicy(provisioning, acct);
 		lockoutPolicy.failedLogin();
 		// failure time is updated
 		assertEquals(1, acct.getPasswordLockoutFailureTimeAsString().length);
 
 		// second Failure
-		lockoutPolicy = new LdapLockoutPolicy(Provisioning.getInstance(), acct);
+		lockoutPolicy = new LdapLockoutPolicy(provisioning, acct);
 		lockoutPolicy.failedLogin();
 		String[] failureTime = acct.getPasswordLockoutFailureTimeAsString();
 		// failure time is updated
 		assertEquals(2, failureTime.length);
 
 		// account should be locked after two failure attempts
-		assertTrue(acct.getAccountStatus().isLockout());
+		assertTrue(provisioning.getAccount(acct.getId()).getAccountStatus().isLockout());
 
 		// Third failure
-		lockoutPolicy = new LdapLockoutPolicy(Provisioning.getInstance(), acct);
+		lockoutPolicy = new LdapLockoutPolicy(provisioning, acct);
 		lockoutPolicy.failedLogin();
 
 		// Third failure attempt should not update failure time
