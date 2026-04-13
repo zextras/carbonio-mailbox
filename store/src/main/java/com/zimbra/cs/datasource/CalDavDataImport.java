96ca2e63cfd6e6e9d757da53f622ba83168bbb35
Keshav Bhatt <keshavnrj@gmail.com>
2026-04-13 17:02:26 +0530

    fix: ensure CalDavDataImport respects import-only mode during sync

--- store/src/main/java/com/zimbra/cs/datasource/CalDavDataImport.java
+++ store/src/main/java/com/zimbra/cs/datasource/CalDavDataImport.java
@@ -662,7 +662,7 @@ public class CalDavDataImport extends MailItemImport {
     while (!allDone) {
       allDone = true;
 
-      if (lastSync > 0) { // Don't push local changes during initial sync.
+      if (lastSync > 0 && !getDataSource().isImportOnly()) { // Don't push local changes during initial sync or in import-only mode.
         // push local deletion
         List<Integer> deleted = new ArrayList<>();
         for (int itemId : mbox.getTombstones(lastSync).getAllIds()) {
