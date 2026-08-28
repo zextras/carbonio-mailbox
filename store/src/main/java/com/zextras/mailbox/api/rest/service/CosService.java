ce3676a31c131e6e1b0cba90dfdd42e887c22156
Davide Frison <36330277+frisonisland@users.noreply.github.com>
2026-08-28 16:28:46 +0200

    refactor: remove cos count API (#1132)
    * refactor: remove cos count API
    
    * test: remove count cos tests
    
    * chore: put back CosResource in InternalApiApplication

--- store/src/main/java/com/zextras/mailbox/api/rest/service/CosService.java
+++ store/src/main/java/com/zextras/mailbox/api/rest/service/CosService.java
@@ -8,14 +8,8 @@ package com.zextras.mailbox.api.rest.service;
 
 import com.zimbra.common.service.ServiceException;
 import com.zimbra.cs.account.Cos;
-import com.zimbra.cs.account.Domain;
 import com.zimbra.cs.account.Provisioning;
-import com.zimbra.cs.account.Provisioning.CountAccountResult.CountAccountByCos;
 import io.vavr.control.Try;
-import java.util.Collection;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
 import java.util.function.Supplier;
 
 public class CosService {
@@ -36,27 +30,4 @@ public class CosService {
     });
   }
 
-  public Try<Map<String, Long>> countCos(Collection<String> cosIds) {
-    return Try.of(() -> {
-      final Provisioning provisioning = provisioningSupplier.get();
-      final Map<String, Long> counts = new LinkedHashMap<>();
-      for (final String cosId :
-          cosIds == null || cosIds.isEmpty() ? allCosIds(provisioning) : cosIds) {
-        counts.put(cosId, 0L);
-      }
-
-      for (final Domain domain : provisioning.getAllDomains()) {
-        for (final CountAccountByCos countByCos :
-            provisioning.countAccount(domain).getCountAccountByCos()) {
-          counts.computeIfPresent(
-              countByCos.getCosId(), (id, count) -> count + countByCos.getCount());
-        }
-      }
-      return counts;
-    });
-  }
-
-  private static List<String> allCosIds(Provisioning provisioning) throws ServiceException {
-    return provisioning.getAllCos().stream().map(Cos::getId).toList();
-  }
 }
--- store/src/test/java/com/zextras/mailbox/api/resource/CosResourceIT.java
+++ store/src/test/java/com/zextras/mailbox/api/resource/CosResourceIT.java
@@ -29,43 +29,6 @@ class CosResourceIT {
 	static final MailboxServerExtension server = new MailboxServerExtension();
 
 	@Test
-	void countsTheRequestedCos() throws Exception {
-		final Cos cos = createCos();
-		createAccountOn(cos);
-		createAccountOn(cos);
-
-		final Response response = countCos("?cosId=" + cos.getId());
-
-		assertEquals(200, response.statusCode());
-		assertThatJson(response.body())
-				.isEqualTo(String.format("{\"total\": 2, \"byCos\": {\"%s\": 2}}", cos.getId()));
-	}
-
-	@Test
-	void countsEveryCosWhenNoCosIdIsGiven() throws Exception {
-		final Cos cos = createCos();
-		createAccountOn(cos);
-
-		final Response response = countCos("");
-
-		assertEquals(200, response.statusCode());
-		assertThatJson(response.body()).node("byCos." + cos.getId()).isEqualTo(1);
-	}
-
-	@Test
-	void rejectsMoreThanOneHundredCosIds() throws Exception {
-		final String cosIds = IntStream.rangeClosed(1, 101)
-				.mapToObj(i -> "cosId=" + UUID.randomUUID())
-				.collect(Collectors.joining("&"));
-
-		final Response response = countCos("?" + cosIds);
-
-		assertEquals(400, response.statusCode());
-		assertThatJson(response.body()).node("error")
-				.isEqualTo("Too many entries: max 100 allowed");
-	}
-
-	@Test
 	void returnsTheCos() throws Exception {
 		final Cos cos = createCos();
 
@@ -86,21 +49,11 @@ class CosResourceIT {
 		assertThatJson(response.body()).node("error").asString().contains(unknownCosId);
 	}
 
-	private static Response countCos(String query) throws Exception {
-		return server.getHttpClient().get(server.getInternalApiEndpoint() + "/cos/count" + query);
-	}
-
 	private static Response getCos(String cosId) throws Exception {
 		return server.getHttpClient().get(server.getInternalApiEndpoint() + "/cos/" + cosId);
 	}
 
 	private static Cos createCos() throws ServiceException {
 		return Provisioning.getInstance().createCos("cos-" + UUID.randomUUID(), new HashMap<>());
-	}
-
-	private static void createAccountOn(Cos cos) throws ServiceException {
-		server.getAccountFactory()
-				.withAttribute(Provisioning.A_zimbraCOSId, cos.getId())
-				.create();
 	}
 }
